5a2e1cd1a9c71c2b69e8cef0a80b4f362bcd02d3
winglechen@aliyun.com <winglechen@gmail.com>
2024-12-05 17:47:42 +0800

    feature: add model Message

---
+++ domain/src/main/java/com/wolf/minimq/domain/model/Message.java
@@ -0,0 +1,20 @@
+package com.wolf.minimq.domain.model;
+
+import java.io.Serializable;
+import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Message implements Serializable {
+    private String topic;
+    private int flag;
+    private Map<String, String> properties;
+    private byte[] body;
+    private String transactionId;
+}
