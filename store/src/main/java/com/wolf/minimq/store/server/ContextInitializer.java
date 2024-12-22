e306f2349acfe27c943cd3123306f1990656b93a
winglechen@aliyun.com <winglechen@gmail.com>
2024-12-22 10:48:14 +0800

    feature: initializeConfig for module store

--- store/src/main/java/com/wolf/minimq/store/server/ContextInitializer.java
+++ store/src/main/java/com/wolf/minimq/store/server/ContextInitializer.java
@@ -1,7 +1,29 @@
 package com.wolf.minimq.store.server;
 
-public class ContextInitializer {
-    public static void init(StoreArgument argument) {
+import com.wolf.minimq.domain.config.CommitLogConfig;
+import com.wolf.minimq.domain.config.StoreConfig;
+import com.wolf.minimq.domain.config.TimerConfig;
 
+public class ContextInitializer {
+    private final StoreArgument argument;
+
+    public static void init(StoreArgument argument) {
+        ContextInitializer initializer = new ContextInitializer(argument);
+        initializer.initialize();
     }
+
+    public ContextInitializer(StoreArgument argument) {
+        this.argument = argument;
+    }
+
+    public void initialize() {
+        initializeConfig();
+    }
+
+    private void initializeConfig() {
+        StoreContext.register(new StoreConfig());
+        StoreContext.register(new CommitLogConfig());
+        StoreContext.register(new TimerConfig());
+    }
+
 }
