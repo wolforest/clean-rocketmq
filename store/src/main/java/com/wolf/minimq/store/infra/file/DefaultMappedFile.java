fb149ec68a8dfb7a072b0d10fa829af394e5c93b
winglechen@aliyun.com <winglechen@gmail.com>
2024-12-24 17:31:59 +0800

    feature: impl select methods of DefaultMappedFile

--- store/src/main/java/com/wolf/minimq/store/infra/file/DefaultMappedFile.java
+++ store/src/main/java/com/wolf/minimq/store/infra/file/DefaultMappedFile.java
@@ -1,10 +1,13 @@
 package com.wolf.minimq.store.infra.file;
 
+import com.sun.jna.NativeLong;
+import com.sun.jna.Pointer;
 import com.wolf.common.util.io.BufferUtil;
 import com.wolf.common.util.io.DirUtil;
 import com.wolf.minimq.domain.service.store.infra.MappedFile;
 import com.wolf.minimq.domain.model.vo.AppendResult;
 import com.wolf.minimq.domain.model.vo.SelectedMappedBuffer;
+import com.wolf.minimq.store.infra.memory.CLibrary;
 import com.wolf.minimq.store.infra.memory.TransientStorePool;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -125,12 +128,38 @@ public class DefaultMappedFile extends ReferenceResource implements MappedFile {
 
     @Override
     public SelectedMappedBuffer select(int pos, int size) {
-        return null;
+        int dataPosition = getWritePosition();
+        if ((pos + size) > dataPosition) {
+            return null;
+        }
+
+        if (!this.hold()) {
+            return null;
+        }
+
+        ByteBuffer buffer = this.mappedByteBuffer.slice();
+        buffer.position(pos);
+
+        ByteBuffer newBuffer = buffer.slice();
+        newBuffer.limit(size);
+
+        return SelectedMappedBuffer.builder()
+            .startOffset(this.offsetInFileName + pos)
+            .byteBuffer(newBuffer)
+            .size(size)
+            .mappedFile(this)
+            .build();
     }
 
     @Override
     public SelectedMappedBuffer select(int pos) {
-        return null;
+        int dataPosition = getWritePosition();
+        if (pos >= dataPosition || pos < 0) {
+            return null;
+        }
+
+        int size = dataPosition - pos;
+        return select(pos, size);
     }
 
     @Override
@@ -204,6 +233,46 @@ public class DefaultMappedFile extends ReferenceResource implements MappedFile {
                 this.fileChannel.close();
             }
         }
+    }
+
+    private boolean isAbleToCommit(final int commitLeastPages) {
+        int commit = commitPosition.get();
+        int write = writePosition.get();
+
+        if (this.isFull()) {
+            return true;
+        }
+
+        if (commitLeastPages > 0) {
+            return ((write / OS_PAGE_SIZE) - (commit / OS_PAGE_SIZE)) >= commitLeastPages;
+        }
+
+        return write > commit;
+    }
+
+    private boolean isAbleToFlush(final int flushLeastPages) {
+        int flush = flushPosition.get();
+        int write = getWritePosition();
+
+        if (this.isFull()) {
+            return true;
+        }
+
+        if (flushLeastPages > 0) {
+            return ((write / OS_PAGE_SIZE) - (flush / OS_PAGE_SIZE)) >= flushLeastPages;
+        }
+
+        return write > flush;
+    }
+
+    private void mlock(Pointer pointer, long address, long beginTime) {
+        int ret = CLibrary.INSTANCE.mlock(pointer, new NativeLong(this.fileSize));
+        log.info("mlock {} {} {} ret = {} time consuming = {}", address, this.fileName, this.fileSize, ret, System.currentTimeMillis() - beginTime);
+    }
+
+    private void madvise(Pointer pointer, long address, long beginTime) {
+        int ret = CLibrary.INSTANCE.madvise(pointer, new NativeLong(this.fileSize), CLibrary.MADV_WILLNEED);
+        log.info("madvise {} {} {} ret = {} time consuming = {}", address, this.fileName, this.fileSize, ret, System.currentTimeMillis() - beginTime);
     }
 
 }
