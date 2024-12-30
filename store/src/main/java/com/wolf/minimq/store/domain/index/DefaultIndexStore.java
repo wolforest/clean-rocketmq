8636700d67f2cded9c04ea963b628cbd357930df
winglechen@aliyun.com <winglechen@gmail.com>
2024-12-30 22:33:50 +0800

    feature: add empty DefaultIndexStore and DefaultTimerStore

---
+++ store/src/main/java/com/wolf/minimq/store/domain/index/DefaultIndexStore.java
@@ -0,0 +1,6 @@
+package com.wolf.minimq.store.domain.index;
+
+import com.wolf.minimq.domain.service.store.domain.IndexStore;
+
+public class DefaultIndexStore implements IndexStore {
+}
---
+++ store/src/main/java/com/wolf/minimq/store/domain/timer/DefaultTimerStore.java
@@ -0,0 +1,6 @@
+package com.wolf.minimq.store.domain.timer;
+
+import com.wolf.minimq.domain.service.store.domain.TimerStore;
+
+public class DefaultTimerStore implements TimerStore {
+}
