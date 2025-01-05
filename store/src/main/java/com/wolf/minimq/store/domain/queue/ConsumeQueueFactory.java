26e86a285dc16fdc4271b808413d5f8148933e27
winglechen@aliyun.com <winglechen@gmail.com>
2025-01-05 17:35:52 +0800

    feature: impl method get of ConsumeQueueFactory

--- store/src/main/java/com/wolf/minimq/store/domain/queue/ConsumeQueueFactory.java
+++ store/src/main/java/com/wolf/minimq/store/domain/queue/ConsumeQueueFactory.java
@@ -2,12 +2,25 @@ package com.wolf.minimq.store.domain.queue;
 
 import com.wolf.minimq.domain.service.store.domain.ConsumeQueue;
 import com.wolf.minimq.domain.service.store.domain.meta.TopicStore;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 public class ConsumeQueueFactory {
+    private static final int TOPIC_MAP_SIZE = 32;
+    private static final int QUEUE_MAP_SIZE = 128;
     private final TopicStore topicStore;
+    /**
+     * consume queue map, structure:
+     * - topic
+     *  - queueId
+     *    - consumeQueue
+     */
+    protected final ConcurrentMap<String/* topic */, ConcurrentMap<Integer/* queueId */, ConsumeQueue>> topicMap;
 
     public ConsumeQueueFactory(TopicStore topicStore) {
         this.topicStore = topicStore;
+        this.topicMap = new ConcurrentHashMap<>(TOPIC_MAP_SIZE);
     }
 
     public ConsumeQueue getOrCreate(String topic, int queueId) {
@@ -15,7 +28,48 @@ public class ConsumeQueueFactory {
             return ErrorConsumeQueue.singleton();
         }
 
+        ConsumeQueue queue = get(topic, queueId);
+        if (queue != null) {
+            return queue;
+        }
+
+        return create(topic, queueId);
+    }
+
+    private ConsumeQueue get(String topic, int queueId) {
+        Map<Integer, ConsumeQueue> queueMap = topicMap.get(topic);
+        if (queueMap == null) {
+            return null;
+        }
+
+        return queueMap.get(queueId);
+    }
+
+    private ConsumeQueue create(String topic, int queueId) {
+        ConcurrentMap<Integer, ConsumeQueue> queueMap = initQueueMap(topic);
+        if (queueMap.containsKey(queueId)) {
+            return queueMap.get(queueId);
+        }
+
+        ConsumeQueue queue = createConsumeQueue(topic, queueId);
+        ConsumeQueue result = queueMap.putIfAbsent(queueId, queue);
+
+        return result == null ? queue : result;
+    }
+
+    private ConsumeQueue createConsumeQueue(String topic, int queueId) {
         return null;
+    }
+
+    private ConcurrentMap<Integer, ConsumeQueue> initQueueMap(String topic) {
+        if (topicMap.containsKey(topic)) {
+            return topicMap.get(topic);
+        }
+
+        ConcurrentMap<Integer, ConsumeQueue> queueMap = new ConcurrentHashMap<>(QUEUE_MAP_SIZE);
+        ConcurrentMap<Integer, ConsumeQueue> resultMap = topicMap.putIfAbsent(topic, queueMap);
+
+        return resultMap == null ? queueMap : resultMap;
     }
 
 }
