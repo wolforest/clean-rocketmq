23472270cc1b6108090f7834feeb411451ed4eca
winglechen@aliyun.com <winglechen@gmail.com>
2025-01-13 18:02:49 +0800

    feature: add ConsumerOffset and ConsumerOffsetService

---
+++ domain/src/main/java/com/wolf/minimq/domain/model/meta/ConsumerOffset.java
@@ -0,0 +1,36 @@
+package com.wolf.minimq.domain.model.meta;
+
+import com.alibaba.fastjson2.annotation.JSONField;
+import java.io.Serializable;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import lombok.Data;
+
+@Data
+public class ConsumerOffset implements Serializable {
+
+    private ConcurrentMap<String, ConcurrentMap<Integer, Long>> offsetMap
+        = new ConcurrentHashMap<>(512);
+
+    @JSONField(serialize = false)
+    public long getOffset(String group, String topic, int queueId) {
+        return 0;
+    }
+
+    @JSONField(serialize = false)
+    public long getAndRemove(String group, String topic, int queueId) {
+        return 0;
+    }
+
+    @JSONField(serialize = false)
+    public Map<Integer, Long> getAll(String group, String topic) {
+        return null;
+    }
+
+    @JSONField(serialize = false)
+    public void putOffset(String group, String topic, int queueId, long offset) {
+
+    }
+
+}
