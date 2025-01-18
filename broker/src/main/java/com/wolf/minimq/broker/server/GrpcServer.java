66223e799a5782d6f9b0ae0407b7c1b6e261bae8
winglechen@aliyun.com <winglechen@gmail.com>
2025-01-19 00:02:04 +0800

    feature: add empty GrpcServer

---
+++ broker/src/main/java/com/wolf/minimq/broker/server/GrpcServer.java
@@ -0,0 +1,30 @@
+package com.wolf.minimq.broker.server;
+
+import com.wolf.common.convention.service.Lifecycle;
+
+public class GrpcServer implements Lifecycle {
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void shutdown() {
+
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void cleanup() {
+
+    }
+
+    @Override
+    public State getState() {
+        return State.RUNNING;
+    }
+}
