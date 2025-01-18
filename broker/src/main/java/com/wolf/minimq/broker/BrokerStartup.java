4c61b80b5360a51c706bb218d2a730587a6bcc21
winglechen@aliyun.com <winglechen@gmail.com>
2025-01-19 00:04:33 +0800

    feature: add empty BrokerStartup

---
+++ broker/src/main/java/com/wolf/minimq/broker/BrokerStartup.java
@@ -0,0 +1,7 @@
+package com.wolf.minimq.broker;
+
+public class BrokerStartup {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ broker/src/main/java/com/wolf/minimq/broker/server/Broker.java
@@ -0,0 +1,35 @@
+package com.wolf.minimq.broker.server;
+
+import com.wolf.common.convention.service.Lifecycle;
+
+/**
+ * gateway of broker module
+ *  - run()
+ */
+public class Broker implements Lifecycle {
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void shutdown() {
+
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void cleanup() {
+
+    }
+
+    @Override
+    public State getState() {
+        return null;
+    }
+}
