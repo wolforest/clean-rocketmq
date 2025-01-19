c6a014920939061f96a9f6b95ce1385ea670265f
winglechen@aliyun.com <winglechen@gmail.com>
2025-01-19 12:07:21 +0800

    feature: add some config properties to BrokerConfig

--- domain/src/main/java/com/wolf/minimq/domain/config/BrokerConfig.java
+++ domain/src/main/java/com/wolf/minimq/domain/config/BrokerConfig.java
@@ -1,7 +1,18 @@
 package com.wolf.minimq.domain.config;
 
 import java.io.Serializable;
+import lombok.Data;
 
+@Data
 public class BrokerConfig implements Serializable {
+    private static final int PROCESSOR_NUMBER = Runtime.getRuntime().availableProcessors();
+
+    private Integer grpcPort = 8081;
+    private boolean enableGrpcEpoll = false;
+    private long grpcShutdownTimeout = 30;
+    private int grpcBossThreadNum = 1;
+    private int grpcWorkerThreadNum = PROCESSOR_NUMBER * 2;
+    private int grpcThreadPoolNums = 16 + PROCESSOR_NUMBER * 2;
+    private int grpcThreadPoolQueueCapacity = 100000;
 
 }
