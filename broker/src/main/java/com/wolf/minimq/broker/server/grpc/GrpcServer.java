065608e3c77231855236439d70f52669b67a244e
winglechen@aliyun.com <winglechen@gmail.com>
2025-01-23 17:05:08 +0800

    feature: impl GrpcServer

--- broker/src/main/java/com/wolf/minimq/broker/server/grpc/GrpcServer.java
+++ broker/src/main/java/com/wolf/minimq/broker/server/grpc/GrpcServer.java
@@ -1,20 +1,39 @@
 package com.wolf.minimq.broker.server.grpc;
 
 import com.wolf.common.convention.service.Lifecycle;
+import com.wolf.common.lang.exception.SystemException;
+import io.grpc.Server;
+import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class GrpcServer implements Lifecycle {
+    private final Server server;
+    private final long timeout;
 
+    public GrpcServer(Server server, long timeout) {
+        this.server = server;
+        this.timeout = timeout;
+    }
 
     @Override
     public void start() {
-
+        try {
+            this.server.start();
+        } catch (Exception e) {
+            log.error("start grpc server error", e);
+            throw new SystemException("start grpc server error");
+        }
     }
 
     @Override
     public void shutdown() {
-
+        try {
+            this.server.shutdown()
+                .awaitTermination(timeout, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            log.error("shutdown grpc server error", e);
+        }
     }
 
     @Override
