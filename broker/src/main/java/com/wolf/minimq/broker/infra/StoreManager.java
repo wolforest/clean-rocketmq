d136a6091bea1a1af7016eac9f85205e762fa19c
winglechen@aliyun.com <winglechen@gmail.com>
2025-01-25 10:01:00 +0800

    feature: add StoreManager

---
+++ broker/src/main/java/com/wolf/minimq/broker/infra/StoreManager.java
@@ -0,0 +1,41 @@
+package com.wolf.minimq.broker.infra;
+
+import com.wolf.common.convention.container.ApplicationContext;
+import com.wolf.common.convention.service.Lifecycle;
+import com.wolf.minimq.broker.server.vo.BrokerContext;
+import com.wolf.minimq.store.Store;
+import com.wolf.minimq.store.server.StoreArgument;
+
+public class StoreManager implements Lifecycle {
+    private Store store;
+
+    @Override
+    public void initialize() {
+        StoreArgument storeArgument = new StoreArgument();
+        store = new Store(storeArgument);
+        store.initialize();
+    }
+
+    @Override
+    public void start() {
+        store.start();
+
+        ApplicationContext storeApi = store.getAPIContext();
+        BrokerContext.registerContext(storeApi);
+    }
+
+    @Override
+    public void shutdown() {
+        store.shutdown();
+    }
+
+    @Override
+    public void cleanup() {
+        store.cleanup();
+    }
+
+    @Override
+    public State getState() {
+        return State.RUNNING;
+    }
+}
