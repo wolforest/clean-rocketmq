a75cb5572221381becdd8ec7446a1542c53d7682
winglechen@aliyun.com <winglechen@gmail.com>
2025-02-18 10:43:51 +0800

    feature: replace rocketmq-proto by inject proto files

--- broker/src/main/java/cn/coderule/minimq/broker/server/grpc/activity/RouteActivity.java
+++ broker/src/main/java/cn/coderule/minimq/broker/server/grpc/activity/RouteActivity.java
@@ -1,6 +1,7 @@
 package cn.coderule.minimq.broker.server.grpc.activity;
 
 import apache.rocketmq.v2.Code;
+import apache.rocketmq.v2.MessageQueue;
 import apache.rocketmq.v2.QueryAssignmentRequest;
 import apache.rocketmq.v2.QueryAssignmentResponse;
 import apache.rocketmq.v2.QueryRouteRequest;
@@ -9,6 +10,7 @@ import apache.rocketmq.v2.Status;
 import cn.coderule.minimq.broker.api.ConsumerController;
 import cn.coderule.minimq.broker.api.ProducerController;
 import io.grpc.stub.StreamObserver;
+import java.util.ArrayList;
 import java.util.concurrent.ThreadPoolExecutor;
 import lombok.Setter;
 
@@ -26,6 +28,7 @@ public class RouteActivity {
 
     public void getRoute(QueryRouteRequest request, StreamObserver<QueryRouteResponse> responseObserver) {
         QueryRouteResponse response = QueryRouteResponse.newBuilder()
+            .addAllMessageQueues(new ArrayList<>())
             .setStatus(Status.newBuilder().setCode(Code.OK))
             .build();
 
