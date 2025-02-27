fe13e6145edabc167732adcb9350af62c68b53b5
winglechen@aliyun.com <winglechen@gmail.com>
2025-02-27 14:39:18 +0800

    feature: add cluster config to StoreConfig

--- domain/src/main/java/cn/coderule/minimq/domain/config/StoreConfig.java
+++ domain/src/main/java/cn/coderule/minimq/domain/config/StoreConfig.java
@@ -6,6 +6,22 @@ import lombok.Data;
 
 @Data
 public class StoreConfig implements Serializable {
+    /**
+     * store cluster name
+     */
+    private String cluster;
+    /**
+     * store (Master/slave) group name
+     */
+    private String group;
+    /**
+     * store group no
+     *  - -1 : local
+     *  - 0 : master
+     *  - 1 ... : slave
+     */
+    private int groupNo;
+
     private String host;
     private int port;
 
