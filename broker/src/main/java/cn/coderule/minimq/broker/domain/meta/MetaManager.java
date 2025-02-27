f3536631b602d94b9c312193c782837e2ba1119f
winglechen@aliyun.com <winglechen@gmail.com>
2025-02-27 18:18:27 +0800

    feature: add empty MetaManager and RouteService

---
+++ broker/src/main/java/cn/coderule/minimq/broker/domain/meta/MetaManager.java
@@ -0,0 +1,30 @@
+package cn.coderule.minimq.broker.domain.meta;
+
+import cn.coderule.common.convention.service.Lifecycle;
+
+public class MetaManager implements Lifecycle {
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void shutdown() {
+
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void cleanup() {
+
+    }
+
+    @Override
+    public State getState() {
+        return null;
+    }
+}
