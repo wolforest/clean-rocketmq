6d9561f81050b0553b876705c16dba829382c21c
winglechen@aliyun.com <winglechen@gmail.com>
2025-02-27 18:23:05 +0800

    feature: add RouteController

---
+++ broker/src/main/java/cn/coderule/minimq/broker/api/RouteController.java
@@ -0,0 +1,17 @@
+package cn.coderule.minimq.broker.api;
+
+import cn.coderule.minimq.broker.domain.meta.RouteService;
+import cn.coderule.minimq.domain.model.MessageQueue;
+import java.util.Set;
+
+public class RouteController {
+    private final RouteService routeService;
+
+    public RouteController(RouteService routeService) {
+        this.routeService = routeService;
+    }
+
+    public Set<MessageQueue> getRoute(String topic) {
+        return routeService.getRoute(topic);
+    }
+}
--- broker/src/main/java/cn/coderule/minimq/broker/domain/meta/RouteService.java
+++ broker/src/main/java/cn/coderule/minimq/broker/domain/meta/RouteService.java
@@ -1,4 +1,11 @@
 package cn.coderule.minimq.broker.domain.meta;
 
+import cn.coderule.minimq.domain.model.MessageQueue;
+import java.util.Set;
+
 public class RouteService {
+
+    public Set<MessageQueue> getRoute(String topic) {
+        return null;
+    }
 }
