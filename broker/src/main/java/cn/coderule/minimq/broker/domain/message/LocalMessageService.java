7bad111e0cce610885a9eb018407b576e40f0bb6
winglechen@aliyun.com <winglechen@gmail.com>
2025-02-27 14:35:38 +0800

    feature: add message package

---
+++ broker/src/main/java/cn/coderule/minimq/broker/domain/message/LocalMessageService.java
@@ -0,0 +1,46 @@
+package cn.coderule.minimq.broker.domain.message;
+
+import cn.coderule.minimq.domain.model.bo.MessageBO;
+import cn.coderule.minimq.domain.model.dto.EnqueueResult;
+import cn.coderule.minimq.domain.model.dto.GetRequest;
+import cn.coderule.minimq.domain.model.dto.GetResult;
+import cn.coderule.minimq.domain.service.store.domain.MessageStore;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+public class LocalMessageService implements MessageStore {
+    @Override
+    public EnqueueResult enqueue(MessageBO messageBO) {
+        return null;
+    }
+
+    @Override
+    public CompletableFuture<EnqueueResult> enqueueAsync(MessageBO messageBO) {
+        return null;
+    }
+
+    @Override
+    public GetResult get(String topic, int queueId, long offset) {
+        return null;
+    }
+
+    @Override
+    public GetResult get(String topic, int queueId, long offset, int num) {
+        return null;
+    }
+
+    @Override
+    public GetResult get(GetRequest request) {
+        return null;
+    }
+
+    @Override
+    public MessageBO getMessage(String topic, int queueId, long offset) {
+        return null;
+    }
+
+    @Override
+    public List<MessageBO> getMessage(String topic, int queueId, long offset, int num) {
+        return List.of();
+    }
+}
