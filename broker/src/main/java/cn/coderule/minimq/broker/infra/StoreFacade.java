4880933fbdda638b94a9693376b2509f99c05df3
winglechen@aliyun.com <winglechen@gmail.com>
2025-03-01 17:57:10 +0800

    feature: add StoreFacade

---
+++ broker/src/main/java/cn/coderule/minimq/broker/infra/StoreFacade.java
@@ -0,0 +1,10 @@
+package cn.coderule.minimq.broker.infra;
+
+import cn.coderule.minimq.broker.domain.meta.RouteService;
+import cn.coderule.minimq.domain.service.store.api.MessageService;
+
+public class StoreFacade {
+    private MessageService messageService;
+    private RouteService routeService;
+    // private StoreClient storeClient;
+}
