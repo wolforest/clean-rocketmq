332df62334517c7dc70edd69fdeec1ac6f195eb3
winglechen@aliyun.com <winglechen@gmail.com>
2025-03-03 18:10:54 +0800

    feature: add empty NettyClient

---
+++ rpc/src/main/java/cn/coderule/minimq/rpc/common/client/NettyClient.java
@@ -0,0 +1,33 @@
+package cn.coderule.minimq.rpc.common.client;
+
+import cn.coderule.minimq.rpc.common.RpcClient;
+import cn.coderule.minimq.rpc.common.core.NettyService;
+import cn.coderule.minimq.rpc.common.core.RpcHook;
+import cn.coderule.minimq.rpc.common.core.RpcPipeline;
+
+public class NettyClient extends NettyService implements RpcClient {
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void shutdown() {
+
+    }
+
+    @Override
+    public void registerRpcHook(RpcHook rpcHook) {
+
+    }
+
+    @Override
+    public void clearRpcHook() {
+
+    }
+
+    @Override
+    public void setRpcPipeline(RpcPipeline pipeline) {
+
+    }
+}
