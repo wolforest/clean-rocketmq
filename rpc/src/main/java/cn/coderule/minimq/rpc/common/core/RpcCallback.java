5fff935524c315da606a628976d9ac198b0e6f40
winglechen@aliyun.com <winglechen@gmail.com>
2025-03-04 12:03:54 +0800

    feature: add methods to interface RpcCallback

--- rpc/src/main/java/cn/coderule/minimq/rpc/common/core/RpcCallback.java
+++ rpc/src/main/java/cn/coderule/minimq/rpc/common/core/RpcCallback.java
@@ -1,4 +1,8 @@
 package cn.coderule.minimq.rpc.common.core;
 
 public interface RpcCallback {
+
+    void onComplete();
+    default void onSuccess(final RpcCommand response) {}
+    default void onFailure(final Throwable throwable) {}
 }
