3d8383b0dd23b0ff8e1de6552073ab9b4b8297eb
winglechen@aliyun.com <winglechen@gmail.com>
2025-03-05 17:00:26 +0800

    feature: add methods to interface RpcClient

--- rpc/src/main/java/cn/coderule/minimq/rpc/common/RpcClient.java
+++ rpc/src/main/java/cn/coderule/minimq/rpc/common/RpcClient.java
@@ -1,4 +1,48 @@
 package cn.coderule.minimq.rpc.common;
 
+import cn.coderule.minimq.rpc.common.core.ResponseFuture;
+import cn.coderule.minimq.rpc.common.core.RpcCallback;
+import cn.coderule.minimq.rpc.common.core.RpcCommand;
+import cn.coderule.minimq.rpc.common.core.RpcProcessor;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+
 public interface RpcClient extends RpcService {
+    boolean isChannelWritable(final String addr);
+    boolean isAddressReachable(final String addr);
+    void closeChannels(final List<String> addrList);
+    default void closeChannel(final String addr) {
+        closeChannels(List.of(addr));
+    }
+
+    void registerProcessor(int requestCode, RpcProcessor processor, ExecutorService executor);
+
+    RpcCommand invokeSync(final String addr, final RpcCommand request, final long timeoutMillis) throws Exception;
+    void invokeAsync(final String addr, final RpcCommand request, final long timeoutMillis, final RpcCallback invokeCallback) throws Exception;
+    void invokeOneway(final String addr, final RpcCommand request, final long timeoutMillis) throws Exception;
+
+    default CompletableFuture<RpcCommand> invoke(final String addr, final RpcCommand request, final long timeoutMillis) {
+        CompletableFuture<RpcCommand> future = new CompletableFuture<>();
+        try {
+            invokeAsync(addr, request, timeoutMillis, new RpcCallback() {
+                @Override
+                public void onComplete(ResponseFuture responseFuture) {
+                }
+
+                @Override
+                public void onSuccess(RpcCommand response) {
+                    future.complete(response);
+                }
+
+                @Override
+                public void onFailure(Throwable throwable) {
+                    future.completeExceptionally(throwable);
+                }
+            });
+        } catch (Throwable t) {
+            future.completeExceptionally(t);
+        }
+        return future;
+    }
 }
--- rpc/src/main/java/cn/coderule/minimq/rpc/common/netty/NettyClient.java
+++ rpc/src/main/java/cn/coderule/minimq/rpc/common/netty/NettyClient.java
@@ -1,10 +1,15 @@
 package cn.coderule.minimq.rpc.common.netty;
 
 import cn.coderule.minimq.rpc.common.RpcClient;
+import cn.coderule.minimq.rpc.common.core.RpcCallback;
+import cn.coderule.minimq.rpc.common.core.RpcCommand;
 import cn.coderule.minimq.rpc.common.core.RpcHook;
 import cn.coderule.minimq.rpc.common.core.RpcListener;
 import cn.coderule.minimq.rpc.common.core.RpcPipeline;
+import cn.coderule.minimq.rpc.common.core.RpcProcessor;
 import cn.coderule.minimq.rpc.config.RpcClientConfig;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
 
 public class NettyClient extends NettyService implements RpcClient {
     private final RpcClientConfig config;
@@ -44,5 +49,40 @@ public class NettyClient extends NettyService implements RpcClient {
 
     }
 
+    @Override
+    public boolean isChannelWritable(String addr) {
+        return false;
+    }
 
+    @Override
+    public boolean isAddressReachable(String addr) {
+        return false;
+    }
+
+    @Override
+    public void closeChannels(List<String> addrList) {
+
+    }
+
+    @Override
+    public void registerProcessor(int requestCode, RpcProcessor processor, ExecutorService executor) {
+
+    }
+
+    @Override
+    public RpcCommand invokeSync(String addr, RpcCommand request,
+        long timeoutMillis) throws Exception {
+        return null;
+    }
+
+    @Override
+    public void invokeAsync(String addr, RpcCommand request, long timeoutMillis,
+        RpcCallback invokeCallback) throws Exception {
+
+    }
+
+    @Override
+    public void invokeOneway(String addr, RpcCommand request, long timeoutMillis) throws Exception {
+
+    }
 }
