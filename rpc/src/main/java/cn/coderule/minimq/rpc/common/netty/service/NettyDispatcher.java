94cbc5666606e9da74da91842ffece3975be559f
winglechen@aliyun.com <winglechen@gmail.com>
2025-03-09 00:09:57 +0800

    feature: impl constructor of NettyDispatcher

--- rpc/src/main/java/cn/coderule/minimq/rpc/common/netty/service/NettyDispatcher.java
+++ rpc/src/main/java/cn/coderule/minimq/rpc/common/netty/service/NettyDispatcher.java
@@ -1,13 +1,52 @@
 package cn.coderule.minimq.rpc.common.netty.service;
 
+import cn.coderule.common.ds.Pair;
+import cn.coderule.minimq.rpc.common.RpcHook;
+import cn.coderule.minimq.rpc.common.core.invoke.ResponseFuture;
 import cn.coderule.minimq.rpc.common.core.invoke.RpcCommand;
 import cn.coderule.minimq.rpc.common.RpcProcessor;
 import io.netty.channel.ChannelHandlerContext;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Semaphore;
 
 public class NettyDispatcher {
-    public void register(int requestCode, RpcProcessor processor, ExecutorService executor) {
+    private final Semaphore onewaySemaphore;
+    private final Semaphore asyncSemaphore;
 
+    /**
+     * response map
+     * { opaque : ResponseFuture }
+     */
+    private final ConcurrentMap<Integer, ResponseFuture> responseMap = new ConcurrentHashMap<>(256);
+    /**
+     * processor map
+     * { requestCode: [RpcProcessor, ExecutorService] }
+     */
+    private final HashMap<Integer, Pair<RpcProcessor, ExecutorService>> processorMap = new HashMap<>(64);
+    private final List<RpcHook> rpcHooks = new ArrayList<>();
+
+    public NettyDispatcher(int onewaySemaphorePermits, int asyncSemaphorePermits) {
+        this.onewaySemaphore = new Semaphore(onewaySemaphorePermits, true);
+        this.asyncSemaphore = new Semaphore(asyncSemaphorePermits, true);
+    }
+
+    public void registerProcessor(int requestCode, RpcProcessor processor, ExecutorService executor) {
+
+    }
+
+    public void registerRpcHook(RpcHook rpcHook) {
+        if (rpcHook != null && !rpcHooks.contains(rpcHook)) {
+            rpcHooks.add(rpcHook);
+        }
+    }
+
+    public void clearRpcHook() {
+        rpcHooks.clear();
     }
 
     public void dispatch(ChannelHandlerContext ctx, RpcCommand command) {
