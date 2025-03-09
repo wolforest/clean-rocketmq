f643f2593cf226f4df757291744732540b45ca99
winglechen@aliyun.com <winglechen@gmail.com>
2025-03-09 11:47:02 +0800

    feature: impl the main flow of processRequest

--- rpc/src/main/java/cn/coderule/minimq/rpc/common/core/invoke/RpcContext.java
+++ rpc/src/main/java/cn/coderule/minimq/rpc/common/core/invoke/RpcContext.java
@@ -1,9 +1,18 @@
 package cn.coderule.minimq.rpc.common.core.invoke;
 
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import lombok.Data;
 
 @Data
 public class RpcContext {
-    private ChannelHandlerContext channelContext;
+    private final ChannelHandlerContext channelContext;
+
+    public RpcContext(ChannelHandlerContext channelContext) {
+        this.channelContext = channelContext;
+    }
+
+    public Channel channel() {
+        return channelContext.channel();
+    }
 }
--- rpc/src/main/java/cn/coderule/minimq/rpc/common/netty/service/NettyDispatcher.java
+++ rpc/src/main/java/cn/coderule/minimq/rpc/common/netty/service/NettyDispatcher.java
@@ -2,17 +2,21 @@ package cn.coderule.minimq.rpc.common.netty.service;
 
 import cn.coderule.common.ds.Pair;
 import cn.coderule.minimq.rpc.common.RpcHook;
+import cn.coderule.minimq.rpc.common.core.invoke.RequestTask;
 import cn.coderule.minimq.rpc.common.core.invoke.ResponseFuture;
 import cn.coderule.minimq.rpc.common.core.invoke.RpcCommand;
 import cn.coderule.minimq.rpc.common.RpcProcessor;
-import io.netty.channel.ChannelHandlerContext;
+import cn.coderule.minimq.rpc.common.core.invoke.RpcContext;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicBoolean;
+import lombok.NonNull;
 
 public class NettyDispatcher {
     private final Semaphore onewaySemaphore;
@@ -29,14 +33,20 @@ public class NettyDispatcher {
      */
     private final HashMap<Integer, Pair<RpcProcessor, ExecutorService>> processorMap = new HashMap<>(64);
     private final List<RpcHook> rpcHooks = new ArrayList<>();
+    protected AtomicBoolean isShuttingDown = new AtomicBoolean(false);
 
     public NettyDispatcher(int onewaySemaphorePermits, int asyncSemaphorePermits) {
         this.onewaySemaphore = new Semaphore(onewaySemaphorePermits, true);
         this.asyncSemaphore = new Semaphore(asyncSemaphorePermits, true);
     }
 
-    public void registerProcessor(int requestCode, RpcProcessor processor, ExecutorService executor) {
+    public void shutdown() {
+        this.isShuttingDown.set(true);
+    }
 
+    public void registerProcessor(int requestCode, @NonNull RpcProcessor processor, @NonNull ExecutorService executor) {
+        Pair<RpcProcessor, ExecutorService> pair = Pair.of(processor, executor);
+        processorMap.put(requestCode, pair);
     }
 
     public void registerRpcHook(RpcHook rpcHook) {
@@ -49,7 +59,7 @@ public class NettyDispatcher {
         rpcHooks.clear();
     }
 
-    public void dispatch(ChannelHandlerContext ctx, RpcCommand command) {
+    public void dispatch(RpcContext ctx, RpcCommand command) {
         if (command == null) {
             return;
         }
@@ -66,11 +76,58 @@ public class NettyDispatcher {
         }
     }
 
-    private void processRequest(ChannelHandlerContext ctx, RpcCommand command) {
+    private void illegalRequestCode(RpcContext ctx, RpcCommand command) {
 
     }
 
-    private void processResponse(ChannelHandlerContext ctx, RpcCommand command) {
+    private void rejectByServer(RpcContext ctx, RpcCommand command) {
+
+    }
+
+    private void rejectByBusiness(RpcContext ctx, RpcCommand command) {
+
+    }
+
+    private void flowControl(RpcContext ctx, RpcCommand command) {
+
+    }
+
+    private void requestFailed(RpcContext ctx, RpcCommand command, Throwable t) {
+
+    }
+
+    private RequestTask createRequestTask(RpcContext ctx, RpcCommand command, RpcProcessor processor) {
+        return null;
+    }
+
+    private void processRequest(RpcContext ctx, RpcCommand command) {
+        if (isShuttingDown.get()) {
+            rejectByServer(ctx, command);
+            return;
+        }
+
+        Pair<RpcProcessor, ExecutorService> processor = processorMap.get(command.getCode());
+        if (processor == null) {
+            illegalRequestCode(ctx, command);
+            return;
+        }
+
+        if (processor.getLeft().reject()) {
+            rejectByBusiness(ctx, command);
+            return;
+        }
+
+        try {
+            RequestTask task = createRequestTask(ctx, command, processor.getLeft());
+            processor.getRight().submit(task);
+        } catch (RejectedExecutionException e) {
+            flowControl(ctx, command);
+        } catch (Throwable t) {
+            requestFailed(ctx, command, t);
+        }
+    }
+
+    private void processResponse(RpcContext ctx, RpcCommand command) {
 
     }
 
