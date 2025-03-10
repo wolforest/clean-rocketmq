4eeed22a773d4c3a5e58d958538ec6529d1b51bb
winglechen@aliyun.com <winglechen@gmail.com>
2025-03-10 23:44:33 +0800

    feature: add empty methods to AddressInvoker

--- rpc/src/main/java/cn/coderule/minimq/rpc/common/netty/service/AddressInvoker.java
+++ rpc/src/main/java/cn/coderule/minimq/rpc/common/netty/service/AddressInvoker.java
@@ -1,4 +1,51 @@
 package cn.coderule.minimq.rpc.common.netty.service;
 
+import cn.coderule.minimq.rpc.common.config.RpcClientConfig;
+import cn.coderule.minimq.rpc.common.core.invoke.RpcCallback;
+import cn.coderule.minimq.rpc.common.core.invoke.RpcCommand;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import java.util.List;
+
 public class AddressInvoker {
+    private final RpcClientConfig config;
+    private final Bootstrap bootstrap;
+    private final ChannelInvoker channelInvoker;
+
+    public AddressInvoker(RpcClientConfig config, Bootstrap bootstrap, ChannelInvoker channelInvoker) {
+        this.config = config;
+        this.bootstrap = bootstrap;
+        this.channelInvoker = channelInvoker;
+    }
+
+    public boolean isChannelWritable(String addr) {
+        return false;
+    }
+
+    public boolean isAddressReachable(String addr) {
+        return false;
+    }
+
+    public void closeChannel(Channel channel) {
+
+    }
+
+    public void closeChannels(List<String> addrList) {
+
+    }
+
+    public RpcCommand invokeSync(String addr, RpcCommand request,
+        long timeoutMillis) throws Exception {
+        return null;
+    }
+
+    public void invokeAsync(String addr, RpcCommand request, long timeoutMillis,
+        RpcCallback invokeCallback) throws Exception {
+
+    }
+
+    public void invokeOneway(String addr, RpcCommand request, long timeoutMillis) throws Exception {
+
+    }
+
 }
