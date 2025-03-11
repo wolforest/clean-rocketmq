9bab4051ab14f4aa145b04c6afaf89121390dc47
winglechen@aliyun.com <winglechen@gmail.com>
2025-03-11 10:23:41 +0800

    feature: add constructor to AddressInvoker

--- rpc/src/main/java/cn/coderule/minimq/rpc/common/netty/service/AddressInvoker.java
+++ rpc/src/main/java/cn/coderule/minimq/rpc/common/netty/service/AddressInvoker.java
@@ -6,16 +6,38 @@ import cn.coderule.minimq.rpc.common.core.invoke.RpcCommand;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class AddressInvoker {
     private final RpcClientConfig config;
     private final Bootstrap bootstrap;
     private final ChannelInvoker channelInvoker;
 
+    private final Lock lock = new ReentrantLock();
+    private final ConcurrentMap<String /* addr */, ChannelWrapper> addressMap = new ConcurrentHashMap<>();
+    private final ConcurrentMap<Channel, ChannelWrapper> channelMap = new ConcurrentHashMap<>();
+
     public AddressInvoker(RpcClientConfig config, Bootstrap bootstrap, ChannelInvoker channelInvoker) {
         this.config = config;
         this.bootstrap = bootstrap;
         this.channelInvoker = channelInvoker;
+    }
+
+    public RpcCommand invokeSync(String addr, RpcCommand request,
+        long timeoutMillis) throws Exception {
+        return null;
+    }
+
+    public void invokeAsync(String addr, RpcCommand request, long timeoutMillis,
+        RpcCallback invokeCallback) throws Exception {
+
+    }
+
+    public void invokeOneway(String addr, RpcCommand request, long timeoutMillis) throws Exception {
+
     }
 
     public boolean isChannelWritable(String addr) {
@@ -31,20 +53,6 @@ public class AddressInvoker {
     }
 
     public void closeChannels(List<String> addrList) {
-
-    }
-
-    public RpcCommand invokeSync(String addr, RpcCommand request,
-        long timeoutMillis) throws Exception {
-        return null;
-    }
-
-    public void invokeAsync(String addr, RpcCommand request, long timeoutMillis,
-        RpcCallback invokeCallback) throws Exception {
-
-    }
-
-    public void invokeOneway(String addr, RpcCommand request, long timeoutMillis) throws Exception {
 
     }
 
