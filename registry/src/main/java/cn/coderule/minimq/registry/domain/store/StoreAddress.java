f7a8e5d3586da968428cce86138d69142249b68f
winglechen@aliyun.com <winglechen@gmail.com>
2025-03-13 13:28:09 +0800

    rename: rename BrokerAddrInfo to StoreAddress

--- registry/src/main/java/cn/coderule/minimq/registry/domain/store/StoreAddress.java
+++ registry/src/main/java/cn/coderule/minimq/registry/domain/store/StoreAddress.java
@@ -4,13 +4,13 @@ import java.io.Serializable;
 import lombok.Data;
 
 @Data
-public class BrokerAddrInfo implements Serializable {
+public class StoreAddress implements Serializable {
     private final String clusterName;
     private final String brokerAddr;
 
     private int hash;
 
-    public BrokerAddrInfo(String clusterName, String brokerAddr) {
+    public StoreAddress(String clusterName, String brokerAddr) {
         this.clusterName = clusterName;
         this.brokerAddr = brokerAddr;
     }
