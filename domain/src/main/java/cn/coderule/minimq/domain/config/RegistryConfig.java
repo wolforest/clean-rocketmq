a1767033d0f474570aae62b6f8553881f57edc72
winglechen@aliyun.com <winglechen@gmail.com>
2025-03-13 12:35:34 +0800

    feature: add some attibutes to RegistryConfig

--- domain/src/main/java/cn/coderule/minimq/domain/config/RegistryConfig.java
+++ domain/src/main/java/cn/coderule/minimq/domain/config/RegistryConfig.java
@@ -1,11 +1,18 @@
 package cn.coderule.minimq.domain.config;
 
-import cn.coderule.common.util.net.NetworkUtil;
 import java.io.Serializable;
 import lombok.Data;
 
 @Data
 public class RegistryConfig implements Serializable {
-    private String host = NetworkUtil.getLocalAddress();
-    private Integer port = 8081;
+    private String host = "0.0.0.0";
+    private Integer port = 9876;
+
+    private int bossThreadNum = 1;
+    private int workerThreadNum = 3;
+    private int businessThreadNum = 8;
+    private int callbackThreadNum = 0;
+
+    private int routeThreadNum = 8;
+    private int processThreadNum = 16;
 }
