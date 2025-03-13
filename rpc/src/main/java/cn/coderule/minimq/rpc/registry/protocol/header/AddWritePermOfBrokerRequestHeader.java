09d6de0702f7104409495478a94067a1ef98ceec
winglechen@aliyun.com <winglechen@gmail.com>
2025-03-13 16:23:44 +0800

    feature: add headers for registry

---
+++ rpc/src/main/java/cn/coderule/minimq/rpc/registry/protocol/header/AddWritePermOfBrokerRequestHeader.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cn.coderule.minimq.rpc.registry.protocol.header;
+
+import cn.coderule.minimq.rpc.common.core.annotation.CFNotNull;
+import cn.coderule.minimq.rpc.common.core.annotation.RocketMQAction;
+import cn.coderule.minimq.rpc.common.core.enums.Action;
+import cn.coderule.minimq.rpc.common.core.enums.ResourceType;
+import cn.coderule.minimq.rpc.common.core.exception.RemotingCommandException;
+import cn.coderule.minimq.rpc.common.protocol.code.RequestCode;
+import cn.coderule.minimq.rpc.common.protocol.header.CommandHeader;
+
+@RocketMQAction(value = RequestCode.ADD_WRITE_PERM_OF_BROKER, resource = ResourceType.CLUSTER, action = Action.UPDATE)
+public class AddWritePermOfBrokerRequestHeader implements CommandHeader {
+    @CFNotNull
+    private String brokerName;
+
+    @Override
+    public void checkFields() throws RemotingCommandException {
+
+    }
+
+    public String getBrokerName() {
+        return brokerName;
+    }
+
+    public void setBrokerName(String brokerName) {
+        this.brokerName = brokerName;
+    }
+}
