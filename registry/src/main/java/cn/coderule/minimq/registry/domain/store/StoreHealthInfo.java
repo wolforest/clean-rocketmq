9457124c4192caa3a25c32444ade09d95d6dd5a9
winglechen@aliyun.com <winglechen@gmail.com>
2025-03-13 13:24:46 +0800

    feature: add StoreHealthInfo

---
+++ registry/src/main/java/cn/coderule/minimq/registry/domain/store/StoreHealthInfo.java
@@ -0,0 +1,15 @@
+package cn.coderule.minimq.registry.domain.store;
+
+import cn.coderule.minimq.rpc.common.protocol.DataVersion;
+import io.netty.channel.Channel;
+import java.io.Serializable;
+import lombok.Data;
+
+@Data
+public class StoreHealthInfo implements Serializable {
+    private long lastUpdateTimestamp;
+    private long heartbeatTimeoutMillis;
+    private DataVersion dataVersion;
+    private Channel channel;
+    private String haServerAddr;
+}
