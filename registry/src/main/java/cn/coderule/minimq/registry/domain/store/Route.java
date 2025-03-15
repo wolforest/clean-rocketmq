86da7daf1ab64bf640e14e2c5abce36bcd1bbcfe
winglechen@aliyun.com <winglechen@gmail.com>
2025-03-15 09:50:38 +0800

    feature: add Route

---
+++ registry/src/main/java/cn/coderule/minimq/registry/domain/store/Route.java
@@ -0,0 +1,38 @@
+package cn.coderule.minimq.registry.domain.store;
+
+import cn.coderule.minimq.domain.model.Topic;
+import cn.coderule.minimq.rpc.registry.protocol.cluster.ClusterInfo;
+import cn.coderule.minimq.rpc.registry.protocol.cluster.GroupInfo;
+import cn.coderule.minimq.rpc.registry.protocol.cluster.StoreInfo;
+import cn.coderule.minimq.rpc.registry.protocol.route.QueueMap;
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import lombok.Data;
+
+@Data
+public class Route implements Serializable {
+    // topicMap: topicName -> groupName -> Topic
+    private final ConcurrentMap<String, Map<String, Topic>> topicMap;
+    // groupMap: groupName -> GroupInfo
+    private final ConcurrentMap<String, GroupInfo> groupMap;
+    // clusterMap: clusterName -> ClusterInfo
+    private final ConcurrentMap<String, ClusterInfo> clusterMap;
+    // healthMap: storeInfo -> StoreHealthInfo
+    private final ConcurrentMap<StoreInfo, StoreHealthInfo> healthMap;
+    // filterMap: storeInfo -> filterServerList
+    private final ConcurrentMap<StoreInfo, List<String>> filterMap;
+    // topicQueue: topicName -> groupName -> QueueMap
+    private final ConcurrentMap<String, Map<String, QueueMap>> topicQueueMap;
+
+    public Route() {
+        this.topicMap = new ConcurrentHashMap<>(1024);
+        this.groupMap = new ConcurrentHashMap<>(128);
+        this.clusterMap = new ConcurrentHashMap<>(32);
+        this.healthMap = new ConcurrentHashMap<>(256);
+        this.filterMap = new ConcurrentHashMap<>(256);
+        this.topicQueueMap = new ConcurrentHashMap<>(1024);
+    }
+}
---
+++ registry/src/main/java/cn/coderule/minimq/registry/domain/store/StoreManager.java
@@ -0,0 +1,27 @@
+package cn.coderule.minimq.registry.domain.store;
+
+import cn.coderule.minimq.domain.config.RegistryConfig;
+import cn.coderule.minimq.rpc.common.RpcClient;
+import cn.coderule.minimq.rpc.registry.protocol.body.StoreRegisterResult;
+import cn.coderule.minimq.rpc.registry.protocol.cluster.StoreInfo;
+import cn.coderule.minimq.rpc.registry.protocol.route.RouteInfo;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class StoreManager {
+    private final RegistryConfig config;
+    private final Route route;
+
+    private final RpcClient rpcClient;
+
+    public StoreManager(RegistryConfig config, RpcClient rpcClient, Route route) {
+        this.route = route;
+        this.config = config;
+        this.rpcClient = rpcClient;
+    }
+
+    public StoreRegisterResult registerStore(StoreInfo storeInfo, RouteInfo routeInfo, List<String> filterList) {
+        return null;
+    }
+}
