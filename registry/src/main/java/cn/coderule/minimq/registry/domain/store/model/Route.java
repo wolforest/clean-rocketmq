90facabc84bd887023edfad255f15fdd9d0fad5d
winglechen@aliyun.com <winglechen@gmail.com>
2025-03-15 11:00:16 +0800

    feature: add some group related methods to Route

--- registry/src/main/java/cn/coderule/minimq/registry/domain/store/model/Route.java
+++ registry/src/main/java/cn/coderule/minimq/registry/domain/store/model/Route.java
@@ -1,13 +1,15 @@
 package cn.coderule.minimq.registry.domain.store.model;
 
 import cn.coderule.minimq.domain.model.Topic;
-import cn.coderule.minimq.rpc.registry.protocol.cluster.ClusterInfo;
 import cn.coderule.minimq.rpc.registry.protocol.cluster.GroupInfo;
 import cn.coderule.minimq.rpc.registry.protocol.cluster.StoreInfo;
 import cn.coderule.minimq.rpc.registry.protocol.route.QueueMap;
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import lombok.Data;
@@ -18,8 +20,8 @@ public class Route implements Serializable {
     private final ConcurrentMap<String, Map<String, Topic>> topicMap;
     // groupMap: groupName -> GroupInfo
     private final ConcurrentMap<String, GroupInfo> groupMap;
-    // clusterMap: clusterName -> ClusterInfo
-    private final ConcurrentMap<String, ClusterInfo> clusterMap;
+    // clusterMap: clusterName -> groupName
+    private final ConcurrentMap<String, Set<String>> clusterMap;
     // healthMap: storeInfo -> StoreHealthInfo
     private final ConcurrentMap<StoreInfo, StoreHealthInfo> healthMap;
     // filterMap: storeInfo -> filterServerList
@@ -35,4 +37,44 @@ public class Route implements Serializable {
         this.filterMap = new ConcurrentHashMap<>(256);
         this.topicQueueMap = new ConcurrentHashMap<>(1024);
     }
+
+    public void addGroupToCluster(String clusterName, String groupName) {
+        Set<String> groupNames =this.clusterMap.computeIfAbsent(clusterName, k -> new HashSet<>());
+        groupNames.add(groupName);
+    }
+
+    public boolean existGroup(String groupName) {
+        return this.groupMap.containsKey(groupName);
+    }
+
+    public GroupInfo getGroup(String groupName) {
+        return this.groupMap.get(groupName);
+    }
+
+    /**
+     * get or create group
+     * - get GroupInfo if exists
+     * - create GroupInfo if not exists
+     *    - add groupName to clusterMap
+     *    - create GroupInfo
+     *    - add GroupInfo to groupMap
+     *
+     * @param clusterName clusterName
+     * @param groupName groupName
+     * @return groupInfo
+     */
+    public GroupInfo getOrCreateGroup(String clusterName, String groupName) {
+        GroupInfo groupInfo = this.groupMap.get(groupName);
+        if (groupInfo != null) {
+            return groupInfo;
+        }
+
+        addGroupToCluster(clusterName, groupName);
+
+        groupInfo = new GroupInfo(clusterName, groupName, new HashMap<>());
+        GroupInfo oldInfo = this.groupMap.putIfAbsent(groupName, groupInfo);
+
+        return oldInfo == null ? groupInfo : oldInfo;
+    }
+
 }
