e618ccd5f0a91d5484fa6a02faa6ffbd369a898e
winglechen@aliyun.com <winglechen@gmail.com>
2025-03-15 16:41:51 +0800

    feature: add topic related rpc classes

---
+++ rpc/src/main/java/cn/coderule/minimq/rpc/registry/protocol/body/TopicConfigSerializeWrapper.java
@@ -0,0 +1,45 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.coderule.minimq.rpc.registry.protocol.body;
+
+import cn.coderule.minimq.domain.model.Topic;
+import cn.coderule.minimq.rpc.common.protocol.DataVersion;
+import cn.coderule.minimq.rpc.common.protocol.codec.RpcSerializable;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+public class TopicConfigSerializeWrapper extends RpcSerializable {
+    private ConcurrentMap<String, Topic> topicConfigTable = new ConcurrentHashMap<>();
+    private DataVersion dataVersion = new DataVersion();
+
+    public ConcurrentMap<String, Topic> getTopicConfigTable() {
+        return topicConfigTable;
+    }
+
+    public void setTopicConfigTable(ConcurrentMap<String, Topic> topicConfigTable) {
+        this.topicConfigTable = topicConfigTable;
+    }
+
+    public DataVersion getDataVersion() {
+        return dataVersion;
+    }
+
+    public void setDataVersion(DataVersion dataVersion) {
+        this.dataVersion = dataVersion;
+    }
+}
