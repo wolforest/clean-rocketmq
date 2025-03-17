9cca64c2693e66d27425261648fff683ab05f6f1
winglechen@aliyun.com <winglechen@gmail.com>
2025-03-17 21:41:53 +0800

    feature: add method getTopicList to StoreRegistry

--- registry/src/main/java/cn/coderule/minimq/registry/domain/store/TopicService.java
+++ registry/src/main/java/cn/coderule/minimq/registry/domain/store/TopicService.java
@@ -4,6 +4,7 @@ import cn.coderule.common.util.lang.collection.CollectionUtil;
 import cn.coderule.minimq.domain.config.RegistryConfig;
 import cn.coderule.minimq.domain.model.Topic;
 import cn.coderule.minimq.registry.domain.store.model.Route;
+import cn.coderule.minimq.rpc.registry.protocol.body.TopicList;
 import java.util.Set;
 import lombok.extern.slf4j.Slf4j;
 
@@ -15,6 +16,21 @@ public class TopicService {
     public TopicService(RegistryConfig config, Route route) {
         this.route = route;
         this.config = config;
+    }
+
+    public TopicList getTopicList() {
+        TopicList topicList = new TopicList();
+
+        try {
+            route.lockRead();
+            topicList.setTopicList(route.getTopicMap().keySet());
+        } catch (Exception e) {
+            log.error("register topic error", e);
+        } finally {
+            route.unlockRead();
+        }
+
+        return topicList;
     }
 
     public void deleteTopic(String topicName) {
