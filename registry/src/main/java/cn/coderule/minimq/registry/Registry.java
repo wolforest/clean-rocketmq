5b8af7438fb6f43c5f6570015a65e5c26a22d2c5
winglechen@aliyun.com <winglechen@gmail.com>
2025-03-18 19:11:24 +0800

    feature: add method main to Registry

--- registry/src/main/java/cn/coderule/minimq/registry/Registry.java
+++ registry/src/main/java/cn/coderule/minimq/registry/Registry.java
@@ -1,4 +1,34 @@
 package cn.coderule.minimq.registry;
 
-public class Registry {
+import cn.coderule.common.convention.service.Lifecycle;
+
+public class Registry implements Lifecycle {
+    public static void main(String[] args) {
+        new Registry().start();
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void shutdown() {
+
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void cleanup() {
+
+    }
+
+    @Override
+    public State getState() {
+        return null;
+    }
 }
