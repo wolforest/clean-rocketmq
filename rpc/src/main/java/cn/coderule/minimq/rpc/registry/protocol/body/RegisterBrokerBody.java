4540b4b7136a5250dafa3ae1c74d5965e7800e53
winglechen@aliyun.com <winglechen@gmail.com>
2025-03-19 21:15:33 +0800

    feature: add GetBrokerMemberGroupResponseBody and RegisterBrokerBody

---
+++ rpc/src/main/java/cn/coderule/minimq/rpc/registry/protocol/body/RegisterBrokerBody.java
@@ -0,0 +1,223 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.coderule.minimq.rpc.registry.protocol.body;
+
+import cn.coderule.minimq.domain.constant.MQConstants;
+import cn.coderule.minimq.domain.model.Topic;
+import cn.coderule.minimq.rpc.common.protocol.DataVersion;
+import cn.coderule.minimq.rpc.common.protocol.codec.RpcSerializable;
+import cn.coderule.minimq.rpc.registry.protocol.statictopic.TopicQueueMappingInfo;
+import com.alibaba.fastjson2.JSON;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.zip.Deflater;
+import java.util.zip.DeflaterOutputStream;
+import java.util.zip.InflaterInputStream;
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+public class RegisterBrokerBody extends RpcSerializable {
+    private static final long MINIMUM_TAKE_TIME_MILLISECOND = 50;
+
+    private TopicConfigAndMappingSerializeWrapper topicConfigSerializeWrapper
+        = new TopicConfigAndMappingSerializeWrapper();
+
+    private List<String> filterServerList = new ArrayList<>();
+
+    public byte[] encode(boolean compress) {
+
+        if (!compress) {
+            return super.encode();
+        }
+        long start = System.currentTimeMillis();
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        DeflaterOutputStream outputStream = new DeflaterOutputStream(byteArrayOutputStream, new Deflater(Deflater.BEST_COMPRESSION));
+        DataVersion dataVersion = topicConfigSerializeWrapper.getDataVersion();
+        ConcurrentMap<String, Topic> topicConfigTable = cloneTopicConfigTable(topicConfigSerializeWrapper.getTopicConfigTable());
+        assert topicConfigTable != null;
+        try {
+            byte[] buffer = dataVersion.encode();
+
+            // write data version
+            outputStream.write(convertIntToByteArray(buffer.length));
+            outputStream.write(buffer);
+
+            int topicNumber = topicConfigTable.size();
+
+            // write number of topic configs
+            outputStream.write(convertIntToByteArray(topicNumber));
+
+            // write topic config entry one by one.
+            for (ConcurrentMap.Entry<String, Topic> next : topicConfigTable.entrySet()) {
+                buffer = next.getValue().encode().getBytes(MQConstants.DEFAULT_CHARSET);
+                outputStream.write(convertIntToByteArray(buffer.length));
+                outputStream.write(buffer);
+            }
+
+            buffer = JSON.toJSONString(filterServerList).getBytes(MQConstants.DEFAULT_CHARSET);
+
+            // write filter server list json length
+            outputStream.write(convertIntToByteArray(buffer.length));
+
+            // write filter server list json
+            outputStream.write(buffer);
+
+            //write the topic queue mapping
+            Map<String, TopicQueueMappingInfo> topicQueueMappingInfoMap = topicConfigSerializeWrapper.getTopicQueueMappingInfoMap();
+            if (topicQueueMappingInfoMap == null) {
+                //as the placeholder
+                topicQueueMappingInfoMap = new ConcurrentHashMap<>();
+            }
+            outputStream.write(convertIntToByteArray(topicQueueMappingInfoMap.size()));
+            for (TopicQueueMappingInfo info: topicQueueMappingInfoMap.values()) {
+                buffer = JSON.toJSONString(info).getBytes(MQConstants.DEFAULT_CHARSET);
+                outputStream.write(convertIntToByteArray(buffer.length));
+                // write filter server list json
+                outputStream.write(buffer);
+            }
+
+            outputStream.finish();
+            long takeTime = System.currentTimeMillis() - start;
+            if (takeTime > MINIMUM_TAKE_TIME_MILLISECOND) {
+                log.info("Compressing takes {}ms", takeTime);
+            }
+            return byteArrayOutputStream.toByteArray();
+        } catch (IOException e) {
+            log.error("Failed to compress RegisterBrokerBody object", e);
+        }
+
+        return null;
+    }
+
+    public static RegisterBrokerBody decode(byte[] data, boolean compressed) throws IOException {
+        if (!compressed) {
+            return RegisterBrokerBody.decode(data, RegisterBrokerBody.class);
+        }
+        long start = System.currentTimeMillis();
+        InflaterInputStream inflaterInputStream = new InflaterInputStream(new ByteArrayInputStream(data));
+        int dataVersionLength = readInt(inflaterInputStream);
+        byte[] dataVersionBytes = readBytes(inflaterInputStream, dataVersionLength);
+        DataVersion dataVersion = DataVersion.decode(dataVersionBytes, DataVersion.class);
+
+        RegisterBrokerBody registerBrokerBody = new RegisterBrokerBody();
+        registerBrokerBody.getTopicConfigSerializeWrapper().setDataVersion(dataVersion);
+        ConcurrentMap<String, Topic> topicConfigTable = registerBrokerBody.getTopicConfigSerializeWrapper().getTopicConfigTable();
+
+        int topicConfigNumber = readInt(inflaterInputStream);
+        log.debug("{} topic configs to extract", topicConfigNumber);
+
+        for (int i = 0; i < topicConfigNumber; i++) {
+            int topicConfigJsonLength = readInt(inflaterInputStream);
+
+            byte[] buffer = readBytes(inflaterInputStream, topicConfigJsonLength);
+            Topic topicConfig = new Topic();
+            String topicConfigJson = new String(buffer, MQConstants.DEFAULT_CHARSET);
+            topicConfig.decode(topicConfigJson);
+            topicConfigTable.put(topicConfig.getTopicName(), topicConfig);
+        }
+
+        int filterServerListJsonLength = readInt(inflaterInputStream);
+
+        byte[] filterServerListBuffer = readBytes(inflaterInputStream, filterServerListJsonLength);
+        String filterServerListJson = new String(filterServerListBuffer, MQConstants.DEFAULT_CHARSET);
+        List<String> filterServerList = new ArrayList<>();
+        try {
+            filterServerList = JSON.parseArray(filterServerListJson, String.class);
+        } catch (Exception e) {
+            log.error("Decompressing occur Exception {}", filterServerListJson);
+        }
+
+        registerBrokerBody.setFilterServerList(filterServerList);
+
+        int topicQueueMappingNum = readInt(inflaterInputStream);
+        Map<String/* topic */, TopicQueueMappingInfo> topicQueueMappingInfoMap = new ConcurrentHashMap<>();
+        for (int i = 0; i < topicQueueMappingNum; i++) {
+            int mappingJsonLen = readInt(inflaterInputStream);
+            byte[] buffer = readBytes(inflaterInputStream, mappingJsonLen);
+            TopicQueueMappingInfo info = TopicQueueMappingInfo.decode(buffer, TopicQueueMappingInfo.class);
+            topicQueueMappingInfoMap.put(info.getTopic(), info);
+        }
+        registerBrokerBody.getTopicConfigSerializeWrapper().setTopicQueueMappingInfoMap(topicQueueMappingInfoMap);
+
+        long takeTime = System.currentTimeMillis() - start;
+        if (takeTime > MINIMUM_TAKE_TIME_MILLISECOND) {
+            log.info("Decompressing takes {}ms", takeTime);
+        }
+        return registerBrokerBody;
+    }
+
+    private static byte[] convertIntToByteArray(int n) {
+        ByteBuffer byteBuffer = ByteBuffer.allocate(4);
+        byteBuffer.putInt(n);
+        return byteBuffer.array();
+    }
+
+    private static byte[] readBytes(InflaterInputStream inflaterInputStream, int length) throws IOException {
+        byte[] buffer = new byte[length];
+        int bytesRead = 0;
+        while (bytesRead < length) {
+            int len = inflaterInputStream.read(buffer, bytesRead, length - bytesRead);
+            if (len == -1) {
+                throw new IOException("End of compressed data has reached");
+            } else {
+                bytesRead += len;
+            }
+        }
+        return buffer;
+    }
+
+    private static int readInt(InflaterInputStream inflaterInputStream) throws IOException {
+        byte[] buffer = readBytes(inflaterInputStream, 4);
+        ByteBuffer byteBuffer = ByteBuffer.wrap(buffer);
+        return byteBuffer.getInt();
+    }
+
+    public TopicConfigAndMappingSerializeWrapper getTopicConfigSerializeWrapper() {
+        return topicConfigSerializeWrapper;
+    }
+
+    public void setTopicConfigSerializeWrapper(TopicConfigAndMappingSerializeWrapper topicConfigSerializeWrapper) {
+        this.topicConfigSerializeWrapper = topicConfigSerializeWrapper;
+    }
+
+    public List<String> getFilterServerList() {
+        return filterServerList;
+    }
+
+    public void setFilterServerList(List<String> filterServerList) {
+        this.filterServerList = filterServerList;
+    }
+
+    private ConcurrentMap<String, Topic> cloneTopicConfigTable(
+        ConcurrentMap<String, Topic> topicConfigConcurrentMap) {
+        if (topicConfigConcurrentMap == null) {
+            return null;
+        }
+        ConcurrentHashMap<String, Topic> result = new ConcurrentHashMap<>(topicConfigConcurrentMap.size());
+        result.putAll(topicConfigConcurrentMap);
+        return result;
+    }
+}
