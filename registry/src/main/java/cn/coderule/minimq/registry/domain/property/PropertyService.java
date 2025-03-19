233e0fb17b4fdb6157525bb76a4ab5c43a74d415
winglechen@aliyun.com <winglechen@gmail.com>
2025-03-19 11:09:14 +0800

    feature: impl method process of PropertyProcessor

--- registry/src/main/java/cn/coderule/minimq/registry/domain/property/PropertyService.java
+++ registry/src/main/java/cn/coderule/minimq/registry/domain/property/PropertyService.java
@@ -1,4 +1,19 @@
 package cn.coderule.minimq.registry.domain.property;
 
+import cn.coderule.minimq.domain.config.RegistryConfig;
+import cn.coderule.minimq.rpc.common.config.Configuration;
+import cn.coderule.minimq.rpc.common.config.RpcServerConfig;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class PropertyService {
+    private final Configuration configuration;
+    private final RegistryConfig registryConfig;
+    private final RpcServerConfig rpcServerConfig;
+
+    public PropertyService(RegistryConfig registryConfig, RpcServerConfig rpcServerConfig) {
+        this.registryConfig = registryConfig;
+        this.rpcServerConfig = rpcServerConfig;
+        this.configuration = new Configuration(registryConfig, rpcServerConfig);
+    }
 }
--- registry/src/main/java/cn/coderule/minimq/registry/processor/PropertyProcessor.java
+++ registry/src/main/java/cn/coderule/minimq/registry/processor/PropertyProcessor.java
@@ -1,19 +1,44 @@
 package cn.coderule.minimq.registry.processor;
 
+import cn.coderule.minimq.registry.domain.property.PropertyService;
 import cn.coderule.minimq.rpc.common.RpcProcessor;
 import cn.coderule.minimq.rpc.common.core.invoke.RpcCommand;
 import cn.coderule.minimq.rpc.common.core.invoke.RpcContext;
+import cn.coderule.minimq.rpc.common.protocol.code.RequestCode;
+import cn.coderule.minimq.rpc.common.protocol.code.SystemResponseCode;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class PropertyProcessor implements RpcProcessor {
+    private final PropertyService propertyService;
+
+    public PropertyProcessor(PropertyService propertyService) {
+        this.propertyService = propertyService;
+    }
+
     @Override
     public RpcCommand process(RpcContext ctx, RpcCommand request) {
-        return null;
+        return switch (request.getCode()) {
+            case RequestCode.UPDATE_NAMESRV_CONFIG -> this.setProperty(ctx, request);
+            case RequestCode.GET_NAMESRV_CONFIG -> this.getProperty(ctx, request);
+            default -> {
+                String error = " request type " + request.getCode() + " not supported";
+                yield RpcCommand.createResponseCommand(SystemResponseCode.REQUEST_CODE_NOT_SUPPORTED, error);
+            }
+        };
     }
 
     @Override
     public boolean reject() {
         return false;
     }
+
+    private RpcCommand getProperty(RpcContext ctx, RpcCommand request) {
+        return null;
+    }
+
+    private RpcCommand setProperty(RpcContext ctx, RpcCommand request) {
+        return null;
+    }
+
 }
