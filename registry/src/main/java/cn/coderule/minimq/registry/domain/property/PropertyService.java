b9677ca3708cb652b2ee0fa0e171d96eece24e73
winglechen@aliyun.com <winglechen@gmail.com>
2025-03-20 00:36:21 +0800

    refactor: delete uesless imports

--- registry/src/main/java/cn/coderule/minimq/registry/domain/property/PropertyService.java
+++ registry/src/main/java/cn/coderule/minimq/registry/domain/property/PropertyService.java
@@ -1,11 +1,8 @@
 package cn.coderule.minimq.registry.domain.property;
 
-import cn.coderule.common.util.lang.BeanUtil;
 import cn.coderule.minimq.domain.config.RegistryConfig;
 import cn.coderule.minimq.rpc.common.config.Configuration;
 import cn.coderule.minimq.rpc.common.config.RpcServerConfig;
-import cn.coderule.minimq.rpc.common.protocol.code.ResponseCode;
-import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Properties;
@@ -16,13 +13,11 @@ import lombok.extern.slf4j.Slf4j;
 public class PropertyService {
     private final Configuration configuration;
     private final RegistryConfig registryConfig;
-    private final RpcServerConfig rpcServerConfig;
 
     private final Set<String> configBlackList = new HashSet<>();
 
     public PropertyService(RegistryConfig registryConfig, RpcServerConfig rpcServerConfig) {
         this.registryConfig = registryConfig;
-        this.rpcServerConfig = rpcServerConfig;
         this.configuration = new Configuration(registryConfig, rpcServerConfig);
 
         initConfigBlackList();
