89705ce392224f111434f3633394d7618d229f10
winglechen@aliyun.com <winglechen@gmail.com>
2025-03-21 17:23:14 +0800

    feature: add empty RegistryServer

---
+++ registry/src/main/java/cn/coderule/minimq/registry/server/RegistryServer.java
@@ -0,0 +1,29 @@
+package cn.coderule.minimq.registry.server;
+
+import cn.coderule.common.convention.service.Lifecycle;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class RegistryServer implements Lifecycle {
+    @Override
+    public void initialize() {
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void shutdown() {
+
+    }
+
+
+
+    @Override
+    public void cleanup() {
+    }
+
+
+}
