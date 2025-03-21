43fae45790d0286c5ef18c089b28a8ba0318b5ef
winglechen@aliyun.com <winglechen@gmail.com>
2025-03-21 17:57:23 +0800

    feature: init RpcServerConfig and RpcClientConfig

--- registry/src/main/java/cn/coderule/minimq/registry/server/context/ConfigLoader.java
+++ registry/src/main/java/cn/coderule/minimq/registry/server/context/ConfigLoader.java
@@ -1,9 +1,23 @@
 package cn.coderule.minimq.registry.server.context;
 
 import cn.coderule.minimq.domain.config.RegistryConfig;
+import cn.coderule.minimq.rpc.common.config.RpcClientConfig;
+import cn.coderule.minimq.rpc.common.config.RpcServerConfig;
 
 public class ConfigLoader {
     public static void load() {
-        RegistryContext.register(new RegistryConfig());
+        RegistryConfig registryConfig = new RegistryConfig();
+        RegistryContext.register(registryConfig);
+
+        RpcServerConfig rpcServerConfig = new RpcServerConfig();
+        rpcServerConfig.setPort(registryConfig.getPort());
+        rpcServerConfig.setBossThreadNum(registryConfig.getBossThreadNum());
+        rpcServerConfig.setWorkerThreadNum(registryConfig.getWorkerThreadNum());
+        rpcServerConfig.setBusinessThreadNum(registryConfig.getBusinessThreadNum());
+        rpcServerConfig.setCallbackThreadNum(registryConfig.getCallbackThreadNum());
+        RegistryContext.register(rpcServerConfig);
+
+        RpcClientConfig rpcClientConfig = new RpcClientConfig();
+        RegistryContext.register(rpcClientConfig);
     }
 }
