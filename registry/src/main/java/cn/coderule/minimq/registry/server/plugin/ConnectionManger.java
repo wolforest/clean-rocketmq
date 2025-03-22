bd16458eb1353f0a7b218f5a2f25a53c47a56c05
winglechen@aliyun.com <winglechen@gmail.com>
2025-03-22 14:59:12 +0800

    feature: impl ConnectionManger

--- registry/src/main/java/cn/coderule/minimq/registry/server/plugin/ConnectionManger.java
+++ registry/src/main/java/cn/coderule/minimq/registry/server/plugin/ConnectionManger.java
@@ -1,9 +1,15 @@
 package cn.coderule.minimq.registry.server.plugin;
 
+import cn.coderule.minimq.registry.domain.store.service.ChannelCloser;
 import cn.coderule.minimq.rpc.common.RpcListener;
 import io.netty.channel.Channel;
 
 public class ConnectionManger implements RpcListener {
+    private final ChannelCloser channelCloser;
+
+    public ConnectionManger(ChannelCloser channelCloser) {
+        this.channelCloser = channelCloser;
+    }
 
     @Override
     public void onConnect(String address, Channel channel) {
@@ -12,17 +18,17 @@ public class ConnectionManger implements RpcListener {
 
     @Override
     public void onClose(String address, Channel channel) {
-
+        channelCloser.close(channel);
     }
 
     @Override
     public void onException(String address, Channel channel) {
-
+        channelCloser.close(channel);
     }
 
     @Override
     public void onIdle(String address, Channel channel) {
-
+        channelCloser.close(channel);
     }
 
     @Override
