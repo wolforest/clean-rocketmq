81256ff1376f8e135d2ca80be3f7c8042e8f7a02
winglechen@aliyun.com <winglechen@gmail.com>
2025-03-22 15:25:14 +0800

    feature: impl ZoneHook

--- registry/src/main/java/cn/coderule/minimq/registry/server/plugin/ZoneHook.java
+++ registry/src/main/java/cn/coderule/minimq/registry/server/plugin/ZoneHook.java
@@ -1,8 +1,21 @@
 package cn.coderule.minimq.registry.server.plugin;
 
+import cn.coderule.common.util.lang.StringUtil;
+import cn.coderule.common.util.lang.collection.MapUtil;
+import cn.coderule.minimq.domain.constant.MQConstants;
 import cn.coderule.minimq.rpc.common.RpcHook;
 import cn.coderule.minimq.rpc.common.core.invoke.RpcCommand;
 import cn.coderule.minimq.rpc.common.core.invoke.RpcContext;
+import cn.coderule.minimq.rpc.common.protocol.code.RequestCode;
+import cn.coderule.minimq.rpc.common.protocol.code.ResponseCode;
+import cn.coderule.minimq.rpc.common.protocol.codec.RpcSerializable;
+import cn.coderule.minimq.rpc.registry.protocol.cluster.GroupInfo;
+import cn.coderule.minimq.rpc.registry.protocol.route.QueueInfo;
+import cn.coderule.minimq.rpc.registry.protocol.route.RouteInfo;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ZoneHook implements RpcHook {
     @Override
@@ -12,6 +25,77 @@ public class ZoneHook implements RpcHook {
 
     @Override
     public void onResponseComplete(RpcContext ctx, RpcCommand request, RpcCommand response) {
+        if (RequestCode.GET_ROUTEINFO_BY_TOPIC != request.getCode()) {
+            return;
+        }
+        if (response == null || response.getBody() == null || ResponseCode.SUCCESS != response.getCode()) {
+            return;
+        }
+        boolean zoneMode = Boolean.parseBoolean(request.getExtFields().get(MQConstants.ZONE_MODE));
+        if (!zoneMode) {
+            return;
+        }
+        String zoneName = request.getExtFields().get(MQConstants.ZONE_NAME);
+        if (StringUtil.isBlank(zoneName)) {
+            return;
+        }
 
+        RouteInfo topicRouteData = RpcSerializable.decode(response.getBody(), RouteInfo.class);
+        response.setBody(filterByZone(topicRouteData, zoneName).encode());
     }
+
+    private RouteInfo filterByZone(RouteInfo topicRouteData, String zoneName) {
+        Map<String, GroupInfo> brokerDataRemoved = filterGroupInfo(topicRouteData, zoneName);
+
+        filterQueueInfo(topicRouteData, brokerDataRemoved);
+        filterFilterInfo(topicRouteData, brokerDataRemoved);
+
+        return topicRouteData;
+    }
+
+    private Map<String, GroupInfo> filterGroupInfo(RouteInfo topicRouteData, String zoneName) {
+        List<GroupInfo> groupInfoReserved = new ArrayList<>();
+        Map<String, GroupInfo> brokerDataRemoved = new HashMap<>();
+        for (GroupInfo groupInfo : topicRouteData.getBrokerDatas()) {
+            //master down, consume from slave. break nearby route rule.
+            if (groupInfo.getBrokerAddrs().get(MQConstants.MASTER_ID) == null
+                || StringUtil.equalsIgnoreCase(groupInfo.getZoneName(), zoneName)) {
+                groupInfoReserved.add(groupInfo);
+            } else {
+                brokerDataRemoved.put(groupInfo.getBrokerName(), groupInfo);
+            }
+        }
+        topicRouteData.setBrokerDatas(groupInfoReserved);
+
+        return brokerDataRemoved;
+    }
+
+    private void filterQueueInfo(RouteInfo topicRouteData, Map<String, GroupInfo> brokerDataRemoved) {
+        List<QueueInfo> queueDataReserved = new ArrayList<>();
+        for (QueueInfo queueData : topicRouteData.getQueueDatas()) {
+            if (!brokerDataRemoved.containsKey(queueData.getBrokerName())) {
+                queueDataReserved.add(queueData);
+            }
+        }
+        topicRouteData.setQueueDatas(queueDataReserved);
+    }
+
+    private void filterFilterInfo(RouteInfo routeInfo, Map<String, GroupInfo> groupRemoved) {
+        if (MapUtil.isEmpty(routeInfo.getFilterServerTable())) {
+            return;
+        }
+
+        // remove filter server table by broker address
+        for (Map.Entry<String, GroupInfo> entry : groupRemoved.entrySet()) {
+            GroupInfo groupInfo = entry.getValue();
+            if (groupInfo.getBrokerAddrs() == null) {
+                continue;
+            }
+            groupInfo.getBrokerAddrs().values()
+                .forEach(brokerAddr -> routeInfo.getFilterServerTable().remove(brokerAddr));
+        }
+    }
+
+
+
 }
