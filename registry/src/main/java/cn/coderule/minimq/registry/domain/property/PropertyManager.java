62451b558204be70eed018d271107c2f96188151
winglechen@aliyun.com <winglechen@gmail.com>
2025-03-23 11:20:50 +0800

    feature: add PropertyManager

---
+++ registry/src/main/java/cn/coderule/minimq/registry/domain/property/PropertyManager.java
@@ -0,0 +1,30 @@
+package cn.coderule.minimq.registry.domain.property;
+
+import cn.coderule.common.convention.service.Lifecycle;
+import cn.coderule.minimq.domain.config.RegistryConfig;
+import cn.coderule.minimq.registry.processor.PropertyProcessor;
+import cn.coderule.minimq.registry.server.ExecutorFactory;
+import cn.coderule.minimq.registry.server.context.RegistryContext;
+import cn.coderule.minimq.rpc.common.config.RpcServerConfig;
+
+public class PropertyManager implements Lifecycle {
+    @Override
+    public void initialize() {
+        PropertyService propertyService = new PropertyService(
+            RegistryContext.getBean(RegistryConfig.class),
+            RegistryContext.getBean(RpcServerConfig.class)
+        );
+
+        ExecutorFactory factory = RegistryContext.getBean(ExecutorFactory.class);
+        PropertyProcessor processor = new PropertyProcessor(propertyService, factory.getDefaultExecutor());
+
+        RegistryContext.register(propertyService);
+        RegistryContext.register(processor);
+    }
+
+    @Override
+    public void start() { }
+
+    @Override
+    public void shutdown() { }
+}
