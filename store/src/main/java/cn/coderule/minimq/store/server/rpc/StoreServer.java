f5bfc064706abc0f4ae83497427b4fbd48c2b010
winglechen@aliyun.com <winglechen@gmail.com>
2025-03-24 13:30:49 +0800

    feature: add StoreServer

---
+++ store/src/main/java/cn/coderule/minimq/store/server/rpc/StoreServer.java
@@ -0,0 +1,20 @@
+package cn.coderule.minimq.store.server.rpc;
+
+import cn.coderule.common.convention.service.Lifecycle;
+
+public class StoreServer implements Lifecycle {
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void shutdown() {
+
+    }
+
+    @Override
+    public void initialize() {
+        Lifecycle.super.initialize();
+    }
+}
