28303aa9ea7f9927f24e20b033eb6a09a759c153
winglechen@aliyun.com <winglechen@gmail.com>
2025-03-24 13:30:13 +0800

    feature: add BrokerServer

---
+++ broker/src/main/java/cn/coderule/minimq/broker/server/rpc/BrokerServer.java
@@ -0,0 +1,20 @@
+package cn.coderule.minimq.broker.server.rpc;
+
+import cn.coderule.common.convention.service.Lifecycle;
+
+public class BrokerServer implements Lifecycle {
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void shutdown() {
+
+    }
+
+    @Override
+    public void initialize() {
+        Lifecycle.super.initialize();
+    }
+}
