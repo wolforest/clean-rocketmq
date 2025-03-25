6a354440e4ae11105bce1f0ad804aa1d6fa2e38b
winglechen@aliyun.com <winglechen@gmail.com>
2025-03-25 16:32:46 +0800

    feature: add RpcManager

---
+++ store/src/main/java/cn/coderule/minimq/store/server/rpc/RpcManager.java
@@ -0,0 +1,22 @@
+package cn.coderule.minimq.store.server.rpc;
+
+import cn.coderule.common.convention.service.Lifecycle;
+
+public class RpcManager implements Lifecycle {
+    @Override
+    public void initialize() {
+        Lifecycle.super.initialize();
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void shutdown() {
+
+    }
+
+
+}
