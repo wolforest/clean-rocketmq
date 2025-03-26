bcb6f92b3a26d6fd1c5ca8317a4ef53d5f868d02
winglechen@aliyun.com <winglechen@gmail.com>
2025-03-26 15:32:05 +0800

    feature: add constructor to SubscriptionProcessor

--- store/src/main/java/cn/coderule/minimq/store/server/rpc/processor/SubscriptionProcessor.java
+++ store/src/main/java/cn/coderule/minimq/store/server/rpc/processor/SubscriptionProcessor.java
@@ -1,12 +1,31 @@
 package cn.coderule.minimq.store.server.rpc.processor;
 
+import cn.coderule.minimq.domain.service.store.api.SubscriptionStore;
 import cn.coderule.minimq.rpc.common.RpcProcessor;
 import cn.coderule.minimq.rpc.common.core.exception.RemotingCommandException;
 import cn.coderule.minimq.rpc.common.core.invoke.RpcCommand;
 import cn.coderule.minimq.rpc.common.core.invoke.RpcContext;
 import cn.coderule.minimq.rpc.common.protocol.code.RequestCode;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import lombok.Getter;
 
 public class SubscriptionProcessor implements RpcProcessor {
+    private final SubscriptionStore subscriptionStore;
+    @Getter
+    private final ExecutorService executor;
+    @Getter
+    private final Set<Integer> codeSet = Set.of(
+        RequestCode.GET_ALL_SUBSCRIPTIONGROUP_CONFIG,
+        RequestCode.DELETE_SUBSCRIPTIONGROUP,
+        RequestCode.UPDATE_AND_CREATE_SUBSCRIPTIONGROUP
+    );
+
+    public SubscriptionProcessor(SubscriptionStore subscriptionStore, ExecutorService executor) {
+        this.subscriptionStore = subscriptionStore;
+        this.executor = executor;
+    }
+
     @Override
     public RpcCommand process(RpcContext ctx, RpcCommand request) throws RemotingCommandException {
         return switch (request.getCode()) {
