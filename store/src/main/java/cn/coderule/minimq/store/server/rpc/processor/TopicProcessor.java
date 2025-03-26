ce36dd7b2a6d8c854ebaf8f243bd7b773fb7ae2a
winglechen@aliyun.com <winglechen@gmail.com>
2025-03-26 15:15:26 +0800

    feature: impl method process of TopicProcessor

--- store/src/main/java/cn/coderule/minimq/store/server/rpc/processor/TopicProcessor.java
+++ store/src/main/java/cn/coderule/minimq/store/server/rpc/processor/TopicProcessor.java
@@ -4,15 +4,39 @@ import cn.coderule.minimq.rpc.common.RpcProcessor;
 import cn.coderule.minimq.rpc.common.core.exception.RemotingCommandException;
 import cn.coderule.minimq.rpc.common.core.invoke.RpcCommand;
 import cn.coderule.minimq.rpc.common.core.invoke.RpcContext;
+import cn.coderule.minimq.rpc.common.protocol.code.RequestCode;
 
 public class TopicProcessor implements RpcProcessor {
     @Override
     public RpcCommand process(RpcContext ctx, RpcCommand request) throws RemotingCommandException {
-        return null;
+        return switch (request.getCode()) {
+            case RequestCode.GET_ALL_TOPIC_CONFIG -> this.getTopic(ctx, request);
+            case RequestCode.DELETE_TOPIC_IN_BROKER -> this.deleteTopic(ctx, request);
+            case RequestCode.UPDATE_AND_CREATE_TOPIC -> this.saveTopic(ctx, request);
+            default -> this.unsupportedCode(ctx, request);
+        };
     }
 
     @Override
     public boolean reject() {
         return false;
+    }
+
+    private RpcCommand saveTopic(RpcContext ctx, RpcCommand request) throws RemotingCommandException {
+        RpcCommand response = RpcCommand.createResponseCommand(null);
+
+        return response.success();
+    }
+
+    private RpcCommand getTopic(RpcContext ctx, RpcCommand request) throws RemotingCommandException {
+        RpcCommand response = RpcCommand.createResponseCommand(null);
+
+        return response.success();
+    }
+
+    private RpcCommand deleteTopic(RpcContext ctx, RpcCommand request) throws RemotingCommandException {
+        RpcCommand response = RpcCommand.createResponseCommand(null);
+
+        return response.success();
     }
 }
