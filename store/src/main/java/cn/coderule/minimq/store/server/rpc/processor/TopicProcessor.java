8a0376860e93d543b86585e7bb0e24fc53ed75ec
winglechen@aliyun.com <winglechen@gmail.com>
2025-03-26 15:29:34 +0800

    feature: add constructor to TopicProcessor

--- store/src/main/java/cn/coderule/minimq/store/server/rpc/processor/TopicProcessor.java
+++ store/src/main/java/cn/coderule/minimq/store/server/rpc/processor/TopicProcessor.java
@@ -1,12 +1,32 @@
 package cn.coderule.minimq.store.server.rpc.processor;
 
+import cn.coderule.minimq.domain.service.store.api.TopicStore;
 import cn.coderule.minimq.rpc.common.RpcProcessor;
 import cn.coderule.minimq.rpc.common.core.exception.RemotingCommandException;
 import cn.coderule.minimq.rpc.common.core.invoke.RpcCommand;
 import cn.coderule.minimq.rpc.common.core.invoke.RpcContext;
 import cn.coderule.minimq.rpc.common.protocol.code.RequestCode;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import lombok.Getter;
 
 public class TopicProcessor implements RpcProcessor {
+    private final TopicStore topicStore;
+
+    @Getter
+    private final ExecutorService executor;
+    @Getter
+    private final Set<Integer> codeSet = Set.of(
+        RequestCode.GET_ALL_TOPIC_CONFIG,
+        RequestCode.DELETE_TOPIC_IN_BROKER,
+        RequestCode.UPDATE_AND_CREATE_TOPIC
+    );
+
+    public TopicProcessor(TopicStore topicStore, ExecutorService executor) {
+        this.topicStore = topicStore;
+        this.executor = executor;
+    }
+
     @Override
     public RpcCommand process(RpcContext ctx, RpcCommand request) throws RemotingCommandException {
         return switch (request.getCode()) {
