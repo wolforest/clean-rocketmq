21f20bfff6e7fb001bf90de441427381f415f36b
winglechen@aliyun.com <winglechen@gmail.com>
2025-03-31 09:38:30 +0800

    feature: add SystemGroupRegister

---
+++ store/src/main/java/cn/coderule/minimq/store/domain/meta/SystemGroupRegister.java
@@ -0,0 +1,78 @@
+package cn.coderule.minimq.store.domain.meta;
+
+import cn.coderule.minimq.domain.constant.MQConstants;
+import cn.coderule.minimq.domain.model.subscription.SubscriptionGroup;
+import cn.coderule.minimq.domain.service.store.domain.meta.SubscriptionService;
+
+public class SystemGroupRegister {
+    private final SubscriptionService subscriptionService;
+
+    public SystemGroupRegister(SubscriptionService subscriptionService) {
+        this.subscriptionService = subscriptionService;
+    }
+
+    private void register() {
+        registerToolsConsumerGroup();
+        registerFilterConsumerGroup();
+        registerSelfTestConsumerGroup();
+        registerOnsHttpProxyGroup();
+        registerCidONSAPIPullGroup();
+        registerCidONSAPIPermissionGroup();
+        registerCidONSAPIOwnerGroup();
+        registerCidSYSTransGroup();
+    }
+
+    private void registerToolsConsumerGroup() {
+        SubscriptionGroup group = new SubscriptionGroup();
+        group.setGroupName(MQConstants.TOOLS_CONSUMER_GROUP);
+        subscriptionService.putGroup(group);
+    }
+
+    private void registerFilterConsumerGroup() {
+        SubscriptionGroup group = new SubscriptionGroup();
+        group.setGroupName(MQConstants.FILTERSRV_CONSUMER_GROUP);
+        subscriptionService.putGroup(group);
+    }
+
+    private void registerSelfTestConsumerGroup() {
+        SubscriptionGroup group = new SubscriptionGroup();
+        group.setGroupName(MQConstants.SELF_TEST_CONSUMER_GROUP);
+        subscriptionService.putGroup(group);
+    }
+
+    private void registerOnsHttpProxyGroup() {
+        SubscriptionGroup group = new SubscriptionGroup();
+        group.setGroupName(MQConstants.ONS_HTTP_PROXY_GROUP);
+        group.setConsumeBroadcastEnable(true);
+        subscriptionService.putGroup(group);
+    }
+
+    private void registerCidONSAPIPullGroup() {
+        SubscriptionGroup group = new SubscriptionGroup();
+        group.setGroupName(MQConstants.CID_ONSAPI_PULL_GROUP);
+        group.setConsumeBroadcastEnable(true);
+        subscriptionService.putGroup(group);
+    }
+
+    private void registerCidONSAPIPermissionGroup() {
+        SubscriptionGroup group = new SubscriptionGroup();
+        group.setGroupName(MQConstants.CID_ONSAPI_PERMISSION_GROUP);
+        group.setConsumeBroadcastEnable(true);
+        subscriptionService.putGroup(group);
+    }
+
+    private void registerCidONSAPIOwnerGroup() {
+        SubscriptionGroup group = new SubscriptionGroup();
+        group.setGroupName(MQConstants.CID_ONSAPI_OWNER_GROUP);
+        group.setConsumeBroadcastEnable(true);
+        subscriptionService.putGroup(group);
+    }
+
+    public void registerCidSYSTransGroup() {
+        SubscriptionGroup group = new SubscriptionGroup();
+        group.setGroupName(MQConstants.CID_SYS_RMQ_TRANS);
+        group.setConsumeBroadcastEnable(true);
+        subscriptionService.putGroup(group);
+    }
+
+}
