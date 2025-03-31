24c8f10dd4870f72f72e2ec19c672dc4f3e24abd
winglechen@aliyun.com <winglechen@gmail.com>
2025-03-31 15:58:39 +0800

    feature: impl init/start/shutdown of DefaultMetaManager

--- store/src/main/java/cn/coderule/minimq/store/domain/meta/DefaultMetaManager.java
+++ store/src/main/java/cn/coderule/minimq/store/domain/meta/DefaultMetaManager.java
@@ -1,20 +1,28 @@
 package cn.coderule.minimq.store.domain.meta;
 
+import cn.coderule.minimq.domain.service.store.api.SubscriptionStore;
 import cn.coderule.minimq.domain.service.store.api.TopicStore;
 import cn.coderule.minimq.domain.service.store.domain.ConsumeQueueGateway;
 import cn.coderule.minimq.domain.service.store.domain.meta.ConsumeOffsetService;
+import cn.coderule.minimq.domain.service.store.domain.meta.SubscriptionService;
+import cn.coderule.minimq.domain.service.store.domain.meta.TopicService;
 import cn.coderule.minimq.domain.service.store.manager.MetaManager;
+import cn.coderule.minimq.store.api.SubscriptionStoreImpl;
 import cn.coderule.minimq.store.api.TopicStoreImpl;
 import cn.coderule.minimq.store.server.StoreContext;
 import cn.coderule.minimq.store.server.bootstrap.StorePath;
 import cn.coderule.minimq.store.infra.StoreRegister;
 
 public class DefaultMetaManager implements MetaManager {
+    private ConsumeOffsetService offsetService;
     private DefaultTopicService topicService;
+    private SubscriptionService subscriptionService;
 
     @Override
     public void initialize() {
+        initConsumerOffset();
         initTopic();
+        initSubscription();
     }
 
     @Override
@@ -24,7 +32,9 @@ public class DefaultMetaManager implements MetaManager {
 
     @Override
     public void shutdown() {
-
+        offsetService.store();
+        topicService.store();
+        subscriptionService.store();
     }
 
     private void injectDependency() {
@@ -34,16 +44,28 @@ public class DefaultMetaManager implements MetaManager {
         topicService.inject(consumeQueueGateway, storeRegister);
     }
 
+    private void initConsumerOffset() {
+        offsetService = new DefaultConsumeOffsetService(StorePath.getConsumerOffsetPath());
+        StoreContext.register(offsetService, ConsumeOffsetService.class);
+    }
+
     private void initTopic() {
-        ConsumeOffsetService offsetService = StoreContext.getBean(ConsumeOffsetService.class);
         topicService = new DefaultTopicService(StorePath.getTopicPath(), offsetService);
+        StoreContext.register(topicService, TopicService.class);
+
         topicService.load();
 
         TopicStore topicApi = new TopicStoreImpl(topicService);
         StoreContext.registerAPI(topicApi, TopicStore.class);
     }
 
-    private void initConsumerOffset() {
-        ConsumeOffsetService consumeOffsetStore = new DefaultConsumeOffsetService(StorePath.getConsumerOffsetPath());
+    private void initSubscription() {
+        String storePath = StorePath.getSubscriptionGroupPath();
+        subscriptionService = new DefaultSubscriptionService(storePath, offsetService);
+        StoreContext.register(subscriptionService, SubscriptionService.class);
+
+        subscriptionService.load();
+
+        //SubscriptionStore subscriptionApi = new SubscriptionStoreImpl(subscriptionService);
     }
 }
