3ce21a6f5e1ff918675aba4b99de641d1066c445
winglechen@aliyun.com <winglechen@gmail.com>
2025-04-12 11:47:44 +0800

    feature: add constructor and lock operations to RouteCache

--- broker/src/main/java/cn/coderule/minimq/broker/domain/meta/RouteCache.java
+++ broker/src/main/java/cn/coderule/minimq/broker/domain/meta/RouteCache.java
@@ -5,17 +5,49 @@ import cn.coderule.minimq.rpc.registry.protocol.route.RouteInfo;
 import java.io.Serializable;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 
 @Data
+@Slf4j
 public class RouteCache implements Serializable {
+    private static final long LOCK_TIMEOUT = 3000L;
+
+    private final Lock lock;
+
     // topicName -> routeInfo
     private final ConcurrentMap<String, RouteInfo> routeMap;
     // groupName -> groupNo -> address
     private final ConcurrentMap<String, Map<Long, String>> addressMap;
     // topicName -> Set<messageQueue>
     private final ConcurrentMap<String, Set<MessageQueue>> queueMap;
+    // topicName -> publishInfo
+    private final ConcurrentMap<String, PublishInfo> publishMap;
 
 
+    public RouteCache() {
+        this.routeMap = new ConcurrentHashMap<>();
+        this.addressMap = new ConcurrentHashMap<>();
+        this.queueMap = new ConcurrentHashMap<>();
+        this.publishMap = new ConcurrentHashMap<>();
+
+        this.lock = new ReentrantLock();
+    }
+
+    public boolean tryLock() {
+        try {
+            return lock.tryLock(LOCK_TIMEOUT, java.util.concurrent.TimeUnit.MILLISECONDS);
+        } catch (InterruptedException e) {
+            log.warn("RouteCache tryLock exception: ", e);
+            return false;
+        }
+    }
+
+    public void unlock() {
+        lock.unlock();
+    }
 }
