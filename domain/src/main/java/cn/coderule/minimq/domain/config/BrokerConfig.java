b3dbffaa7b9d2efb36576fea241acf0afc89cd3f
winglechen@aliyun.com <winglechen@gmail.com>
2025-04-14 21:17:55 +0800

    feature: add enableEmbedStore and enableRemoteStore to BrokerConfig

--- domain/src/main/java/cn/coderule/minimq/domain/config/BrokerConfig.java
+++ domain/src/main/java/cn/coderule/minimq/domain/config/BrokerConfig.java
@@ -4,17 +4,14 @@ import cn.coderule.common.util.lang.SystemUtil;
 import cn.coderule.common.util.net.NetworkUtil;
 import java.io.Serializable;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 
 @Data
 public class BrokerConfig implements Serializable {
     private String host = NetworkUtil.getLocalAddress();
     private Integer port = 8080;
 
-    /**
-     * should be false in production env
-     */
-    private boolean enableLocalStore = true;
+    private boolean enableEmbedStore = true;
+    private boolean enableRemoteStore = true;
 
     private int producerThreadNum = SystemUtil.getProcessorNumber();
     private int producerQueueCapacity = 10000;
