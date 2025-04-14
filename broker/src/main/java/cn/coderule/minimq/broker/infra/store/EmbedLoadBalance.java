91255f06a8a3e1b6744194725cb2d0fabfdcfc61
winglechen@aliyun.com <winglechen@gmail.com>
2025-04-14 19:33:03 +0800

    feature: add constructor to EmbedLoadBalance and RemoteLoadBalance

--- broker/src/main/java/cn/coderule/minimq/broker/infra/store/EmbedLoadBalance.java
+++ broker/src/main/java/cn/coderule/minimq/broker/infra/store/EmbedLoadBalance.java
@@ -1,4 +1,25 @@
 package cn.coderule.minimq.broker.infra.store;
 
+import cn.coderule.minimq.broker.infra.embed.EmbedSubscriptionStore;
+import cn.coderule.minimq.broker.infra.embed.EmbedTopicStore;
+import cn.coderule.minimq.domain.config.BrokerConfig;
+
 public class EmbedLoadBalance {
+    private final BrokerConfig brokerConfig;
+    private final EmbedTopicStore topicStore;
+    private final EmbedSubscriptionStore subscriptionStore;
+
+    public EmbedLoadBalance(BrokerConfig brokerConfig, EmbedTopicStore topicStore, EmbedSubscriptionStore subscriptionStore) {
+        this.brokerConfig = brokerConfig;
+        this.topicStore = topicStore;
+        this.subscriptionStore = subscriptionStore;
+    }
+
+    public boolean containsTopic(String topicName) {
+        return false;
+    }
+
+    public boolean containsSubscription(String topicName) {
+        return false;
+    }
 }
--- broker/src/main/java/cn/coderule/minimq/broker/infra/store/RemoteLoadBalance.java
+++ broker/src/main/java/cn/coderule/minimq/broker/infra/store/RemoteLoadBalance.java
@@ -1,4 +1,23 @@
 package cn.coderule.minimq.broker.infra.store;
 
+import cn.coderule.minimq.broker.infra.route.RouteLoader;
+import cn.coderule.minimq.domain.config.BrokerConfig;
+
 public class RemoteLoadBalance {
+    private final BrokerConfig brokerConfig;
+    private final RouteLoader routeLoader;
+
+    public RemoteLoadBalance(BrokerConfig brokerConfig, RouteLoader routeLoader) {
+        this.brokerConfig = brokerConfig;
+        this.routeLoader = routeLoader;
+    }
+
+    public String getServerAddress(String topicName) {
+        return null;
+    }
+
+    public String getServerAddress(String topicName, int queueId) {
+        return null;
+    }
+
 }
