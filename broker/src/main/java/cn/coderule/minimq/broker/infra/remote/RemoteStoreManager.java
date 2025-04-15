7d1156a43fa13f84d77671e3bf6b47fa5c290839
winglechen@aliyun.com <winglechen@gmail.com>
2025-04-15 10:05:28 +0800

    feature: add RemoteStoreManager

---
+++ broker/src/main/java/cn/coderule/minimq/broker/infra/remote/RemoteStoreManager.java
@@ -0,0 +1,38 @@
+package cn.coderule.minimq.broker.infra.remote;
+
+import cn.coderule.common.convention.service.Lifecycle;
+import cn.coderule.minimq.broker.infra.route.RouteLoader;
+import cn.coderule.minimq.broker.server.BrokerContext;
+import cn.coderule.minimq.domain.config.BrokerConfig;
+
+public class RemoteStoreManager implements Lifecycle {
+    private BrokerConfig brokerConfig;
+    private RemoteLoadBalance loadBalance;
+
+    @Override
+    public void initialize() {
+        this.brokerConfig = BrokerContext.getBean(BrokerConfig.class);
+        if (!this.brokerConfig.isEnableRemoteStore()) {
+            return;
+        }
+
+        RouteLoader routeLoader = BrokerContext.getBean(RouteLoader.class);
+        this.loadBalance = new RemoteLoadBalance(this.brokerConfig, routeLoader);
+
+    }
+
+    @Override
+    public void start() {
+        if (!brokerConfig.isEnableRemoteStore()) {
+            return;
+        }
+    }
+
+    @Override
+    public void shutdown() {
+        if (!brokerConfig.isEnableRemoteStore()) {
+            return;
+        }
+    }
+
+}
