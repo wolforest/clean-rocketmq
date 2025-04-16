0c023cc3f95d892da8aaa4a7375131fc03daa2bc
winglechen@aliyun.com <winglechen@gmail.com>
2025-04-16 08:23:29 +0800

    feature: add StoreManager

---
+++ broker/src/main/java/cn/coderule/minimq/broker/infra/store/StoreManager.java
@@ -0,0 +1,20 @@
+package cn.coderule.minimq.broker.infra.store;
+
+import cn.coderule.common.convention.service.Lifecycle;
+
+public class StoreManager implements Lifecycle {
+    @Override
+    public void initialize() {
+        Lifecycle.super.initialize();
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void shutdown() {
+
+    }
+}
