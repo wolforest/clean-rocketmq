0b14362afa45b1ea5b43c29af27f6ff4d6de44e1
winglechen@aliyun.com <winglechen@gmail.com>
2025-04-16 08:08:20 +0800

    feature: impl EmbedMessageStore

--- broker/src/main/java/cn/coderule/minimq/broker/infra/embed/EmbedMessageStore.java
+++ broker/src/main/java/cn/coderule/minimq/broker/infra/embed/EmbedMessageStore.java
@@ -4,47 +4,50 @@ import cn.coderule.minimq.domain.domain.model.message.MessageBO;
 import cn.coderule.minimq.domain.domain.dto.EnqueueResult;
 import cn.coderule.minimq.domain.domain.dto.GetRequest;
 import cn.coderule.minimq.domain.domain.dto.GetResult;
+import cn.coderule.minimq.domain.service.store.api.MessageStore;
 import cn.coderule.minimq.domain.service.store.domain.MessageQueue;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public class EmbedMessageStore extends AbstractEmbedStore implements MessageQueue {
-    public EmbedMessageStore(EmbedLoadBalance loadBalance) {
+    private final MessageStore messageStore;
+    public EmbedMessageStore(MessageStore messageStore, EmbedLoadBalance loadBalance) {
         super(loadBalance);
+        this.messageStore = messageStore;
     }
 
     @Override
     public EnqueueResult enqueue(MessageBO messageBO) {
-        return null;
+        return messageStore.enqueue(messageBO);
     }
 
     @Override
     public CompletableFuture<EnqueueResult> enqueueAsync(MessageBO messageBO) {
-        return null;
+        return messageStore.enqueueAsync(messageBO);
     }
 
     @Override
     public GetResult get(String topic, int queueId, long offset) {
-        return null;
+        return messageStore.get(topic, queueId, offset);
     }
 
     @Override
     public GetResult get(String topic, int queueId, long offset, int num) {
-        return null;
+        return messageStore.get(topic, queueId, offset, num);
     }
 
     @Override
     public GetResult get(GetRequest request) {
-        return null;
+        return messageStore.get(request);
     }
 
     @Override
     public MessageBO getMessage(String topic, int queueId, long offset) {
-        return null;
+        return messageStore.getMessage(topic, queueId, offset);
     }
 
     @Override
     public List<MessageBO> getMessage(String topic, int queueId, long offset, int num) {
-        return List.of();
+        return messageStore.getMessage(topic, queueId, offset, num);
     }
 }
