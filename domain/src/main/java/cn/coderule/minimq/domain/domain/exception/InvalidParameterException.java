13c5d14c83a4c5b9575c2f922af32c9aeeb9481f
winglechen@aliyun.com <winglechen@gmail.com>
2025-04-19 22:57:30 +0800

    feature: add InvalidParameterException

---
+++ domain/src/main/java/cn/coderule/minimq/domain/domain/exception/InvalidParameterException.java
@@ -0,0 +1,20 @@
+package cn.coderule.minimq.domain.domain.exception;
+
+import cn.coderule.common.lang.exception.BusinessException;
+import cn.coderule.minimq.domain.domain.enums.InvalidCode;
+import lombok.Getter;
+
+@Getter
+public class InvalidParameterException extends BusinessException {
+    private static final String DEFAULT_MESSAGE = "Invalid parameter";
+    private final InvalidCode invalidCode;
+
+    public InvalidParameterException(InvalidCode code) {
+        this(code, DEFAULT_MESSAGE);
+    }
+
+    public InvalidParameterException(InvalidCode code, String message) {
+        super(500, message);
+        this.invalidCode = code;
+    }
+}
