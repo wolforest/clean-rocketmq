59f885ce65de482bacf468eecf559b7312e78f36
winglechen@aliyun.com <winglechen@gmail.com>
2025-04-23 15:19:53 +0800

    feature: change interface of EmbedTopicStore and EmbedSubscriptionStore

--- broker/src/main/java/cn/coderule/minimq/broker/infra/embed/EmbedSubscriptionStore.java
+++ broker/src/main/java/cn/coderule/minimq/broker/infra/embed/EmbedSubscriptionStore.java
@@ -1,26 +1,8 @@
 package cn.coderule.minimq.broker.infra.embed;
 
-import cn.coderule.minimq.domain.domain.model.subscription.SubscriptionGroup;
-import cn.coderule.minimq.domain.service.store.api.SubscriptionStore;
+public class EmbedSubscriptionStore extends AbstractEmbedStore  {
 
-public class EmbedSubscriptionStore implements SubscriptionStore {
-    @Override
-    public boolean existsGroup(String groupName) {
-        return false;
-    }
-
-    @Override
-    public SubscriptionGroup getGroup(String groupName) {
-        return null;
-    }
-
-    @Override
-    public void saveGroup(SubscriptionGroup group) {
-
-    }
-
-    @Override
-    public void deleteGroup(String groupName, boolean cleanOffset) {
-
+    public EmbedSubscriptionStore(EmbedLoadBalance loadBalance) {
+        super(loadBalance);
     }
 }
--- broker/src/main/java/cn/coderule/minimq/broker/infra/embed/EmbedTopicStore.java
+++ broker/src/main/java/cn/coderule/minimq/broker/infra/embed/EmbedTopicStore.java
@@ -1,26 +1,9 @@
 package cn.coderule.minimq.broker.infra.embed;
 
-import cn.coderule.minimq.domain.domain.model.Topic;
-import cn.coderule.minimq.domain.service.store.api.TopicStore;
 
-public class EmbedTopicStore implements TopicStore {
-    @Override
-    public boolean exists(String topicName) {
-        return false;
-    }
+public class EmbedTopicStore extends AbstractEmbedStore  {
 
-    @Override
-    public Topic getTopic(String topicName) {
-        return null;
-    }
-
-    @Override
-    public void saveTopic(Topic topic) {
-
-    }
-
-    @Override
-    public void deleteTopic(String topicName) {
-
+    public EmbedTopicStore(EmbedLoadBalance loadBalance) {
+        super(loadBalance);
     }
 }
