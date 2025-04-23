4930aed1cb89476b8ff490d81d2474c2af856090
winglechen@aliyun.com <winglechen@gmail.com>
2025-04-23 20:29:44 +0800

    feature: add MessageValidator

---
+++ broker/src/main/java/cn/coderule/minimq/broker/api/validator/MessageValidator.java
@@ -0,0 +1,9 @@
+package cn.coderule.minimq.broker.api.validator;
+
+import cn.coderule.minimq.domain.domain.model.message.MessageBO;
+
+public class MessageValidator {
+    public static void validate(MessageBO messageBO) {
+
+    }
+}
