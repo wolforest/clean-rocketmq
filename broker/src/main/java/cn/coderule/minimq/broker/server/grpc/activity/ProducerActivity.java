46e505a6539074cc72c2637c4f22ffd7ace1a04b
winglechen@aliyun.com <winglechen@gmail.com>
2025-04-24 15:24:06 +0800

    style: delete some useless import

--- broker/src/main/java/cn/coderule/minimq/broker/server/grpc/activity/ProducerActivity.java
+++ broker/src/main/java/cn/coderule/minimq/broker/server/grpc/activity/ProducerActivity.java
@@ -1,43 +1,24 @@
 package cn.coderule.minimq.broker.server.grpc.activity;
 
-import apache.rocketmq.v2.Code;
-import apache.rocketmq.v2.Encoding;
 import apache.rocketmq.v2.ForwardMessageToDeadLetterQueueRequest;
 import apache.rocketmq.v2.ForwardMessageToDeadLetterQueueResponse;
-import apache.rocketmq.v2.Message;
-import apache.rocketmq.v2.MessageType;
-import apache.rocketmq.v2.QueryAssignmentRequest;
-import apache.rocketmq.v2.QueryAssignmentResponse;
-import apache.rocketmq.v2.QueryRouteRequest;
-import apache.rocketmq.v2.QueryRouteResponse;
 import apache.rocketmq.v2.SendMessageRequest;
 import apache.rocketmq.v2.SendMessageResponse;
-import apache.rocketmq.v2.SendResultEntry;
 import apache.rocketmq.v2.Status;
-import cn.coderule.common.util.lang.StringUtil;
 import cn.coderule.minimq.broker.api.ProducerController;
 import cn.coderule.minimq.broker.server.grpc.converter.MessageConverter;
 import cn.coderule.minimq.broker.server.grpc.converter.ProducerConverter;
-import cn.coderule.minimq.domain.domain.constant.MessageConst;
-import cn.coderule.minimq.domain.domain.constant.flag.MessageSysFlag;
-import cn.coderule.minimq.domain.domain.dto.EnqueueResult;
 import cn.coderule.minimq.domain.domain.enums.InvalidCode;
 import cn.coderule.minimq.rpc.common.core.RequestContext;
 import cn.coderule.minimq.domain.domain.model.message.MessageBO;
 import cn.coderule.minimq.rpc.common.grpc.activity.ActivityHelper;
-import cn.coderule.minimq.rpc.common.grpc.core.ResponseBuilder;
 import cn.coderule.minimq.rpc.common.grpc.core.exception.GrpcException;
 import io.grpc.stub.StreamObserver;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.function.Function;
 import lombok.Setter;
-import org.apache.commons.lang3.StringUtils;
 
 public class ProducerActivity {
     private final ThreadPoolExecutor executor;
@@ -56,12 +37,13 @@ public class ProducerActivity {
     }
 
     public void produce(RequestContext context, SendMessageRequest request, StreamObserver<SendMessageResponse> responseObserver) {
-        ActivityHelper<SendMessageRequest, SendMessageResponse> helper = getProduceHelper(context, request, responseObserver);
+        ActivityHelper<SendMessageRequest, SendMessageResponse> helper
+            = getProduceHelper(context, request, responseObserver);
 
         try {
-            Runnable task = () -> produceAsync(context, request)
+            Runnable task = ()
+                -> produceAsync(context, request)
                 .whenComplete(helper::writeResponse);
-
             this.executor.submit(helper.createTask(task));
         } catch (Throwable t) {
             helper.writeResponse(null, t);
@@ -74,10 +56,12 @@ public class ProducerActivity {
      * @param responseObserver response
      */
     public void moveToDLQ(RequestContext context, ForwardMessageToDeadLetterQueueRequest request, StreamObserver<ForwardMessageToDeadLetterQueueResponse> responseObserver) {
-        ActivityHelper<ForwardMessageToDeadLetterQueueRequest, ForwardMessageToDeadLetterQueueResponse> helper = getMoveToDLQHelper(context, request, responseObserver);
+        ActivityHelper<ForwardMessageToDeadLetterQueueRequest, ForwardMessageToDeadLetterQueueResponse> helper
+            = getMoveToDLQHelper(context, request, responseObserver);
 
         try {
-            Runnable task = () -> moveToDLQAsync(context, request)
+            Runnable task = ()
+                -> moveToDLQAsync(context, request)
                 .whenComplete(helper::writeResponse);
 
             this.executor.submit(helper.createTask(task));
