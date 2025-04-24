fb5342b3a1833aa06479e9becf4e6c5292503bd3
winglechen@aliyun.com <winglechen@gmail.com>
2025-04-24 15:20:06 +0800

    feature: add ProducerConverter

---
+++ broker/src/main/java/cn/coderule/minimq/broker/server/grpc/converter/ProducerConverter.java
@@ -0,0 +1,77 @@
+package cn.coderule.minimq.broker.server.grpc.converter;
+
+import apache.rocketmq.v2.Code;
+import apache.rocketmq.v2.SendMessageRequest;
+import apache.rocketmq.v2.SendMessageResponse;
+import apache.rocketmq.v2.SendResultEntry;
+import cn.coderule.common.util.lang.StringUtil;
+import cn.coderule.minimq.domain.domain.dto.EnqueueResult;
+import cn.coderule.minimq.rpc.common.core.RequestContext;
+import cn.coderule.minimq.rpc.common.grpc.core.ResponseBuilder;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class ProducerConverter {
+    public static SendMessageResponse toSendMessageResponse(
+        RequestContext ctx, SendMessageRequest request, List<EnqueueResult> resultList) {
+        SendMessageResponse.Builder builder = SendMessageResponse.newBuilder();
+
+        Set<Code> responseCodes = new HashSet<>();
+        for (EnqueueResult result : resultList) {
+            SendResultEntry resultEntry = createSendResultEntry(result);
+            builder.addEntries(resultEntry);
+            responseCodes.add(resultEntry.getStatus().getCode());
+        }
+
+        setStatus(builder, responseCodes);
+        return builder.build();
+    }
+
+    private static SendResultEntry createSendResultEntry(EnqueueResult result) {
+        return switch (result.getStatus()) {
+            case FLUSH_DISK_TIMEOUT -> SendResultEntry.newBuilder()
+                .setStatus(ResponseBuilder.getInstance().buildStatus(
+                    Code.MASTER_PERSISTENCE_TIMEOUT,
+                    "send message failed, sendStatus=" + result.getStatus())
+                )
+                .build();
+            case FLUSH_SLAVE_TIMEOUT -> SendResultEntry.newBuilder()
+                .setStatus(ResponseBuilder.getInstance().buildStatus(
+                    Code.SLAVE_PERSISTENCE_TIMEOUT,
+                    "send message failed, sendStatus=" + result.getStatus())
+                )
+                .build();
+            case SLAVE_NOT_AVAILABLE -> SendResultEntry.newBuilder()
+                .setStatus(ResponseBuilder.getInstance().buildStatus(
+                    Code.HA_NOT_AVAILABLE,
+                    "send message failed, sendStatus=" + result.getStatus())
+                )
+                .build();
+            case PUT_OK -> SendResultEntry.newBuilder()
+                .setStatus(ResponseBuilder.getInstance().buildStatus(Code.OK, Code.OK.name()))
+                .setOffset(result.getQueueOffset())
+                .setMessageId(StringUtil.defaultString(result.getMessageId()))
+                .setTransactionId(StringUtil.defaultString(result.getTransactionId()))
+                .build();
+            default -> SendResultEntry.newBuilder()
+                .setStatus(ResponseBuilder.getInstance().buildStatus(
+                    Code.INTERNAL_SERVER_ERROR,
+                    "send message failed, sendStatus=" + result.getStatus())
+                )
+                .build();
+        };
+    }
+
+    private static void setStatus(SendMessageResponse.Builder builder, Set<Code> responseCodes) {
+        if (responseCodes.size() > 1) {
+            builder.setStatus(ResponseBuilder.getInstance().buildStatus(Code.MULTIPLE_RESULTS, Code.MULTIPLE_RESULTS.name()));
+        } else if (responseCodes.size() == 1) {
+            Code code = responseCodes.stream().findAny().get();
+            builder.setStatus(ResponseBuilder.getInstance().buildStatus(code, code.name()));
+        } else {
+            builder.setStatus(ResponseBuilder.getInstance().buildStatus(Code.INTERNAL_SERVER_ERROR, "send status is empty"));
+        }
+    }
+
+}
