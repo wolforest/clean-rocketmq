a9bb117e6fc171b0a469a7af40a4ac1d408598ea
winglechen@aliyun.com <winglechen@gmail.com>
2025-05-04 00:03:15 +0800

    feature: add GrpcChannelId

---
+++ rpc/src/main/java/cn/coderule/minimq/rpc/common/grpc/core/GrpcChannelId.java
@@ -0,0 +1,39 @@
+package cn.coderule.minimq.rpc.common.grpc.core;
+
+import cn.coderule.common.util.lang.BeanUtil;
+import io.netty.channel.ChannelId;
+
+public class GrpcChannelId implements ChannelId {
+    private final String clientId;
+
+    public GrpcChannelId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    @Override
+    public String asShortText() {
+        return this.clientId;
+    }
+
+    @Override
+    public String asLongText() {
+        return this.clientId;
+    }
+
+    @Override
+    public int compareTo(ChannelId o) {
+        if (this == o) {
+            return 0;
+        }
+
+        if (o instanceof GrpcChannelId other) {
+            return BeanUtil.compareToBuilder()
+                .append(this.clientId, other.clientId)
+                .toComparison();
+        }
+
+        return asLongText().compareTo(
+            o.asLongText()
+        );
+    }
+}
