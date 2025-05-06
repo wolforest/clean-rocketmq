ad8e35b6286c5dd8b1bec7701920782a54af4d51
winglechen@aliyun.com <winglechen@gmail.com>
2025-05-06 21:17:50 +0800

    feature: add consumer request

---
+++ rpc/src/main/java/cn/coderule/minimq/rpc/broker/protocol/consumer/request/PopRequest.java
@@ -0,0 +1,24 @@
+package cn.coderule.minimq.rpc.broker.protocol.consumer.request;
+
+import cn.coderule.minimq.rpc.broker.protocol.heartbeat.SubscriptionData;
+import cn.coderule.minimq.rpc.common.core.RequestContext;
+import java.io.Serializable;
+import lombok.Data;
+
+@Data
+public class PopRequest implements Serializable {
+    private RequestContext requestContext;
+    private long timeout;
+    private String attemptId;
+
+    private String topicName;
+    private String consumerGroup;
+    private int maxNum;
+    private long invisibleTime;
+    private long pollTime;
+    private int initMode;
+
+    private boolean fifo;
+    private SubscriptionData subscriptionData;
+    private PopFilter filter;
+}
