57e4364787afb6d8e3b2d77ab86686686608553d
winglechen@aliyun.com <winglechen@gmail.com>
2025-05-06 17:52:57 +0800

    feature: add DefaultReceiptHandler

---
+++ broker/src/main/java/cn/coderule/minimq/broker/domain/consumer/DefaultReceiptHandler.java
@@ -0,0 +1,32 @@
+package cn.coderule.minimq.broker.domain.consumer;
+
+import cn.coderule.common.convention.service.Lifecycle;
+import cn.coderule.minimq.domain.domain.model.consumer.receipt.MessageReceiptHandle;
+import cn.coderule.minimq.rpc.common.core.RequestContext;
+import io.netty.channel.Channel;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class DefaultReceiptHandler implements ReceiptHandler, Lifecycle {
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void shutdown() {
+
+    }
+
+    @Override
+    public void addReceiptHandle(RequestContext context, Channel channel, String group, String msgID,
+        MessageReceiptHandle messageReceiptHandle) {
+
+    }
+
+    @Override
+    public MessageReceiptHandle removeReceiptHandle(RequestContext context, Channel channel, String group, String msgID,
+        String receiptHandle) {
+        return null;
+    }
+}
