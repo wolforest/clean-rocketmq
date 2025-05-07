3e5107cdfadbcc522abea3c7bb66e83d5888c4c7
winglechen@aliyun.com <winglechen@gmail.com>
2025-05-07 15:52:26 +0800

    feature: add MessageSender

---
+++ broker/src/main/java/cn/coderule/minimq/broker/domain/producer/MessageSender.java
@@ -0,0 +1,109 @@
+package cn.coderule.minimq.broker.domain.producer;
+
+import cn.coderule.common.convention.service.Lifecycle;
+import cn.coderule.common.lang.concurrent.thread.pool.ThreadPoolFactory;
+import cn.coderule.common.util.lang.collection.CollectionUtil;
+import cn.coderule.minimq.domain.config.BrokerConfig;
+import cn.coderule.minimq.domain.domain.dto.EnqueueResult;
+import cn.coderule.minimq.domain.domain.model.message.MessageBO;
+import cn.coderule.minimq.rpc.common.core.RequestContext;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MessageSender implements Lifecycle {
+    private final BrokerConfig brokerConfig;
+    private final ThreadPoolExecutor executor;
+
+    public MessageSender(BrokerConfig brokerConfig) {
+        this.brokerConfig = brokerConfig;
+        this.executor = createExecutor();
+    }
+
+    public CompletableFuture<EnqueueResult> send(RequestContext context, MessageBO messageBO) {
+        // validate topic
+        // validate message
+        // select message queue
+        // init uuid
+
+        // send message
+        // static topic checking
+        // build sendMessageContext
+        // execute pre send hook
+        // clear reserved properties
+        // get topic
+        // select message queue
+        // handle retry or DLQ
+        // check cleanup policy
+        // add message info
+        // handle transaction info
+        // send message(sync or async)
+        // call store api
+        // execute post send hook
+        // format response
+        // execute send callback
+        // execute complete callback
+        return null;
+    }
+
+    public CompletableFuture<List<EnqueueResult>> send(RequestContext context, List<MessageBO> messageList) {
+        if (CollectionUtil.isEmpty(messageList)) {
+            return CompletableFuture.completedFuture(List.of());
+        }
+
+        List<CompletableFuture<EnqueueResult>> futureList = new ArrayList<>();
+        for (MessageBO messageBO : messageList) {
+            CompletableFuture<EnqueueResult> future = send(context, messageBO);
+            futureList.add(future);
+        }
+
+        return combineEnqueueResult(futureList);
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void shutdown() {
+        this.executor.shutdown();
+    }
+
+    private ThreadPoolExecutor createExecutor() {
+        return ThreadPoolFactory.create(
+            brokerConfig.getProducerThreadNum(),
+            brokerConfig.getProducerThreadNum(),
+            1,
+            TimeUnit.MINUTES,
+            "producer-thread-pool",
+            brokerConfig.getProducerQueueCapacity()
+        );
+    }
+
+    private CompletableFuture<List<EnqueueResult>> combineEnqueueResult(List<CompletableFuture<EnqueueResult>> futureList) {
+        CompletableFuture<Void> all = CompletableFuture.allOf(futureList.toArray(new CompletableFuture[0]));
+        return all.thenApply(v -> {
+            List<EnqueueResult> resultList = new ArrayList<>();
+            for (CompletableFuture<EnqueueResult> future : futureList) {
+                addEnqueueResult(resultList, future);
+            }
+            return resultList;
+        });
+    }
+
+    private void addEnqueueResult(List<EnqueueResult> resultList, CompletableFuture<EnqueueResult> future) {
+        try {
+            EnqueueResult enqueueResult = future.get();
+            resultList.add(enqueueResult);
+        } catch (Throwable t) {
+            log.error("produce message error", t);
+            resultList.add(EnqueueResult.failure());
+        }
+    }
+
+}
--- broker/src/main/java/cn/coderule/minimq/broker/domain/producer/Producer.java
+++ broker/src/main/java/cn/coderule/minimq/broker/domain/producer/Producer.java
@@ -1,20 +1,16 @@
 package cn.coderule.minimq.broker.domain.producer;
 
-import cn.coderule.common.convention.service.Lifecycle;
-import cn.coderule.common.util.lang.collection.CollectionUtil;
 import cn.coderule.minimq.rpc.broker.protocol.producer.ProducerInfo;
 import cn.coderule.minimq.rpc.common.core.RequestContext;
 import cn.coderule.minimq.domain.domain.model.message.MessageBO;
 import cn.coderule.minimq.domain.domain.dto.EnqueueResult;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ThreadPoolExecutor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class Producer implements Lifecycle {
-    private ThreadPoolExecutor executor;
+public class Producer {
+    private MessageSender messageSender;
 
     public boolean register(ProducerInfo producerInfo) {
         return true;
@@ -25,77 +21,13 @@ public class Producer implements Lifecycle {
     }
 
     public CompletableFuture<EnqueueResult> produce(RequestContext context, MessageBO messageBO) {
-        // validate topic
-        // validate message
-        // select message queue
-        // init uuid
-
-        // send message
-            // static topic checking
-            // build sendMessageContext
-            // execute pre send hook
-            // clear reserved properties
-            // get topic
-            // select message queue
-            // handle retry or DLQ
-            // check cleanup policy
-            // add message info
-            // handle transaction info
-            // send message(sync or async)
-                // call store api
-                // execute post send hook
-                // format response
-        // execute send callback
-        // execute complete callback
-
-        return null;
+        return messageSender.send(context, messageBO);
     }
 
     public CompletableFuture<List<EnqueueResult>> produce(RequestContext context, List<MessageBO> messageList) {
-        if (CollectionUtil.isEmpty(messageList)) {
-            return CompletableFuture.completedFuture(List.of());
-        }
-
-        List<CompletableFuture<EnqueueResult>> futureList = new ArrayList<>();
-        for (MessageBO messageBO : messageList) {
-            CompletableFuture<EnqueueResult> future = produce(context, messageBO);
-            futureList.add(future);
-        }
-
-        return combineEnqueueResult(futureList);
+        return messageSender.send(context, messageList);
     }
 
-    @Override
-    public void start() {
 
-    }
-
-    @Override
-    public void shutdown() {
-        if (executor != null) {
-            executor.shutdown();
-        }
-    }
-
-    private CompletableFuture<List<EnqueueResult>> combineEnqueueResult(List<CompletableFuture<EnqueueResult>> futureList) {
-        CompletableFuture<Void> all = CompletableFuture.allOf(futureList.toArray(new CompletableFuture[0]));
-        return all.thenApply(v -> {
-            List<EnqueueResult> resultList = new ArrayList<>();
-            for (CompletableFuture<EnqueueResult> future : futureList) {
-                addEnqueueResult(resultList, future);
-            }
-            return resultList;
-        });
-    }
-
-    private void addEnqueueResult(List<EnqueueResult> resultList, CompletableFuture<EnqueueResult> future) {
-        try {
-            EnqueueResult enqueueResult = future.get();
-            resultList.add(enqueueResult);
-        } catch (Throwable t) {
-            log.error("produce message error", t);
-            resultList.add(EnqueueResult.failure());
-        }
-    }
 
 }
