ad8b67f7a0c0c632d27509679103ca3c49dac228
winglechen@aliyun.com <winglechen@gmail.com>
2025-05-12 15:22:13 +0800

    feature: impl ConsumeHookManager

--- broker/src/main/java/cn/coderule/minimq/broker/domain/consumer/ConsumeHookManager.java
+++ broker/src/main/java/cn/coderule/minimq/broker/domain/consumer/ConsumeHookManager.java
@@ -1,9 +1,18 @@
 package cn.coderule.minimq.broker.domain.consumer;
 
+import cn.coderule.common.util.lang.collection.CollectionUtil;
 import cn.coderule.minimq.domain.domain.model.consumer.ConsumeContext;
 import cn.coderule.minimq.domain.service.broker.hook.ConsumeHook;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ConsumeHookManager implements ConsumeHook {
+    private final List<ConsumeHook> hooks = new ArrayList<>();
+
+    public void registerHook(ConsumeHook hook) {
+        hooks.add(hook);
+    }
+
     @Override
     public String hookName() {
         return ConsumeHookManager.class.getSimpleName();
@@ -11,11 +20,23 @@ public class ConsumeHookManager implements ConsumeHook {
 
     @Override
     public void preConsume(ConsumeContext context) {
+        if (CollectionUtil.isEmpty(hooks)) {
+            return;
+        }
 
+        for (ConsumeHook hook : hooks) {
+            hook.preConsume(context);
+        }
     }
 
     @Override
     public void PostConsume(ConsumeContext context) {
+        if (CollectionUtil.isEmpty(hooks)) {
+            return;
+        }
 
+        for (ConsumeHook hook : hooks) {
+            hook.PostConsume(context);
+        }
     }
 }
