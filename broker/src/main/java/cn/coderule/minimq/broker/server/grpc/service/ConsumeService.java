76af8df5dbd6102d009e88fb4ebadc68883ec1a5
winglechen@aliyun.com <winglechen@gmail.com>
2025-05-17 23:42:03 +0800

    feature: add ConsumeService

---
+++ broker/src/main/java/cn/coderule/minimq/broker/server/grpc/service/ConsumeService.java
@@ -0,0 +1,33 @@
+package cn.coderule.minimq.broker.server.grpc.service;
+
+import apache.rocketmq.v2.Code;
+import apache.rocketmq.v2.ReceiveMessageRequest;
+import apache.rocketmq.v2.ReceiveMessageResponse;
+import cn.coderule.minimq.broker.api.ConsumerController;
+import cn.coderule.minimq.domain.domain.dto.request.PopRequest;
+import cn.coderule.minimq.domain.domain.model.cluster.RequestContext;
+import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ConsumeService {
+    private final ConsumerController consumerController;
+    private final StreamObserver<ReceiveMessageResponse> streamObserver;
+
+    public ConsumeService(ConsumerController consumerController, StreamObserver<ReceiveMessageResponse> streamObserver) {
+        this.consumerController = consumerController;
+        this.streamObserver = streamObserver;
+    }
+
+    public void writeResponse(RequestContext context, ReceiveMessageRequest request, PopRequest popRequest) {
+
+    }
+
+    public void writeResponse(RequestContext context, Code code, String message) {
+
+    }
+
+    public void writeResponse(RequestContext context, ReceiveMessageRequest request, Throwable t) {
+
+    }
+}
