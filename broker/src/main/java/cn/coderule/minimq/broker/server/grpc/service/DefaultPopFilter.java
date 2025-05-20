ffc3999304ae231a473439a41f9a96fbcc5b8cdc
winglechen@aliyun.com <winglechen@gmail.com>
2025-05-20 10:03:16 +0800

    feature: add DefaultPopFilter

---
+++ broker/src/main/java/cn/coderule/minimq/broker/server/grpc/service/DefaultPopFilter.java
@@ -0,0 +1,41 @@
+package cn.coderule.minimq.broker.server.grpc.service;
+
+import cn.coderule.minimq.domain.domain.model.cluster.RequestContext;
+import cn.coderule.minimq.domain.domain.model.cluster.heartbeat.SubscriptionData;
+import cn.coderule.minimq.domain.domain.model.message.MessageBO;
+import cn.coderule.minimq.domain.service.broker.consume.PopFilter;
+import java.util.Set;
+
+public class DefaultPopFilter implements PopFilter {
+    private final int maxAttempts;
+
+    public DefaultPopFilter(int maxAttempts) {
+        this.maxAttempts = maxAttempts;
+    }
+
+    @Override
+    public FilterResult filterMessage(RequestContext ctx, String consumerGroup, SubscriptionData subscriptionData,
+        MessageBO messageExt) {
+        if (!isTagMatched(subscriptionData.getTagsSet(), messageExt.getTags())) {
+            return FilterResult.NO_MATCH;
+        }
+
+        if (messageExt.getReconsumeTimes() >= maxAttempts) {
+            return FilterResult.TO_DLQ;
+        }
+
+        return FilterResult.MATCH;
+    }
+
+    private boolean isTagMatched(Set<String> tagsSet, String tags) {
+        if (tagsSet.isEmpty()) {
+            return true;
+        }
+
+        if (tags == null) {
+            return false;
+        }
+
+        return tagsSet.contains(tags);
+    }
+}
