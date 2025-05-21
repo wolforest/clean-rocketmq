b4da0c1e0186bd7b0ad941e1cad674f8a8ba9059
winglechen@aliyun.com <winglechen@gmail.com>
2025-05-21 16:17:53 +0800

    feature: add AckService and InvisibleService

---
+++ broker/src/main/java/cn/coderule/minimq/broker/server/grpc/service/consume/AckService.java
@@ -0,0 +1,24 @@
+package cn.coderule.minimq.broker.server.grpc.service.consume;
+
+import cn.coderule.minimq.broker.api.ConsumerController;
+import cn.coderule.minimq.broker.server.grpc.service.ChannelManager;
+import cn.coderule.minimq.broker.server.grpc.service.SettingManager;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class AckService {
+    private final ConsumerController consumerController;
+    private final SettingManager settingManager;
+    private final ChannelManager channelManager;
+
+    public AckService(
+        ConsumerController consumerController,
+        SettingManager settingManager,
+        ChannelManager channelManager
+    ) {
+        this.consumerController = consumerController;
+        this.settingManager = settingManager;
+        this.channelManager = channelManager;
+    }
+
+}
