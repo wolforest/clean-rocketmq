5ca4d5cd6315099fabb929a64fbaa06a3a6f0de0
winglechen@aliyun.com <winglechen@gmail.com>
2025-05-28 14:45:39 +0800

    feature: impl method run of ReviveThread

--- broker/src/main/java/cn/coderule/minimq/broker/domain/consumer/pop/ReviveThread.java
+++ broker/src/main/java/cn/coderule/minimq/broker/domain/consumer/pop/ReviveThread.java
@@ -3,8 +3,11 @@ package cn.coderule.minimq.broker.domain.consumer.pop;
 import cn.coderule.common.lang.concurrent.thread.ServiceThread;
 import cn.coderule.minimq.domain.config.BrokerConfig;
 import cn.coderule.minimq.domain.config.MessageConfig;
+import cn.coderule.minimq.domain.domain.model.consumer.pop.revive.ConsumeReviveObj;
 import cn.coderule.minimq.domain.service.broker.infra.MQStore;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class ReviveThread extends ServiceThread {
     private final BrokerConfig brokerConfig;
     private final MessageConfig messageConfig;
@@ -12,6 +15,8 @@ public class ReviveThread extends ServiceThread {
     private final int queueId;
 
     private final MQStore mqStore;
+
+    private volatile boolean skipRevive = false;
 
     public ReviveThread(BrokerConfig brokerConfig, String reviveTopic, int queueId, MQStore mqStore) {
         this.brokerConfig = brokerConfig;
@@ -28,6 +33,30 @@ public class ReviveThread extends ServiceThread {
 
     @Override
     public void run() {
+        while (!this.isStopped()) {
+            if (shouldSkip()) continue;
 
+            log.info("start revive topic={}; reviveQueueId={}", reviveTopic, queueId);
+
+            ConsumeReviveObj consumeReviveObj = consumeReviveObj();
+            if (skipRevive) {
+                log.info("skip revive topic={}; reviveQueueId={}", reviveTopic, queueId);
+                continue;
+            }
+
+            revive(consumeReviveObj);
+        }
+    }
+
+    private ConsumeReviveObj consumeReviveObj() {
+        return null;
+    }
+
+    private void revive(ConsumeReviveObj consumeReviveObj) {
+
+    }
+
+    private boolean shouldSkip() {
+        return false;
     }
 }
