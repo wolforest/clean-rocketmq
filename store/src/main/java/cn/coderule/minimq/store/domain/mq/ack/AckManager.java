bf3505e3ed2d66fd2b456d6e1738144433286069
winglechen@aliyun.com <winglechen@gmail.com>
2025-06-02 17:30:46 +0800

    feature: add AckManager

---
+++ store/src/main/java/cn/coderule/minimq/store/domain/mq/ack/AckManager.java
@@ -0,0 +1,28 @@
+package cn.coderule.minimq.store.domain.mq.ack;
+
+import cn.coderule.common.convention.service.Lifecycle;
+import cn.coderule.minimq.domain.config.StoreConfig;
+import cn.coderule.minimq.domain.domain.model.meta.topic.KeyBuilder;
+import cn.coderule.minimq.store.server.bootstrap.StoreContext;
+
+public class AckManager implements Lifecycle {
+    @Override
+    public void initialize() {
+        StoreConfig storeConfig = StoreContext.getBean(StoreConfig.class);
+        String reviveTopic = KeyBuilder.buildClusterReviveTopic(storeConfig.getCluster());
+
+
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void shutdown() {
+
+    }
+
+
+}
