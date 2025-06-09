9809ea72fffdefed7ffd1f1ea85ecee9a721c644
winglechen@aliyun.com <winglechen@gmail.com>
2025-06-09 17:20:47 +0800

    feature: impl method retry of RetryService

--- store/src/main/java/cn/coderule/minimq/store/domain/mq/revive/RetryService.java
+++ store/src/main/java/cn/coderule/minimq/store/domain/mq/revive/RetryService.java
@@ -1,4 +1,59 @@
 package cn.coderule.minimq.store.domain.mq.revive;
 
+import cn.coderule.minimq.domain.config.StoreConfig;
+import cn.coderule.minimq.domain.domain.dto.EnqueueResult;
+import cn.coderule.minimq.domain.domain.model.consumer.pop.checkpoint.PopCheckPoint;
+import cn.coderule.minimq.domain.domain.model.consumer.pop.helper.PopConverter;
+import cn.coderule.minimq.domain.domain.model.message.MessageBO;
+import cn.coderule.minimq.domain.service.store.domain.meta.ConsumeOffsetService;
+import cn.coderule.minimq.domain.service.store.domain.meta.TopicService;
+import cn.coderule.minimq.domain.service.store.domain.mq.MQService;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class RetryService {
+    private final StoreConfig storeConfig;
+    private final MQService mqService;
+    private final TopicService topicService;
+    private final ConsumeOffsetService consumeOffsetService;
+
+    public RetryService(
+        StoreConfig storeConfig,
+        MQService mqService,
+        TopicService topicService,
+        ConsumeOffsetService consumeOffsetService
+    ) {
+        this.storeConfig = storeConfig;
+        this.mqService = mqService;
+        this.topicService = topicService;
+        this.consumeOffsetService = consumeOffsetService;
+    }
+
+    public boolean retry(PopCheckPoint point, MessageBO message) {
+        MessageBO retryMessage = createRetryMessage(point, message);
+        initRetryTopic(retryMessage.getTopic());
+        initConsumeOffset(retryMessage.getTopic(), point.getCId());
+
+        EnqueueResult result = mqService.enqueue(retryMessage);
+        if (!result.isSuccess()) {
+            log.error("Retry failed, retryMessage: {}, result: {}", retryMessage, result);
+        }
+
+        return result.isSuccess();
+    }
+
+    private MessageBO createRetryMessage(PopCheckPoint point, MessageBO message) {
+        SocketAddress storeHost = new InetSocketAddress(storeConfig.getHost(), storeConfig.getPort());
+        return PopConverter.toMessageBO(point, message, storeHost);
+    }
+
+    private void initRetryTopic(String topic) {
+
+    }
+
+    private void initConsumeOffset(String topic, String consumeGroup) {
+
+    }
 }
