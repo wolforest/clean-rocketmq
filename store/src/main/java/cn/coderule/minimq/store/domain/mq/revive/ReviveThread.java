2efc24390405f9ac4890f7966bac0ee11dc1a74f
winglechen@aliyun.com <winglechen@gmail.com>
2025-06-10 21:51:12 +0800

    feature: impl method calculateAndWait of ReviveThread

--- store/src/main/java/cn/coderule/minimq/store/domain/mq/revive/ReviveThread.java
+++ store/src/main/java/cn/coderule/minimq/store/domain/mq/revive/ReviveThread.java
@@ -1,13 +1,18 @@
 package cn.coderule.minimq.store.domain.mq.revive;
 
 import cn.coderule.common.lang.concurrent.thread.ServiceThread;
+import cn.coderule.common.util.lang.collection.CollectionUtil;
+import cn.coderule.minimq.domain.config.MessageConfig;
 import cn.coderule.minimq.domain.domain.constant.PopConstants;
+import cn.coderule.minimq.domain.domain.model.consumer.pop.checkpoint.PopCheckPoint;
 import cn.coderule.minimq.domain.domain.model.consumer.pop.revive.ReviveBuffer;
 import cn.coderule.minimq.domain.service.store.domain.meta.ConsumeOffsetService;
+import java.util.ArrayList;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class ReviveThread extends ServiceThread {
+    private final MessageConfig messageConfig;
     private final String reviveTopic;
     private final int queueId;
     private long reviveOffset;
@@ -16,9 +21,11 @@ public class ReviveThread extends ServiceThread {
     private final ReviveConsumer consumer;
     private final ConsumeOffsetService consumeOffsetService;
 
+    private long reviveTimestamp = -1;
     private volatile boolean skipRevive = false;
 
     public ReviveThread(ReviveContext context, int queueId, Reviver reviver, ReviveConsumer consumer) {
+        this.messageConfig = context.getMessageConfig();
         this.reviveTopic = context.getReviveTopic();
         this.queueId = queueId;
 
@@ -93,7 +100,41 @@ public class ReviveThread extends ServiceThread {
         );
     }
 
+    private int initCounter(ReviveBuffer buffer, int counter) {
+        ArrayList<PopCheckPoint> pointList = buffer.getSortedList();
+        long delay = 0;
+        long now = System.currentTimeMillis();
+
+        if (CollectionUtil.isEmpty(pointList)) {
+            reviveTimestamp = now;
+        } else {
+            long firstReviveTime = pointList.get(0).getReviveTime();
+            reviveTimestamp = firstReviveTime;
+            delay = (now - firstReviveTime) / 1000;
+            counter = 1;
+        }
+
+        log.info("revive finished, topic={}; reviveQueueId={}; startOffset={}; endOffset={}, delay={};",
+            reviveTopic, queueId, buffer.getInitialOffset(), buffer.getOffset(), delay);
+
+        return counter;
+    }
+
     private int calculateAndWait(ReviveBuffer buffer, int counter) {
+        counter = initCounter(buffer, counter);
+
+        ArrayList<PopCheckPoint> pointList = buffer.getSortedList();
+        if (!CollectionUtil.isEmpty(pointList)) {
+            return counter;
+        }
+
+        long interval = counter * messageConfig.getReviveInterval();
+        this.await(interval);
+
+        if (counter < messageConfig.getReviveMaxSlow()) {
+            counter++;
+        }
+
         return counter;
     }
 
