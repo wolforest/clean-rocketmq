d11e8121b084bcd929de100ab42f71ae9804342a
winglechen@aliyun.com <winglechen@gmail.com>
2025-06-13 17:19:10 +0800

    feature: add InvisibleService

---
+++ broker/src/main/java/cn/coderule/minimq/broker/domain/consumer/consume/InvisibleService.java
@@ -0,0 +1,12 @@
+package cn.coderule.minimq.broker.domain.consumer.consume;
+
+import cn.coderule.minimq.domain.domain.dto.request.InvisibleRequest;
+import cn.coderule.minimq.domain.domain.dto.response.AckResult;
+import cn.coderule.minimq.domain.domain.model.cluster.RequestContext;
+import java.util.concurrent.CompletableFuture;
+
+public class InvisibleService {
+    public CompletableFuture<AckResult> changeInvisible(RequestContext context, InvisibleRequest request) {
+        return null;
+    }
+}
