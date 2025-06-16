5b7c082584214f6a667faabaa55ec7c14d59cab2
winglechen@aliyun.com <winglechen@gmail.com>
2025-06-17 00:50:16 +0800

    feature: add method removeIdleChannels to ConsumerRegister

--- broker/src/main/java/cn/coderule/minimq/broker/domain/consumer/consumer/ConsumerRegister.java
+++ broker/src/main/java/cn/coderule/minimq/broker/domain/consumer/consumer/ConsumerRegister.java
@@ -5,9 +5,11 @@ import cn.coderule.minimq.domain.domain.dto.request.ConsumerInfo;
 import cn.coderule.minimq.domain.domain.dto.running.ConsumerGroupInfo;
 import cn.coderule.minimq.domain.domain.enums.consume.ConsumerEvent;
 import cn.coderule.minimq.domain.domain.model.cluster.ClientChannelInfo;
+import cn.coderule.minimq.domain.domain.model.cluster.heartbeat.SubscriptionData;
 import cn.coderule.minimq.domain.service.broker.listener.ConsumerListener;
 import cn.coderule.minimq.rpc.common.rpc.netty.service.helper.NettyHelper;
 import io.netty.channel.Channel;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -92,6 +94,10 @@ public class ConsumerRegister {
     }
 
     private void invokeListeners(ConsumerEvent event, String group, Object... args) {
+        if (listeners.isEmpty()) {
+            return;
+        }
+
         for (ConsumerListener listener : listeners) {
             try {
                 listener.handle(event, group, args);
@@ -101,7 +107,40 @@ public class ConsumerRegister {
         }
     }
 
-    private void removeIdleChannels() {
+    private List<String> getIdleTopicList(ConcurrentMap<String, SubscriptionData> subscriptionTable) {
+        List<String> topicList = new ArrayList<>();
+        subscriptionTable.forEach((topic, subscriptionData) -> {
+            long now = System.currentTimeMillis();
+            if (now - subscriptionData.getSubVersion() > subscriptionExpireTime) {
+                topicList.add(topic);
+            }
+        });
 
+        return topicList;
+     }
+
+    private List<String> getIdleGroupList() {
+        List<String> removeList = new ArrayList<>();
+        compensationMap.forEach((group, info) -> {
+            ConcurrentMap<String, SubscriptionData> subscriptionTable = info.getSubscriptionTable();
+            List<String> topicList = getIdleTopicList(subscriptionTable);
+
+            for (String topic : topicList) {
+                subscriptionTable.remove(topic);
+                if (subscriptionTable.isEmpty()) {
+                    removeList.add(group);
+                }
+            }
+        });
+
+        return removeList;
+    }
+
+    private void removeIdleChannels() {
+        List<String> removeList = getIdleGroupList();
+
+        for (String group : removeList) {
+            compensationMap.remove(group);
+        }
     }
 }
