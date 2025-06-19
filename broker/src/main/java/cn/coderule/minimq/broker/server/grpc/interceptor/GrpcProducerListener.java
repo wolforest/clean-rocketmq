6344a8d4cb63c59ebb2a2bdb52260cbca719ab09
winglechen@aliyun.com <winglechen@gmail.com>
2025-06-19 17:55:52 +0800

    feature: impl GrpcProducerListener

--- broker/src/main/java/cn/coderule/minimq/broker/server/grpc/interceptor/GrpcProducerListener.java
+++ broker/src/main/java/cn/coderule/minimq/broker/server/grpc/interceptor/GrpcProducerListener.java
@@ -1,5 +1,7 @@
 package cn.coderule.minimq.broker.server.grpc.interceptor;
 
+import cn.coderule.minimq.broker.server.grpc.service.channel.ChannelManager;
+import cn.coderule.minimq.broker.server.grpc.service.channel.SettingManager;
 import cn.coderule.minimq.domain.domain.enums.produce.ProducerEvent;
 import cn.coderule.minimq.domain.domain.model.cluster.ClientChannelInfo;
 import cn.coderule.minimq.domain.service.broker.listener.ProducerListener;
@@ -7,8 +9,21 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class GrpcProducerListener implements ProducerListener {
-    @Override
-    public void handle(ProducerEvent event, String group, ClientChannelInfo clientChannelInfo) {
+    private final SettingManager settingManager;
+    private final ChannelManager channelManager;
 
+    public GrpcProducerListener(SettingManager settingManager, ChannelManager channelManager) {
+        this.settingManager = settingManager;
+        this.channelManager = channelManager;
+    }
+
+    @Override
+    public void handle(ProducerEvent event, String group, ClientChannelInfo channelInfo) {
+        if (event != ProducerEvent.CLIENT_UNREGISTER) {
+            return;
+        }
+
+        channelManager.removeChannel(channelInfo.getClientId());
+        settingManager.removeSettings(channelInfo.getClientId());
     }
 }
