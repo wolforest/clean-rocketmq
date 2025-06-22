d9702af44c94d60b9e3462cc90633f47eb6c93fc
winglechen@aliyun.com <winglechen@gmail.com>
2025-06-22 13:20:48 +0800

    feature: add PrepareService

---
+++ broker/src/main/java/cn/coderule/minimq/broker/domain/transaction/PrepareService.java
@@ -0,0 +1,12 @@
+package cn.coderule.minimq.broker.domain.transaction;
+
+import cn.coderule.minimq.domain.domain.dto.EnqueueResult;
+import cn.coderule.minimq.domain.domain.model.cluster.RequestContext;
+import cn.coderule.minimq.domain.domain.model.message.MessageBO;
+import java.util.concurrent.CompletableFuture;
+
+public class PrepareService {
+    public CompletableFuture<EnqueueResult> prepare(RequestContext context, MessageBO messageBO) {
+        return null;
+    }
+}
