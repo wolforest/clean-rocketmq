f272db33a9d5075cc3de31c22a83b94be70b880a
winglechen@aliyun.com <winglechen@gmail.com>
2025-06-23 21:16:30 +0800

    comment: add comments to TaskConfig

--- domain/src/main/java/cn/coderule/minimq/domain/config/server/TaskConfig.java
+++ domain/src/main/java/cn/coderule/minimq/domain/config/server/TaskConfig.java
@@ -6,5 +6,15 @@ import lombok.Data;
 @Data
 public class TaskConfig implements Serializable {
     // embed | shardByQueue | shardByServer
+    // bind | preempt | assign
+    /**
+     * taskMode:
+     * 1. bind:
+     *   1.1 embed
+     *   1.2 static binding
+     *   1.3 dynamic binding
+     * 2. preempt:
+     * 3. assign:
+     */
     private String taskMode = "";
 }
