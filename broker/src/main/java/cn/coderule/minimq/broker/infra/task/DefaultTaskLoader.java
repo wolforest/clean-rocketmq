9b0ba95924460cd17f15a7e932bbf182b0ace7c0
winglechen@aliyun.com <winglechen@gmail.com>
2025-06-26 16:14:24 +0800

    feature: add empty DefaultTaskLoader

---
+++ broker/src/main/java/cn/coderule/minimq/broker/infra/task/DefaultTaskLoader.java
@@ -0,0 +1,21 @@
+package cn.coderule.minimq.broker.infra.task;
+
+import cn.coderule.minimq.domain.service.broker.infra.task.TaskFactory;
+import cn.coderule.minimq.domain.service.broker.infra.task.TaskLoader;
+
+public class DefaultTaskLoader implements TaskLoader {
+    @Override
+    public void addTimerFactory(TaskFactory factory) {
+
+    }
+
+    @Override
+    public void addReviveFactory(TaskFactory factory) {
+
+    }
+
+    @Override
+    public void addTransactionFactory(TaskFactory factory) {
+
+    }
+}
