2fc79f066a5b8768f049d070b60a214313223698
winglechen@aliyun.com <winglechen@gmail.com>
2025-06-26 18:37:41 +0800

    feature: add BindingTaskLoader and ShardingTaskLoader

---
+++ broker/src/main/java/cn/coderule/minimq/broker/infra/task/loader/BindingTaskLoader.java
@@ -0,0 +1,26 @@
+package cn.coderule.minimq.broker.infra.task.loader;
+
+import cn.coderule.minimq.domain.service.broker.infra.task.TaskFactory;
+import cn.coderule.minimq.domain.service.broker.infra.task.TaskLoader;
+
+public class BindingTaskLoader implements TaskLoader {
+    @Override
+    public void setTimerFactory(TaskFactory factory) {
+
+    }
+
+    @Override
+    public void setReviveFactory(TaskFactory factory) {
+
+    }
+
+    @Override
+    public void setTransactionFactory(TaskFactory factory) {
+
+    }
+
+    @Override
+    public void load() {
+
+    }
+}
