521ef6833fd81d55890f7f940751cf91b1da2f54
winglechen@aliyun.com <winglechen@gmail.com>
2025-06-26 16:52:18 +0800

    feature: add TimerFactory

---
+++ broker/src/main/java/cn/coderule/minimq/broker/domain/timer/TimerFactory.java
@@ -0,0 +1,11 @@
+package cn.coderule.minimq.broker.domain.timer;
+
+import cn.coderule.minimq.domain.domain.cluster.task.StoreTaskSet;
+import cn.coderule.minimq.domain.service.broker.infra.task.TaskFactory;
+
+public class TimerFactory implements TaskFactory {
+    @Override
+    public void create(StoreTaskSet task) {
+
+    }
+}
