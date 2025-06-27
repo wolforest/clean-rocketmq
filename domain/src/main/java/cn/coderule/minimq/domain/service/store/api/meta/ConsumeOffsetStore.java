c5ec6798186bff5a9c4ee99e564f8940b8aabcfc
winglechen@aliyun.com <winglechen@gmail.com>
2025-06-28 00:15:19 +0800

    feature: add methods deleteByTopic/deleteByGroup/findTopicByGroup/findGroupByTopic to interface ConsumeOffsetStore

--- domain/src/main/java/cn/coderule/minimq/domain/service/store/api/meta/ConsumeOffsetStore.java
+++ domain/src/main/java/cn/coderule/minimq/domain/service/store/api/meta/ConsumeOffsetStore.java
@@ -1,7 +1,10 @@
 package cn.coderule.minimq.domain.service.store.api.meta;
 
+import cn.coderule.minimq.domain.domain.meta.offset.GroupResult;
+import cn.coderule.minimq.domain.domain.meta.offset.OffsetFilter;
 import cn.coderule.minimq.domain.domain.meta.offset.OffsetRequest;
 import cn.coderule.minimq.domain.domain.meta.offset.OffsetResult;
+import cn.coderule.minimq.domain.domain.meta.offset.TopicResult;
 
 public interface ConsumeOffsetStore {
     OffsetResult getOffset(OffsetRequest request);
@@ -9,4 +12,9 @@ public interface ConsumeOffsetStore {
 
     void putOffset(OffsetRequest request);
 
+    void deleteByTopic(OffsetFilter filter);
+    void deleteByGroup(OffsetFilter filter);
+
+    TopicResult findTopicByGroup(OffsetFilter filter);
+    GroupResult findGroupByTopic(OffsetFilter filter);
 }
--- store/src/main/java/cn/coderule/minimq/store/api/ConsumeOffsetStoreImpl.java
+++ store/src/main/java/cn/coderule/minimq/store/api/ConsumeOffsetStoreImpl.java
@@ -1,9 +1,13 @@
 package cn.coderule.minimq.store.api;
 
+import cn.coderule.minimq.domain.domain.meta.offset.GroupResult;
+import cn.coderule.minimq.domain.domain.meta.offset.OffsetFilter;
 import cn.coderule.minimq.domain.domain.meta.offset.OffsetRequest;
 import cn.coderule.minimq.domain.domain.meta.offset.OffsetResult;
+import cn.coderule.minimq.domain.domain.meta.offset.TopicResult;
 import cn.coderule.minimq.domain.service.store.api.meta.ConsumeOffsetStore;
 import cn.coderule.minimq.domain.service.store.domain.meta.ConsumeOffsetService;
+import java.util.Set;
 
 public class ConsumeOffsetStoreImpl implements ConsumeOffsetStore {
     private final ConsumeOffsetService offsetService;
@@ -40,5 +44,27 @@ public class ConsumeOffsetStoreImpl implements ConsumeOffsetStore {
             request.getMessageQueue().getQueueId(),
             request.getNewOffset()
         );
+    }
+
+    @Override
+    public void deleteByTopic(OffsetFilter filter) {
+        offsetService.deleteByTopic(filter.getTopic());
+    }
+
+    @Override
+    public void deleteByGroup(OffsetFilter filter) {
+        offsetService.deleteByGroup(filter.getGroup());
+    }
+
+    @Override
+    public TopicResult findTopicByGroup(OffsetFilter filter) {
+        Set<String> topicSet = offsetService.findTopicByGroup(filter.getGroup());
+        return TopicResult.build(topicSet);
+    }
+
+    @Override
+    public GroupResult findGroupByTopic(OffsetFilter filter) {
+        Set<String> groupSet = offsetService.findGroupByTopic(filter.getTopic());
+        return GroupResult.build(groupSet);
     }
 }
