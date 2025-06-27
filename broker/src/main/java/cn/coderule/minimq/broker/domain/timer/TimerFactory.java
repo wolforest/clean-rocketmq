c0446e89e4d7bc6d6b88539e7d8971276b568ee4
winglechen@aliyun.com <winglechen@gmail.com>
2025-06-27 22:10:04 +0800

    feature: rename CheckerManager to CheckerFactory

--- broker/src/main/java/cn/coderule/minimq/broker/domain/timer/TimerFactory.java
+++ broker/src/main/java/cn/coderule/minimq/broker/domain/timer/TimerFactory.java
@@ -1,11 +1,11 @@
 package cn.coderule.minimq.broker.domain.timer;
 
-import cn.coderule.minimq.domain.domain.cluster.task.StoreTask;
+import cn.coderule.minimq.domain.domain.cluster.task.QueueTask;
 import cn.coderule.minimq.domain.service.broker.infra.task.TaskFactory;
 
 public class TimerFactory implements TaskFactory {
     @Override
-    public void create(StoreTask task) {
+    public void create(QueueTask task) {
 
     }
 }
--- broker/src/main/java/cn/coderule/minimq/broker/domain/transaction/check/CheckerFactory.java
+++ broker/src/main/java/cn/coderule/minimq/broker/domain/transaction/check/CheckerFactory.java
@@ -1,40 +1,35 @@
 package cn.coderule.minimq.broker.domain.transaction.check;
 
 import cn.coderule.common.convention.service.Lifecycle;
-import cn.coderule.common.util.lang.collection.CollectionUtil;
 import cn.coderule.minimq.domain.config.server.BrokerConfig;
 import cn.coderule.minimq.domain.domain.cluster.task.QueueTask;
-import cn.coderule.minimq.domain.domain.cluster.task.StoreTask;
 import cn.coderule.minimq.domain.service.broker.infra.task.TaskFactory;
-import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import lombok.extern.slf4j.Slf4j;
 
-public class CheckManager implements TaskFactory, Lifecycle {
+@Slf4j
+public class CheckerFactory implements TaskFactory, Lifecycle {
     private final BrokerConfig brokerConfig;
     private final ConcurrentMap<Integer, TransactionChecker> checkerMap;
 
-    public CheckManager(BrokerConfig brokerConfig) {
+    public CheckerFactory(BrokerConfig brokerConfig) {
         this.brokerConfig = brokerConfig;
         this.checkerMap = new ConcurrentHashMap<>();
     }
 
     @Override
-    public void create(StoreTask taskSet) {
-        Set<Integer> queueSet = taskSet.getTransactionQueueSet();
-        if (CollectionUtil.isEmpty(queueSet)) {
-            return;
-        }
+    public void create(QueueTask task) {
+        checkerMap.computeIfAbsent(task.getQueueId(), queueId -> {
+            TransactionChecker checker = new TransactionChecker(brokerConfig, task);
+            log.info("create transaction checker: storeGroup={}, queueId={}",
+                task.getStoreGroup(), queueId);
 
-        for (Integer queueId : queueSet) {
-            checkerMap.computeIfAbsent(queueId, k -> {
-                QueueTask task = new QueueTask(taskSet.getStoreGroup(), queueId);
-                TransactionChecker checker = new TransactionChecker(brokerConfig, task);
-
-                checker.start();
-                return checker;
-            });
-        }
+            checker.start();
+            log.info("start transaction checker: storeGroup={}, queueId={}",
+                task.getStoreGroup(), queueId);
+            return checker;
+        });
     }
 
     @Override
