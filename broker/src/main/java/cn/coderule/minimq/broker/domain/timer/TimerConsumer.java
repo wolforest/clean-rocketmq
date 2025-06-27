ecb2e0d2cb93d452c3ffab5e47c1520a33988b35
winglechen@aliyun.com <winglechen@gmail.com>
2025-06-27 22:59:22 +0800

    feature: impl TimerFactory

--- broker/src/main/java/cn/coderule/minimq/broker/domain/timer/TimerConsumer.java
+++ broker/src/main/java/cn/coderule/minimq/broker/domain/timer/TimerConsumer.java
@@ -1,8 +1,15 @@
 package cn.coderule.minimq.broker.domain.timer;
 
 import cn.coderule.common.lang.concurrent.thread.ServiceThread;
+import cn.coderule.minimq.domain.domain.cluster.task.QueueTask;
 
 public class TimerConsumer extends ServiceThread {
+    private final QueueTask task;
+
+    public TimerConsumer(QueueTask task) {
+        this.task = task;
+    }
+
     @Override
     public String getServiceName() {
         return TimerConsumer.class.getSimpleName();
--- broker/src/main/java/cn/coderule/minimq/broker/domain/timer/TimerFactory.java
+++ broker/src/main/java/cn/coderule/minimq/broker/domain/timer/TimerFactory.java
@@ -3,11 +3,31 @@ package cn.coderule.minimq.broker.domain.timer;
 import cn.coderule.common.convention.service.Lifecycle;
 import cn.coderule.minimq.domain.domain.cluster.task.QueueTask;
 import cn.coderule.minimq.domain.service.broker.infra.task.TaskFactory;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class TimerFactory implements TaskFactory, Lifecycle {
+    private final ConcurrentMap<Integer, TimerConsumer> workerMap;
+
+    public TimerFactory() {
+        this.workerMap = new ConcurrentHashMap<>();
+    }
+
     @Override
     public void create(QueueTask task) {
+        workerMap.computeIfAbsent(task.getQueueId(), queueId -> {
+            TimerConsumer consumer = new TimerConsumer(task);
+            log.info("create timer consumer: storeGroup={}, queueId={}",
+                task.getStoreGroup(), queueId);
 
+            consumer.start();
+            log.info("start timer consumer: storeGroup={}, queueId={}",
+                task.getStoreGroup(), queueId);
+
+            return consumer;
+        });
     }
 
     @Override
@@ -17,6 +37,8 @@ public class TimerFactory implements TaskFactory, Lifecycle {
 
     @Override
     public void shutdown() {
-
+        for (TimerConsumer worker : workerMap.values()) {
+            worker.shutdown();
+        }
     }
 }
