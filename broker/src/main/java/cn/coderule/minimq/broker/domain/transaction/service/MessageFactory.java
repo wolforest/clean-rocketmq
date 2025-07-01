0da5b15e8d2c46863d5e3f91c8f2d10ae07cfab3
winglechen@aliyun.com <winglechen@gmail.com>
2025-07-01 17:25:58 +0800

    feature: add method calculateBodyLength to MessageFactory

--- broker/src/main/java/cn/coderule/minimq/broker/domain/transaction/service/MessageFactory.java
+++ broker/src/main/java/cn/coderule/minimq/broker/domain/transaction/service/MessageFactory.java
@@ -3,15 +3,19 @@ package cn.coderule.minimq.broker.domain.transaction.service;
 import cn.coderule.common.util.lang.string.StringUtil;
 import cn.coderule.minimq.broker.domain.transaction.TransactionUtil;
 import cn.coderule.minimq.broker.domain.transaction.model.DeleteBuffer;
+import cn.coderule.minimq.broker.domain.transaction.model.OffsetQueue;
+import cn.coderule.minimq.domain.config.TransactionConfig;
 import cn.coderule.minimq.domain.core.constant.MessageConst;
 import cn.coderule.minimq.domain.core.constant.flag.MessageSysFlag;
 import cn.coderule.minimq.domain.domain.message.MessageBO;
 import cn.coderule.minimq.domain.utils.MessageUtils;
 
 public class MessageFactory {
+    private final TransactionConfig transactionConfig;
     private final DeleteBuffer deleteBuffer;
 
-    public MessageFactory(DeleteBuffer deleteBuffer) {
+    public MessageFactory(TransactionConfig transactionConfig, DeleteBuffer deleteBuffer) {
+        this.transactionConfig = transactionConfig;
         this.deleteBuffer = deleteBuffer;
     }
 
@@ -33,37 +37,61 @@ public class MessageFactory {
         return msg;
     }
 
-    public MessageBO recreatePrepareMessage(MessageBO message) {
-        MessageBO prepareMsg = new MessageBO();
-        prepareMsg.setWaitStore(false);
-        prepareMsg.setMsgId(message.getMsgId());
-        prepareMsg.setTopic(message.getProperty(MessageConst.PROPERTY_REAL_TOPIC));
-        prepareMsg.setBody(message.getBody());
-        String realQueueIdStr = message.getProperty(MessageConst.PROPERTY_REAL_QUEUE_ID);
+    public MessageBO recreatePrepareMessage(MessageBO prepareMessage) {
+        MessageBO newMsg = new MessageBO();
+        newMsg.setWaitStore(false);
+        newMsg.setMsgId( prepareMessage.getMsgId());
+        newMsg.setTopic( prepareMessage.getProperty(MessageConst.PROPERTY_REAL_TOPIC));
+        newMsg.setBody( prepareMessage.getBody());
+        String realQueueIdStr =  prepareMessage.getProperty(MessageConst.PROPERTY_REAL_QUEUE_ID);
         if (StringUtil.isNumeric(realQueueIdStr)) {
-            prepareMsg.setQueueId(Integer.parseInt(realQueueIdStr));
+            newMsg.setQueueId(Integer.parseInt(realQueueIdStr));
         }
-        prepareMsg.setFlag(message.getFlag());
-        prepareMsg.setTagsCode(message.getTagsCode());
-        prepareMsg.setBornTimestamp(message.getBornTimestamp());
-        prepareMsg.setBornHost(message.getBornHost());
-        prepareMsg.setTransactionId(message.getProperty(MessageConst.PROPERTY_UNIQ_CLIENT_MESSAGE_ID_KEYIDX));
+        newMsg.setFlag( prepareMessage.getFlag());
+        newMsg.setTagsCode( prepareMessage.getTagsCode());
+        newMsg.setBornTimestamp( prepareMessage.getBornTimestamp());
+        newMsg.setBornHost( prepareMessage.getBornHost());
+        newMsg.setTransactionId( prepareMessage.getProperty(MessageConst.PROPERTY_UNIQ_CLIENT_MESSAGE_ID_KEYIDX));
 
-        prepareMsg.setProperties(message.getProperties());
-        prepareMsg.putProperty(MessageConst.PROPERTY_TRANSACTION_PREPARED, "true");
-        prepareMsg.deleteProperty(MessageConst.PROPERTY_TRANSACTION_PREPARED_QUEUE_OFFSET);
-        prepareMsg.deleteProperty(MessageConst.PROPERTY_REAL_QUEUE_ID);
+        newMsg.setProperties( prepareMessage.getProperties());
+        newMsg.putProperty(MessageConst.PROPERTY_TRANSACTION_PREPARED, "true");
+        newMsg.deleteProperty(MessageConst.PROPERTY_TRANSACTION_PREPARED_QUEUE_OFFSET);
+        newMsg.deleteProperty(MessageConst.PROPERTY_REAL_QUEUE_ID);
 
-        prepareMsg.setPropertiesString(MessageUtils.propertiesToString(prepareMsg.getProperties()));
+        newMsg.setPropertiesString(MessageUtils.propertiesToString(newMsg.getProperties()));
 
-        int sysFlag = message.getSysFlag();
+        int sysFlag =  prepareMessage.getSysFlag();
         sysFlag |= MessageSysFlag.TRANSACTION_PREPARED_TYPE;
-        prepareMsg.setSysFlag(sysFlag);
+        newMsg.setSysFlag(sysFlag);
 
-        return prepareMsg;
+        return newMsg;
     }
 
     public MessageBO createCommitMessage(int queueId, String offset) {
+        String commitTopic = TransactionUtil.buildCommitTopic();
+        OffsetQueue offsetQueue = deleteBuffer.getQueue(queueId);
+
+        int offsetLength = null == offset ? 0 : offset.length();
+        int bodyLength = calculateBodyLength(offsetLength, offsetQueue);
+
         return null;
+    }
+
+    private int calculateBodyLength(int offsetLength, OffsetQueue offsetQueue) {
+        int bodyLength = offsetLength;
+        int maxLength = transactionConfig.getMaxCommitMessageLength();
+
+        if (bodyLength >= maxLength) {
+            return bodyLength;
+        }
+
+        int size = offsetQueue.getTotalSize();
+        if (size > maxLength || bodyLength + size > maxLength) {
+            bodyLength = maxLength + 100;
+        } else {
+            bodyLength += size;
+        }
+
+        return bodyLength;
     }
 }
--- domain/src/main/java/cn/coderule/minimq/domain/config/TransactionConfig.java
+++ domain/src/main/java/cn/coderule/minimq/domain/config/TransactionConfig.java
@@ -17,7 +17,7 @@ import lombok.Data;
 public class TransactionConfig implements Serializable {
     private long transactionTimeout = 6_000;
 
-    private int maxCommitMessageSize = 4096;
+    private int maxCommitMessageLength = 4096;
     private int batchCommitInterval = 3_000;
 
     private int maxBatchCheckSize = 15;
