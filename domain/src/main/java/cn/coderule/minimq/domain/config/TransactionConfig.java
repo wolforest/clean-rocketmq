c991b980256182804a9a8c7b582dcfa9477ad357
winglechen@aliyun.com <winglechen@gmail.com>
2025-07-01 16:20:58 +0800

    comment: add comments to TransactionConfig

--- domain/src/main/java/cn/coderule/minimq/domain/config/TransactionConfig.java
+++ domain/src/main/java/cn/coderule/minimq/domain/config/TransactionConfig.java
@@ -3,6 +3,16 @@ package cn.coderule.minimq.domain.config;
 import java.io.Serializable;
 import lombok.Data;
 
+/**
+ * There are two kinds of messages in transaction:
+ * 1. Prepare Message
+ *    the message stored before business message,
+ *    to start the transaction.
+ * 2. Commit Message
+ *    the message stored after business message,
+ *    to end the transaction.
+ *    the transaction may be commited or rollback.
+ */
 @Data
 public class TransactionConfig implements Serializable {
     private long transactionTimeout = 6_000;
