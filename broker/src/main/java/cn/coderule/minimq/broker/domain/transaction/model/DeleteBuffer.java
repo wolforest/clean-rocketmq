6774d8dca26492bf8479664cf4a5248dfed79aff
winglechen@aliyun.com <winglechen@gmail.com>
2025-07-01 14:58:38 +0800

    feature: add method getQueue to DeleteBuffer

--- broker/src/main/java/cn/coderule/minimq/broker/domain/transaction/model/DeleteBuffer.java
+++ broker/src/main/java/cn/coderule/minimq/broker/domain/transaction/model/DeleteBuffer.java
@@ -4,9 +4,23 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 public class DeleteBuffer {
+    private static final int DEFAULT_QUEUE_LENGTH = 20_000;
+
     private final ConcurrentMap<Integer, OffsetQueue> queueMap;
 
     public DeleteBuffer() {
         this.queueMap = new ConcurrentHashMap<>();
+    }
+
+    public OffsetQueue getQueue(int queueId) {
+        OffsetQueue offsetQueue = queueMap.get(queueId);
+        if (offsetQueue != null) {
+            return offsetQueue;
+        }
+
+        offsetQueue = new OffsetQueue(System.currentTimeMillis(), DEFAULT_QUEUE_LENGTH);
+        OffsetQueue old = queueMap.putIfAbsent(queueId, offsetQueue);
+
+        return old != null ? old : offsetQueue;
     }
 }
