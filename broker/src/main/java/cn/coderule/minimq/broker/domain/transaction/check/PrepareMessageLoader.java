814be726845fe60b02093e1fcbad361c0825c2b7
winglechen@aliyun.com <winglechen@gmail.com>
2025-07-02 18:27:46 +0800

    feature: add PrepareMessageLoader

---
+++ broker/src/main/java/cn/coderule/minimq/broker/domain/transaction/check/PrepareMessageLoader.java
@@ -0,0 +1,20 @@
+package cn.coderule.minimq.broker.domain.transaction.check;
+
+import cn.coderule.minimq.broker.domain.transaction.service.MessageService;
+import cn.coderule.minimq.domain.domain.consumer.consume.mq.DequeueResult;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class PrepareMessageLoader {
+    private MessageService messageService;
+    private TransactionContext transactionContext;
+
+    public PrepareMessageLoader(TransactionContext transactionContext) {
+        this.transactionContext = transactionContext;
+        this.messageService = transactionContext.getMessageService();
+    }
+
+    public DequeueResult load(CheckContext checkContext) {
+        return DequeueResult.notFound();
+    }
+}
