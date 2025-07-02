7858294f54fe062e9ddcaae6b1575e5d8d3362c2
winglechen@aliyun.com <winglechen@gmail.com>
2025-07-02 17:04:44 +0800

    feature: add CheckBuffer

---
+++ domain/src/main/java/cn/coderule/minimq/domain/domain/transaction/CheckBuffer.java
@@ -0,0 +1,29 @@
+package cn.coderule.minimq.domain.domain.transaction;
+
+import cn.coderule.minimq.domain.domain.MessageQueue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+public class CheckBuffer {
+    // prepareQueue -> commitQueue
+    private final ConcurrentMap<MessageQueue, MessageQueue> queueMap;
+
+    public CheckBuffer() {
+        this.queueMap = new ConcurrentHashMap<>();
+    }
+
+    public MessageQueue getCommitQueue(MessageQueue prepareQueue) {
+        return queueMap.get(prepareQueue);
+    }
+
+    public MessageQueue createCommitQueue(MessageQueue prepareQueue) {
+        MessageQueue commitQueue = MessageQueue.builder()
+                .topicName(TransactionUtil.buildCommitTopic())
+                .groupName(prepareQueue.getGroupName())
+                .queueId(prepareQueue.getQueueId())
+                .build();
+
+        queueMap.put(prepareQueue, commitQueue);
+        return commitQueue;
+    }
+}
