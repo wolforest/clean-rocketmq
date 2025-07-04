c229c72d7840be22277aa94c9720a45fa6ac9fc9
winglechen@aliyun.com <winglechen@gmail.com>
2025-07-04 13:12:02 +0800

    feature: impl method loadCommitMessage of TransactionChecker

--- broker/src/main/java/cn/coderule/minimq/broker/domain/transaction/check/TransactionChecker.java
+++ broker/src/main/java/cn/coderule/minimq/broker/domain/transaction/check/TransactionChecker.java
@@ -11,6 +11,7 @@ import cn.coderule.minimq.domain.core.enums.message.MessageStatus;
 import cn.coderule.minimq.domain.domain.MessageQueue;
 import cn.coderule.minimq.domain.domain.cluster.task.QueueTask;
 import cn.coderule.minimq.domain.domain.consumer.consume.mq.DequeueResult;
+import cn.coderule.minimq.domain.domain.message.MessageBO;
 import cn.coderule.minimq.domain.domain.transaction.CheckBuffer;
 import cn.coderule.minimq.domain.domain.transaction.TransactionUtil;
 import java.util.Set;
@@ -89,6 +90,7 @@ public class TransactionChecker extends ServiceThread {
 
         DequeueResult commitResult = loadCommitMessage(checkContext);
         if (commitResult.isEmpty()) {
+            updateOffset(checkContext, commitResult);
             return;
         }
 
@@ -103,10 +105,13 @@ public class TransactionChecker extends ServiceThread {
             return commitResult;
         }
 
-
-
+        formatCommitResult(checkContext, commitResult);
 
         return commitResult;
+    }
+
+    private void formatCommitResult(CheckContext context, DequeueResult result) {
+
     }
 
     private boolean validateCommitResult(CheckContext context, DequeueResult result) {
@@ -118,6 +123,12 @@ public class TransactionChecker extends ServiceThread {
         if (result.isEmpty()) {
             log.error("no commit message for checking: commitQueue={}, commitOffset={}",
                 context.getCommitQueue(), context.getCommitOffset());
+            return false;
+        }
+
+        if (result.isOffsetIllegal()) {
+            log.error("commit message offset illegal: commitQueue={}, commitOffset={}, nextOffset={}",
+                context.getCommitQueue(), context.getCommitOffset(), result.getNextOffset());
             return false;
         }
 
