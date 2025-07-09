fda1d4ca83df059634e2832b046e960c40c9d8b5
winglechen@aliyun.com <winglechen@gmail.com>
2025-07-09 11:59:18 +0800

    feature: add empty CommitLogTransfer

--- store/src/main/java/cn/coderule/minimq/store/server/ha/processor/CommitLogSynchronizer.java
+++ store/src/main/java/cn/coderule/minimq/store/server/ha/processor/CommitLogSynchronizer.java
@@ -1,11 +1,25 @@
 package cn.coderule.minimq.store.server.ha.processor;
 
+import cn.coderule.common.lang.concurrent.thread.ServiceThread;
 import cn.coderule.minimq.domain.domain.producer.EnqueueResult;
 import cn.coderule.minimq.domain.domain.cluster.store.InsertFuture;
 import java.util.concurrent.CompletableFuture;
+import lombok.extern.slf4j.Slf4j;
 
-public class CommitLogSynchronizer {
+@Slf4j
+public class CommitLogSynchronizer extends ServiceThread {
+
+    @Override
+    public String getServiceName() {
+        return CommitLogSynchronizer.class.getSimpleName();
+    }
+
     public CompletableFuture<EnqueueResult> sync(InsertFuture result) {
         return result.getFuture();
+    }
+
+    @Override
+    public void run() {
+
     }
 }
---
+++ store/src/main/java/cn/coderule/minimq/store/server/ha/processor/CommitLogTransfer.java
@@ -0,0 +1,21 @@
+package cn.coderule.minimq.store.server.ha.processor;
+
+import cn.coderule.common.lang.concurrent.thread.ServiceThread;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class CommitLogTransfer extends ServiceThread {
+    @Override
+    public String getServiceName() {
+        return "";
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    private void transfer() {
+
+    }
+}
