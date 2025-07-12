d984114495d339a3c291ae86ab08b9ec4152122e
winglechen@aliyun.com <winglechen@gmail.com>
2025-07-12 10:33:03 +0800

    feature: add methods updateMasterAddress to HAService

--- store/src/main/java/cn/coderule/minimq/store/server/ha/HAService.java
+++ store/src/main/java/cn/coderule/minimq/store/server/ha/HAService.java
@@ -1,9 +1,11 @@
 package cn.coderule.minimq.store.server.ha;
 
 import cn.coderule.common.convention.service.Lifecycle;
+import cn.coderule.minimq.store.server.ha.client.HAClient;
 import cn.coderule.minimq.store.server.ha.core.HAContext;
 import cn.coderule.minimq.store.server.ha.core.monitor.StateMonitor;
 import cn.coderule.minimq.store.server.ha.core.monitor.StateRequest;
+import cn.coderule.minimq.store.server.ha.server.HAServer;
 import cn.coderule.minimq.store.server.ha.server.processor.SlaveMonitor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -12,7 +14,13 @@ public class HAService implements Lifecycle {
     private final StateMonitor stateMonitor;
     private final SlaveMonitor slaveMonitor;
 
+    private final HAServer haServer;
+    private final HAClient haClient;
+
     public HAService(HAContext context) {
+        this.haServer = context.getHaServer();
+        this.haClient = context.getHaClient();
+
         this.stateMonitor = new StateMonitor(
             context.getStoreConfig(),
             context.getHaServer(),
@@ -20,6 +28,22 @@ public class HAService implements Lifecycle {
         );
 
         this.slaveMonitor = context.getSlaveMonitor();
+    }
+
+    public void updateMasterAddress(String addr) {
+        if (haClient == null) {
+            return;
+        }
+
+        haClient.setMasterAddress(addr);
+    }
+
+    public void updateMasterHaAddress(String addr) {
+        if (haClient == null) {
+            return;
+        }
+
+        haClient.setMasterHaAddress(addr);
     }
 
     public void monitorState(StateRequest request) {
