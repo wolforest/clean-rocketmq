fb706e40a166110a2881c32801d3e225075d3c9d
winglechen@aliyun.com <winglechen@gmail.com>
2025-07-15 15:49:46 +0800

    feature: add some attributes to CommitLogTransfer

--- store/src/main/java/cn/coderule/minimq/store/server/ha/server/processor/CommitLogTransfer.java
+++ store/src/main/java/cn/coderule/minimq/store/server/ha/server/processor/CommitLogTransfer.java
@@ -2,9 +2,12 @@ package cn.coderule.minimq.store.server.ha.server.processor;
 
 import cn.coderule.common.convention.service.Lifecycle;
 import cn.coderule.common.lang.concurrent.thread.ServiceThread;
+import cn.coderule.minimq.domain.domain.cluster.store.SelectedMappedBuffer;
 import cn.coderule.minimq.domain.service.store.api.CommitLogStore;
+import cn.coderule.minimq.store.server.ha.core.DefaultHAConnection;
 import cn.coderule.minimq.store.server.ha.core.HAConnection;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
@@ -12,11 +15,18 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class CommitLogTransfer extends ServiceThread implements Lifecycle {
+    private CommitLogStore commitLogStore;
     private final HAConnection connection;
+
     private final Selector selector;
     private final SocketChannel socketChannel;
+    private final ByteBuffer headerBuffer;
+    private SelectedMappedBuffer selectedBuffer;
 
-    private CommitLogStore commitLogStore;
+    private boolean transferDone = true;
+    private long nextTransferOffset;
+    private long lastWriteTime;
+    private long lastReportTime;
 
     public CommitLogTransfer(HAConnection connection) throws IOException {
         this.connection = connection;
@@ -24,6 +34,12 @@ public class CommitLogTransfer extends ServiceThread implements Lifecycle {
         this.selector = connection.openSelector();
         connection.registerSelector(selector, SelectionKey.OP_WRITE);
         this.setDaemon(true);
+
+        long now = System.currentTimeMillis();
+        this.nextTransferOffset = -1;
+        this.headerBuffer = ByteBuffer.allocate(DefaultHAConnection.TRANSFER_HEADER_SIZE);
+        this.lastWriteTime = now;
+        this.lastReportTime = now;
     }
 
     @Override
@@ -33,6 +49,10 @@ public class CommitLogTransfer extends ServiceThread implements Lifecycle {
 
     @Override
     public void run() {
+
+    }
+
+    private void initOffset() {
 
     }
 
