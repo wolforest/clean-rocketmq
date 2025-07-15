e768e9d4e50f8714912a9475d6ad685f0a7af057
winglechen@aliyun.com <winglechen@gmail.com>
2025-07-15 10:41:23 +0800

    feature: add dependency to CommitLogTransfer

--- store/src/main/java/cn/coderule/minimq/store/server/ha/server/processor/CommitLogTransfer.java
+++ store/src/main/java/cn/coderule/minimq/store/server/ha/server/processor/CommitLogTransfer.java
@@ -2,15 +2,25 @@ package cn.coderule.minimq.store.server.ha.server.processor;
 
 import cn.coderule.common.convention.service.Lifecycle;
 import cn.coderule.common.lang.concurrent.thread.ServiceThread;
+import cn.coderule.minimq.store.server.ha.core.HAConnection;
+import java.io.IOException;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class CommitLogTransfer extends ServiceThread implements Lifecycle {
+    private final HAConnection connection;
+    private final Selector selector;
+    private final SocketChannel socketChannel;
 
-
-
-    public CommitLogTransfer() {
-
+    public CommitLogTransfer(HAConnection connection) throws IOException {
+        this.connection = connection;
+        this.socketChannel = connection.getSocketChannel();
+        this.selector = connection.openSelector();
+        connection.registerSelector(selector, SelectionKey.OP_WRITE);
+        this.setDaemon(true);
     }
 
     @Override
