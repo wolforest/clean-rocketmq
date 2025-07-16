9d316f8aace96283a84f4c4d528fc407f5976730
winglechen@aliyun.com <winglechen@gmail.com>
2025-07-16 17:58:24 +0800

    feature: add ScanResult

---
+++ domain/src/main/java/cn/coderule/minimq/domain/domain/timer/ScanResult.java
@@ -0,0 +1,29 @@
+
+package cn.coderule.minimq.domain.domain.timer;
+
+import java.io.Serializable;
+import java.util.LinkedList;
+import lombok.Data;
+
+@Data
+public class ScanResult implements Serializable {
+    LinkedList<TimerEvent> normalMsgStack = new LinkedList<>();
+    LinkedList<TimerEvent> deleteMsgStack = new LinkedList<>();
+    int code = 0;
+
+    public void addDeleteMsgStack(TimerEvent timerEvent) {
+        deleteMsgStack.add(timerEvent);
+    }
+
+    public void addNormalMsgStack(TimerEvent timerEvent) {
+        normalMsgStack.addFirst(timerEvent);
+    }
+
+    public int sizeOfDeleteMsgStack() {
+        return deleteMsgStack.size();
+    }
+
+    public int sizeOfNormalMsgStack() {
+        return normalMsgStack.size();
+    }
+}
