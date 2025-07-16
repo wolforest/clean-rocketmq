4a9b3e16b043c2fce72fcd5842bfeef97c77ba1b
winglechen@aliyun.com <winglechen@gmail.com>
2025-07-16 17:47:52 +0800

    feature: add checkpoint related methods to interface Timer

--- domain/src/main/java/cn/coderule/minimq/domain/service/store/domain/timer/Timer.java
+++ domain/src/main/java/cn/coderule/minimq/domain/service/store/domain/timer/Timer.java
@@ -1,4 +1,8 @@
 package cn.coderule.minimq.domain.service.store.domain.timer;
 
+import cn.coderule.minimq.domain.domain.timer.TimerCheckpoint;
+
 public interface Timer {
+    void storeCheckpoint(TimerCheckpoint checkpoint);
+    TimerCheckpoint loadCheckpoint();
 }
--- store/src/main/java/cn/coderule/minimq/store/domain/timer/DefaultTimer.java
+++ store/src/main/java/cn/coderule/minimq/store/domain/timer/DefaultTimer.java
@@ -1,6 +1,16 @@
 package cn.coderule.minimq.store.domain.timer;
 
+import cn.coderule.minimq.domain.domain.timer.TimerCheckpoint;
 import cn.coderule.minimq.domain.service.store.domain.timer.Timer;
 
 public class DefaultTimer implements Timer {
+    @Override
+    public void storeCheckpoint(TimerCheckpoint checkpoint) {
+
+    }
+
+    @Override
+    public TimerCheckpoint loadCheckpoint() {
+        return null;
+    }
 }
