294eed4508ed0b159edceca31e34224a5746b6bc
winglechen@aliyun.com <winglechen@gmail.com>
2025-07-17 10:34:11 +0800

    feature: add CheckpointService

---
+++ store/src/main/java/cn/coderule/minimq/store/domain/timer/CheckpointService.java
@@ -0,0 +1,13 @@
+package cn.coderule.minimq.store.domain.timer;
+
+import cn.coderule.minimq.domain.domain.timer.TimerCheckpoint;
+
+public class CheckpointService {
+    public void store(TimerCheckpoint checkpoint) {
+
+    }
+
+    public TimerCheckpoint load() {
+        return null;
+    }
+}
--- store/src/main/java/cn/coderule/minimq/store/domain/timer/DefaultTimer.java
+++ store/src/main/java/cn/coderule/minimq/store/domain/timer/DefaultTimer.java
@@ -1,19 +1,28 @@
 package cn.coderule.minimq.store.domain.timer;
 
+import cn.coderule.minimq.domain.config.server.StoreConfig;
 import cn.coderule.minimq.domain.domain.timer.ScanResult;
 import cn.coderule.minimq.domain.domain.timer.TimerCheckpoint;
 import cn.coderule.minimq.domain.domain.timer.TimerEvent;
 import cn.coderule.minimq.domain.service.store.domain.timer.Timer;
 
 public class DefaultTimer implements Timer {
+    private final StoreConfig storeConfig;
+    private final CheckpointService checkpointService;
+
+    public DefaultTimer(StoreConfig storeConfig, CheckpointService checkpointService) {
+        this.storeConfig = storeConfig;
+        this.checkpointService = checkpointService;
+    }
+
     @Override
     public void storeCheckpoint(TimerCheckpoint checkpoint) {
-
+        checkpointService.store(checkpoint);
     }
 
     @Override
     public TimerCheckpoint loadCheckpoint() {
-        return null;
+        return checkpointService.load();
     }
 
     @Override
