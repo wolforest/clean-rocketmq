d3f5c68e6b87c0e2da389e8656fe819436d275cb
winglechen@aliyun.com <winglechen@gmail.com>
2025-07-18 18:41:37 +0800

    feature: add some properties to TimerConfig

--- domain/src/main/java/cn/coderule/minimq/domain/config/TimerConfig.java
+++ domain/src/main/java/cn/coderule/minimq/domain/config/TimerConfig.java
@@ -7,4 +7,18 @@ import lombok.Data;
 public class TimerConfig implements Serializable {
     private boolean enableTimer = true;
     private boolean enableRocksDB = false;
+    private boolean enableWarmup = false;
+    private boolean skipUnknownError = false;
+
+    private int timerLogFileSize = 100 * 1024 * 1024;
+
+    private int totalSlots = 7 * 24 * 3600;
+    private int wheelSlots = 2 * 24 * 3600;
+    private int precision = 1_000;
+
+    private int flushInterval = 1_000;
+    private int consumerThreadNum = 3;
+    private int producerThreadNum = 3;
+
+    private int maxDelayTime = 7 * 24 * 3600;
 }
