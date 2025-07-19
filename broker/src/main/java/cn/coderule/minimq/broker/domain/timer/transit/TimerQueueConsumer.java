787b9fa407fb26b309aa33176654acfc1e35dca4
winglechen@aliyun.com <winglechen@gmail.com>
2025-07-19 17:43:00 +0800

    feature: impl method run of TimerQueueConsumer

--- broker/src/main/java/cn/coderule/minimq/broker/domain/timer/transit/TimerQueueConsumer.java
+++ broker/src/main/java/cn/coderule/minimq/broker/domain/timer/transit/TimerQueueConsumer.java
@@ -2,14 +2,19 @@ package cn.coderule.minimq.broker.domain.timer.transit;
 
 import cn.coderule.common.lang.concurrent.thread.ServiceThread;
 import cn.coderule.minimq.broker.domain.timer.service.TimerContext;
+import cn.coderule.minimq.domain.config.TimerConfig;
 import cn.coderule.minimq.domain.domain.cluster.task.QueueTask;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class TimerQueueConsumer extends ServiceThread {
     private final TimerContext context;
+    private final TimerConfig timerConfig;
     private final QueueTask task;
 
     public TimerQueueConsumer(TimerContext context, QueueTask task) {
         this.context = context;
+        this.timerConfig = context.getBrokerConfig().getTimerConfig();
         this.task = task;
     }
 
@@ -20,6 +25,24 @@ public class TimerQueueConsumer extends ServiceThread {
 
     @Override
     public void run() {
+        log.info("{} service started", this.getServiceName());
 
+        long interval = 100L * timerConfig.getPrecision() / 1_000;
+
+        while (!this.isStopped()) {
+            try {
+                if (!consume()) {
+                    this.await(interval);
+                }
+            } catch (Throwable t) {
+                log.error("{} service has exception. ", this.getServiceName(), t);
+            }
+        }
+
+        log.info("{} service end", this.getServiceName());
+    }
+
+    private boolean consume() {
+        return false;
     }
 }
