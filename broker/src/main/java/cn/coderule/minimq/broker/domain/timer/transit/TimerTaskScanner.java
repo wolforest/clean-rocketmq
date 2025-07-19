a5ba4feb327407b147eba5805217479c9bb2a572
winglechen@aliyun.com <winglechen@gmail.com>
2025-07-19 17:10:47 +0800

    feature: add empty TimerTaskScanner

---
+++ broker/src/main/java/cn/coderule/minimq/broker/domain/timer/transit/TimerTaskScanner.java
@@ -0,0 +1,27 @@
+package cn.coderule.minimq.broker.domain.timer.transit;
+
+import cn.coderule.common.lang.concurrent.thread.ServiceThread;
+import cn.coderule.minimq.domain.config.server.BrokerConfig;
+import cn.coderule.minimq.domain.domain.timer.TimerQueue;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TimerTaskScanner extends ServiceThread {
+    private final BrokerConfig brokerConfig;
+    private final TimerQueue timerQueue;
+
+    public TimerTaskScanner(BrokerConfig brokerConfig, TimerQueue timerQueue) {
+        this.brokerConfig = brokerConfig;
+        this.timerQueue = timerQueue;
+    }
+
+    @Override
+    public String getServiceName() {
+        return TimerTaskScanner.class.getSimpleName();
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
