fb89d93942be896b2352be45ba05feba1ad177b1
winglechen@aliyun.com <winglechen@gmail.com>
2025-07-19 17:15:17 +0800

    feature: add TimerContext

---
+++ broker/src/main/java/cn/coderule/minimq/broker/domain/timer/TimerContext.java
@@ -0,0 +1,20 @@
+package cn.coderule.minimq.broker.domain.timer;
+
+import cn.coderule.minimq.domain.config.server.BrokerConfig;
+import cn.coderule.minimq.domain.domain.timer.TimerQueue;
+import cn.coderule.minimq.domain.domain.timer.state.TimerState;
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TimerContext implements Serializable {
+    private BrokerConfig brokerConfig;
+    private TimerQueue timerQueue;
+    private TimerState timerState;
+}
