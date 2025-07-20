50b236841918b856dc63b491b3dbae22234d1dcc
winglechen@aliyun.com <winglechen@gmail.com>
2025-07-20 12:00:55 +0800

    feature: replace constructor parameters by TimerContext

--- broker/src/main/java/cn/coderule/minimq/broker/domain/timer/transit/TimerMessageProducer.java
+++ broker/src/main/java/cn/coderule/minimq/broker/domain/timer/transit/TimerMessageProducer.java
@@ -1,6 +1,7 @@
 package cn.coderule.minimq.broker.domain.timer.transit;
 
 import cn.coderule.common.lang.concurrent.thread.ServiceThread;
+import cn.coderule.minimq.broker.domain.timer.service.TimerContext;
 import cn.coderule.minimq.domain.config.server.BrokerConfig;
 import cn.coderule.minimq.domain.domain.timer.TimerQueue;
 import lombok.extern.slf4j.Slf4j;
@@ -10,9 +11,9 @@ public class TimerMessageProducer extends ServiceThread {
     private final BrokerConfig brokerConfig;
     private final TimerQueue timerQueue;
 
-    public TimerMessageProducer(BrokerConfig brokerConfig, TimerQueue timerQueue) {
-        this.brokerConfig = brokerConfig;
-        this.timerQueue = timerQueue;
+    public TimerMessageProducer(TimerContext context) {
+        this.brokerConfig = context.getBrokerConfig();
+        this.timerQueue = context.getTimerQueue();
     }
 
     @Override
--- broker/src/main/java/cn/coderule/minimq/broker/domain/timer/transit/TimerTaskScanner.java
+++ broker/src/main/java/cn/coderule/minimq/broker/domain/timer/transit/TimerTaskScanner.java
@@ -1,6 +1,7 @@
 package cn.coderule.minimq.broker.domain.timer.transit;
 
 import cn.coderule.common.lang.concurrent.thread.ServiceThread;
+import cn.coderule.minimq.broker.domain.timer.service.TimerContext;
 import cn.coderule.minimq.domain.config.server.BrokerConfig;
 import cn.coderule.minimq.domain.domain.timer.TimerQueue;
 import lombok.extern.slf4j.Slf4j;
@@ -10,9 +11,9 @@ public class TimerTaskScanner extends ServiceThread {
     private final BrokerConfig brokerConfig;
     private final TimerQueue timerQueue;
 
-    public TimerTaskScanner(BrokerConfig brokerConfig, TimerQueue timerQueue) {
-        this.brokerConfig = brokerConfig;
-        this.timerQueue = timerQueue;
+    public TimerTaskScanner(TimerContext context) {
+        this.brokerConfig = context.getBrokerConfig();
+        this.timerQueue = context.getTimerQueue();
     }
 
     @Override
