f598b13f85bddcda9a485cb2fe880a5e691eba95
winglechen@aliyun.com <winglechen@gmail.com>
2025-07-22 17:38:44 +0800

    feature: impl method schedule of TimerTaskScheduler

--- broker/src/main/java/cn/coderule/minimq/broker/domain/timer/transit/TimerTaskScheduler.java
+++ broker/src/main/java/cn/coderule/minimq/broker/domain/timer/transit/TimerTaskScheduler.java
@@ -1,18 +1,22 @@
 package cn.coderule.minimq.broker.domain.timer.transit;
 
 import cn.coderule.common.lang.concurrent.thread.ServiceThread;
+import cn.coderule.common.util.lang.collection.CollectionUtil;
 import cn.coderule.minimq.broker.domain.timer.context.TimerContext;
 import cn.coderule.minimq.broker.infra.store.TimerStore;
+import cn.coderule.minimq.domain.config.TimerConfig;
 import cn.coderule.minimq.domain.config.server.BrokerConfig;
 import cn.coderule.minimq.domain.domain.cluster.task.QueueTask;
+import cn.coderule.minimq.domain.domain.timer.TimerEvent;
 import cn.coderule.minimq.domain.domain.timer.TimerQueue;
 import cn.coderule.minimq.domain.domain.timer.state.TimerState;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class TimerTaskScheduler extends ServiceThread {
     private final TimerContext timerContext;
-    private final BrokerConfig brokerConfig;
+    private final TimerConfig timerConfig;
     private final TimerQueue timerQueue;
     private final TimerState timerState;
     private final TimerStore timerStore;
@@ -21,7 +25,8 @@ public class TimerTaskScheduler extends ServiceThread {
 
     public TimerTaskScheduler(TimerContext context) {
         this.timerContext = context;
-        this.brokerConfig = context.getBrokerConfig();
+        BrokerConfig brokerConfig = context.getBrokerConfig();
+        this.timerConfig = brokerConfig.getTimerConfig();
         this.timerQueue = context.getTimerQueue();
         this.timerState = context.getTimerState();
         this.timerStore = context.getTimerStore();
@@ -34,6 +39,7 @@ public class TimerTaskScheduler extends ServiceThread {
 
     @Override
     public void run() {
+        setState(State.STARTING);
         log.info("{} service started", this.getServiceName());
         if (!loadQueueTask()) {
             return;
@@ -48,9 +54,24 @@ public class TimerTaskScheduler extends ServiceThread {
         }
 
         log.info("{} service end", this.getServiceName());
+        setState(State.ENDING);
     }
 
-    private void schedule() {
+    private void schedule() throws InterruptedException {
+        setState(State.WAITING);
+
+        long timeout = 100L * timerConfig.getPrecision() / 1_000;
+        List<TimerEvent> eventList = timerQueue.pollScheduleEvent(timeout);;
+        if (CollectionUtil.isEmpty(eventList)) {
+            return;
+        }
+
+        setState(State.RUNNING);
+        process(eventList);
+        eventList.clear();
+    }
+
+    private void process(List<TimerEvent> eventList) {
 
     }
 
