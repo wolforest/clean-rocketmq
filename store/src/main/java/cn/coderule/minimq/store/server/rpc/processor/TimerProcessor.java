48fe2956e902b9f93a677b56912d2125026b6112
winglechen@aliyun.com <winglechen@gmail.com>
2025-07-24 16:50:02 +0800

    feature: add empty TimerProcessor

---
+++ store/src/main/java/cn/coderule/minimq/store/server/rpc/processor/TimerProcessor.java
@@ -0,0 +1,20 @@
+package cn.coderule.minimq.store.server.rpc.processor;
+
+import cn.coderule.minimq.rpc.common.rpc.RpcProcessor;
+import cn.coderule.minimq.rpc.common.rpc.core.exception.RemotingCommandException;
+import cn.coderule.minimq.rpc.common.rpc.core.invoke.RpcCommand;
+import cn.coderule.minimq.rpc.common.rpc.core.invoke.RpcContext;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TimerProcessor implements RpcProcessor {
+    @Override
+    public RpcCommand process(RpcContext ctx, RpcCommand request) throws RemotingCommandException {
+        return null;
+    }
+
+    @Override
+    public boolean reject() {
+        return false;
+    }
+}
