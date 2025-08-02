80b437b7fff047341e4244f06551e2bce17d9b07
winglechen@aliyun.com <winglechen@gmail.com>
2025-08-02 11:19:32 +0800

    feature: add enableEmbedStore check to EmbedStoreManager

--- broker/src/main/java/cn/coderule/minimq/broker/infra/embed/EmbedStoreManager.java
+++ broker/src/main/java/cn/coderule/minimq/broker/infra/embed/EmbedStoreManager.java
@@ -3,36 +3,57 @@ package cn.coderule.minimq.broker.infra.embed;
 import cn.coderule.common.convention.container.ApplicationContext;
 import cn.coderule.common.convention.service.Lifecycle;
 import cn.coderule.minimq.broker.server.bootstrap.BrokerContext;
+import cn.coderule.minimq.domain.config.server.BrokerConfig;
 import cn.coderule.minimq.store.Store;
 import cn.coderule.minimq.store.server.bootstrap.StoreContext;
 import cn.coderule.minimq.store.server.bootstrap.StoreArgument;
 
 public class EmbedStoreManager implements Lifecycle {
+    private BrokerConfig brokerConfig;
     private Store store;
 
     @Override
     public void initialize() throws Exception {
-        StoreArgument storeArgument = new StoreArgument();
-        store = new Store(storeArgument);
-        store.initialize();
+        this.brokerConfig = BrokerContext.getBean(BrokerConfig.class);
+        if (!this.brokerConfig.isEnableEmbedStore()) {
+            return;
+        }
+
+        initStore();
     }
 
     @Override
     public void start() throws Exception {
+        if (!this.brokerConfig.isEnableEmbedStore()) {
+            return;
+        }
+
         store.start();
 
-        ApplicationContext storeApi = StoreContext.API;
-        BrokerContext.registerContext(storeApi);
+
     }
 
     @Override
     public void shutdown() throws Exception {
+        if (!this.brokerConfig.isEnableEmbedStore()) {
+            return;
+        }
+
         store.shutdown();
     }
 
     @Override
     public void cleanup() throws Exception {
         store.cleanup();
+    }
+
+    private void initStore() throws Exception {
+        StoreArgument storeArgument = new StoreArgument();
+        store = new Store(storeArgument);
+        store.initialize();
+
+        ApplicationContext storeApi = StoreContext.API;
+        BrokerContext.registerContext(storeApi);
     }
 
 }
