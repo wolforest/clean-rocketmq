ec0be779751dabb109d13347c9eb1243a663f708
winglechen@aliyun.com <winglechen@gmail.com>
2025-08-11 00:44:01 +0800

    feature: add comments to interface ProduceHook

--- domain/src/main/java/cn/coderule/minimq/domain/service/broker/produce/ProduceHook.java
+++ domain/src/main/java/cn/coderule/minimq/domain/service/broker/produce/ProduceHook.java
@@ -2,6 +2,13 @@ package cn.coderule.minimq.domain.service.broker.produce;
 
 import cn.coderule.minimq.domain.domain.producer.ProduceContext;
 
+/**
+ * message produce hook.
+ * possible hooks:
+ *  - check service ready hook
+ *  - check inner batch hook
+ *  - timer hook, to change timer message topic
+ */
 public interface ProduceHook {
     String hookName();
 
