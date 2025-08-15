ba6f2580299f7ae821ec29f0a4f3dfbce06225fc
winglechen@aliyun.com <winglechen@gmail.com>
2025-08-15 18:19:09 +0800

    feature: impl method validateInvisibleTime of PopValidator

--- broker/src/main/java/cn/coderule/minimq/broker/api/validator/PopValidator.java
+++ broker/src/main/java/cn/coderule/minimq/broker/api/validator/PopValidator.java
@@ -2,6 +2,8 @@ package cn.coderule.minimq.broker.api.validator;
 
 import cn.coderule.minimq.domain.config.business.MessageConfig;
 import cn.coderule.minimq.domain.config.server.BrokerConfig;
+import cn.coderule.minimq.domain.core.enums.code.InvalidCode;
+import cn.coderule.minimq.domain.core.exception.InvalidRequestException;
 import cn.coderule.minimq.domain.domain.consumer.consume.pop.PopRequest;
 
 public class PopValidator {
@@ -18,6 +20,21 @@ public class PopValidator {
     }
 
     public void validateInvisibleTime(long invisibleTime) {
+        if (invisibleTime < messageConfig.getMinInvisibleTime()) {
+            throw new InvalidRequestException(
+                InvalidCode.ILLEGAL_INVISIBLE_TIME,
+                "Invisible time is smaller than: " + messageConfig.getMinInvisibleTime());
+        }
 
+        long maxInvisibleTime = messageConfig.getMaxInvisibleTime();
+        if (maxInvisibleTime <= 0) {
+            return;
+        }
+
+        if (invisibleTime > maxInvisibleTime) {
+            throw new InvalidRequestException(
+                InvalidCode.ILLEGAL_INVISIBLE_TIME,
+                "Invisible time is larger than: " + messageConfig.getMaxInvisibleTime());
+        }
     }
 }
