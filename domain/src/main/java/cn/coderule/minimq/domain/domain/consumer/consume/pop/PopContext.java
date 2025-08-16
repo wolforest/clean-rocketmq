150124b6a3a0a7b91cbf05655c66cddb1999d576
winglechen@aliyun.com <winglechen@gmail.com>
2025-08-16 23:46:32 +0800

    feature: add property reviveQueueId to PopContext

--- domain/src/main/java/cn/coderule/minimq/domain/domain/consumer/consume/pop/PopContext.java
+++ domain/src/main/java/cn/coderule/minimq/domain/domain/consumer/consume/pop/PopContext.java
@@ -6,12 +6,17 @@ import lombok.Data;
 
 @Data
 public class PopContext implements Serializable {
-    private PopRequest popRequest;
-    private MessageQueue messageQueue;
+    private final long popTime;
+    private final PopRequest popRequest;
+    private final MessageQueue messageQueue;
+
+    private int reviveQueueId;
 
     public PopContext(PopRequest popRequest, MessageQueue messageQueue) {
         this.popRequest = popRequest;
         this.messageQueue = messageQueue;
+
+        this.popTime = System.currentTimeMillis();
     }
 
 }
