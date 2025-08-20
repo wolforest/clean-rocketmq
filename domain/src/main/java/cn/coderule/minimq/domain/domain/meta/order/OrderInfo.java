a7b170ed920f438dee6da24f615bdf69d12fd2a2
winglechen@aliyun.com <winglechen@gmail.com>
2025-08-20 14:44:08 +0800

    feature: add method isLocked to OrderInfo

--- domain/src/main/java/cn/coderule/minimq/domain/domain/meta/order/OrderInfo.java
+++ domain/src/main/java/cn/coderule/minimq/domain/domain/meta/order/OrderInfo.java
@@ -1,5 +1,6 @@
 package cn.coderule.minimq.domain.domain.meta.order;
 
+import cn.coderule.common.util.lang.collection.CollectionUtil;
 import com.alibaba.fastjson2.annotation.JSONField;
 import java.io.Serializable;
 import java.util.List;
@@ -52,7 +53,66 @@ public class OrderInfo implements Serializable {
     @JSONField(name = "a")
     private String attemptId;
 
+    @JSONField(serialize = false, deserialize = false)
     public boolean isLocked(String attemptId, long invisibleTime) {
+        if (CollectionUtil.isEmpty(offsetList)) {
+            return false;
+        }
+
+        if (null != this.attemptId && this.attemptId.equals(attemptId)) {
+            return false;
+        }
+
+        if (null == this.invisibleTime || this.invisibleTime <= 0) {
+            this.invisibleTime = invisibleTime;
+        }
+
+        int num = offsetList.size();
+        long now = System.currentTimeMillis();
+        for (int i = 0; i < num; i++) {
+            if (!hasNotAck(i)) {
+                continue;
+            }
+
+            long nextVisibleTime = calculateNextVisibleTime(i, invisibleTime);
+            if (nextVisibleTime > now) {
+                return true;
+            }
+        }
+
         return false;
     }
+
+    @JSONField(serialize = false, deserialize = false)
+    public boolean hasNotAck(int offsetIndex) {
+        return (commitOffsetBit & (1L << offsetIndex)) == 0;
+    }
+
+    @JSONField(serialize = false, deserialize = false)
+    public long getQueueOffset(int offsetIndex) {
+        return getQueueOffset(this.offsetList, offsetIndex);
+    }
+
+    private static long getQueueOffset(List<Long> offsetList, int offsetIndex) {
+        if (offsetIndex == 0) {
+            return offsetList.get(0);
+        }
+        return offsetList.get(0) + offsetList.get(offsetIndex);
+    }
+
+    private long calculateNextVisibleTime(int i, long invisibleTime) {
+        long nextVisibleTime = popTime + invisibleTime;
+        if (offsetNextVisibleTime == null) {
+            return nextVisibleTime;
+        }
+
+        long offset = getQueueOffset(i);
+        Long time = offsetNextVisibleTime.get(offset);
+        if (time != null) {
+            return time;
+        }
+
+        return nextVisibleTime;
+    }
+
 }
