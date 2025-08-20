51119079d329476ded60a53c6dee8d442210efab
winglechen@aliyun.com <winglechen@gmail.com>
2025-08-20 14:20:32 +0800

    feature: add KeyUtils for consume order package

--- domain/src/main/java/cn/coderule/minimq/domain/domain/meta/order/ConsumeOrder.java
+++ domain/src/main/java/cn/coderule/minimq/domain/domain/meta/order/ConsumeOrder.java
@@ -5,12 +5,22 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 public class ConsumeOrder implements Serializable {
-    private static final String TOPIC_GROUP_SEPARATOR = "@";
     private static final long CLEAN_SPAN_FROM_LAST = 24 * 3600 * 1000;
 
     private final ConcurrentMap<String, ConcurrentMap<Integer, OrderInfo>> orderMap;
 
     public ConsumeOrder() {
         this.orderMap = new ConcurrentHashMap<>();
+    }
+
+    public boolean isLocked(OrderRequest request) {
+        return false;
+    }
+
+    public void update(OrderRequest request) {
+    }
+
+    public void clearLock() {
+
     }
 }
--- domain/src/main/java/cn/coderule/minimq/domain/domain/meta/order/OrderRequest.java
+++ domain/src/main/java/cn/coderule/minimq/domain/domain/meta/order/OrderRequest.java
@@ -1,26 +1,33 @@
 package cn.coderule.minimq.domain.domain.meta.order;
 
-import cn.coderule.minimq.domain.domain.cluster.RequestContext;
 import java.io.Serializable;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class OrderRequest implements Serializable {
-    private RequestContext requestContext;
-    private long timeout;
+    String attemptId;
+    private boolean isRetry;
 
     private String topicName;
     private String consumerGroup;
     private int queueId;
-    private boolean isRetry;
 
     private long popTime;
     private long invisibleTime;
+
+    private List<Long> offsetList;
+    StringBuilder orderInfoBuilder;
+
+    private String getKey() {
+        return KeyUtils.buildKey(topicName, consumerGroup);
+    }
 
 }
