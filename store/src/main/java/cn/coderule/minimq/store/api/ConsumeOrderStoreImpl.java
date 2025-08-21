9da247258d5600a3fe7d18630348d4d49a98f0b6
winglechen@aliyun.com <winglechen@gmail.com>
2025-08-21 11:50:54 +0800

    feature: add ConsumeOrderStoreImpl

---
+++ store/src/main/java/cn/coderule/minimq/store/api/ConsumeOrderStoreImpl.java
@@ -0,0 +1,38 @@
+package cn.coderule.minimq.store.api;
+
+import cn.coderule.minimq.domain.domain.meta.order.OrderRequest;
+import cn.coderule.minimq.domain.service.store.api.meta.ConsumeOrderStore;
+import cn.coderule.minimq.domain.service.store.domain.meta.ConsumeOrderService;
+
+public class ConsumeOrderStoreImpl implements ConsumeOrderStore {
+    private final ConsumeOrderService consumeOrderService;
+
+    public ConsumeOrderStoreImpl(ConsumeOrderService consumeOrderService) {
+        this.consumeOrderService = consumeOrderService;
+    }
+
+    @Override
+    public boolean isLocked(OrderRequest request) {
+        return consumeOrderService.isLocked(request);
+    }
+
+    @Override
+    public void lock(OrderRequest request) {
+        consumeOrderService.lock(request);
+    }
+
+    @Override
+    public void unlock(OrderRequest request) {
+        consumeOrderService.unlock(request);
+    }
+
+    @Override
+    public long commit(OrderRequest request) {
+        return consumeOrderService.commit(request);
+    }
+
+    @Override
+    public void updateInvisible(OrderRequest request) {
+        consumeOrderService.updateInvisible(request);
+    }
+}
