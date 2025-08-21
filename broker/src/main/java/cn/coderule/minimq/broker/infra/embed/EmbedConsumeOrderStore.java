4d1320c72035e7358fe4b5292e905cf2236fb9f9
winglechen@aliyun.com <winglechen@gmail.com>
2025-08-21 17:22:38 +0800

    feature: add EmbedConsumeOrderStore

---
+++ broker/src/main/java/cn/coderule/minimq/broker/infra/embed/EmbedConsumeOrderStore.java
@@ -0,0 +1,39 @@
+package cn.coderule.minimq.broker.infra.embed;
+
+import cn.coderule.minimq.domain.domain.meta.order.OrderRequest;
+import cn.coderule.minimq.domain.service.store.api.meta.ConsumeOrderStore;
+import cn.coderule.minimq.rpc.store.facade.ConsumeOrderFacade;
+
+public class EmbedConsumeOrderStore extends AbstractEmbedStore implements ConsumeOrderFacade {
+    private final ConsumeOrderStore consumeOrderStore;
+
+    public EmbedConsumeOrderStore(ConsumeOrderStore consumeOrderStore, EmbedLoadBalance loadBalance) {
+        super(loadBalance);
+        this.consumeOrderStore = consumeOrderStore;
+    }
+
+    @Override
+    public boolean isLocked(OrderRequest request) {
+        return consumeOrderStore.isLocked(request);
+    }
+
+    @Override
+    public void lock(OrderRequest request) {
+        consumeOrderStore.lock(request);
+    }
+
+    @Override
+    public void unlock(OrderRequest request) {
+        consumeOrderStore.unlock(request);
+    }
+
+    @Override
+    public long commit(OrderRequest request) {
+        return consumeOrderStore.commit(request);
+    }
+
+    @Override
+    public void updateInvisible(OrderRequest request) {
+        consumeOrderStore.updateInvisible(request);
+    }
+}
