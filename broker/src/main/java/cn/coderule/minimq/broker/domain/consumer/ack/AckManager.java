8f7cac3ed6235375c53eeb9a5b88b78d2086d0ad
winglechen@aliyun.com <winglechen@gmail.com>
2025-08-22 12:52:14 +0800

    feature: impl method initialize of AckManager

--- broker/src/main/java/cn/coderule/minimq/broker/domain/consumer/ack/AckManager.java
+++ broker/src/main/java/cn/coderule/minimq/broker/domain/consumer/ack/AckManager.java
@@ -1,11 +1,16 @@
 package cn.coderule.minimq.broker.domain.consumer.ack;
 
 import cn.coderule.common.convention.service.Lifecycle;
+import cn.coderule.minimq.broker.server.bootstrap.BrokerContext;
 
 public class AckManager implements Lifecycle {
     @Override
     public void initialize() throws Exception {
-        Lifecycle.super.initialize();
+        AckService ackService = new AckService();
+        BrokerContext.register(ackService);
+
+        InvisibleService invisibleService = new InvisibleService();
+        BrokerContext.register(invisibleService);
     }
 
     @Override
