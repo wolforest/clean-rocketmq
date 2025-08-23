1f7b3950611869d04d1bd6afb237211deb7c4455
winglechen@aliyun.com <winglechen@gmail.com>
2025-08-23 09:29:58 +0800

    feature: add empty RenewManager

---
+++ broker/src/main/java/cn/coderule/minimq/broker/domain/consumer/renew/RenewManager.java
@@ -0,0 +1,22 @@
+package cn.coderule.minimq.broker.domain.consumer.renew;
+
+import cn.coderule.common.convention.service.Lifecycle;
+
+public class RenewManager implements Lifecycle {
+    @Override
+    public void initialize() throws Exception {
+
+    }
+
+    @Override
+    public void start() throws Exception {
+
+    }
+
+    @Override
+    public void shutdown() throws Exception {
+
+    }
+
+
+}
