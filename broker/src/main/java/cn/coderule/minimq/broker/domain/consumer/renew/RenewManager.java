b31ce5234b6e985e3d0490279c10174a1cbaa2cb
winglechen@aliyun.com <winglechen@gmail.com>
2025-08-23 15:53:29 +0800

    feature: add method initRenewListener/initReceiptHandler to RenewManager

--- broker/src/main/java/cn/coderule/minimq/broker/domain/consumer/renew/RenewManager.java
+++ broker/src/main/java/cn/coderule/minimq/broker/domain/consumer/renew/RenewManager.java
@@ -1,15 +1,23 @@
 package cn.coderule.minimq.broker.domain.consumer.renew;
 
 import cn.coderule.common.convention.service.Lifecycle;
+import cn.coderule.minimq.broker.domain.consumer.ack.InvisibleService;
 import cn.coderule.minimq.broker.server.bootstrap.BrokerContext;
 import cn.coderule.minimq.domain.config.server.BrokerConfig;
+import cn.coderule.minimq.domain.service.broker.consume.ReceiptHandler;
 
 public class RenewManager implements Lifecycle {
     private BrokerConfig brokerConfig;
 
+    private RenewListener renewListener;
+    private ReceiptHandler receiptHandler;
+
     @Override
     public void initialize() throws Exception {
         brokerConfig = BrokerContext.getBean(BrokerConfig.class);
+
+        initRenewListener();
+        initReceiptHandler();
     }
 
     @Override
@@ -21,6 +29,17 @@ public class RenewManager implements Lifecycle {
     public void shutdown() throws Exception {
 
     }
+
+    private void initRenewListener() {
+        InvisibleService invisibleService = BrokerContext.getBean(InvisibleService.class);
+        renewListener = new RenewListener(invisibleService);
+    }
+
+    private void initReceiptHandler() {
+        receiptHandler = new DefaultReceiptHandler(brokerConfig, renewListener);
+    }
+
+
 
 
 }
