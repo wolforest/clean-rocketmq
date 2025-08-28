cd6dde5b2e1d1f18440c8c99f517bd5c31437cb8
winglechen@aliyun.com <winglechen@gmail.com>
2025-08-29 00:58:44 +0800

    style: move private method to the buttom of OffsetService

--- store/src/main/java/cn/coderule/minimq/store/domain/mq/queue/OffsetService.java
+++ store/src/main/java/cn/coderule/minimq/store/domain/mq/queue/OffsetService.java
@@ -117,84 +117,6 @@ public class OffsetService {
         setOffsetByMessageList(request, result);
     }
 
-    private long correctNextOffset(long oldOffset, long newOffset) {
-        MetaConfig metaConfig = storeConfig.getMetaConfig();
-
-        if (storeConfig.isMaster() || metaConfig.isEnableOffsetCheckInSlave()) {
-            return newOffset;
-        }
-
-        return oldOffset;
-    }
-
-    private void setOffsetIfQueueNotExists(DequeueRequest request, DequeueResult result) {
-        result.setStatus(MessageStatus.NO_MATCHED_LOGIC_QUEUE);
-        result.setMinOffset(0);
-        result.setMaxOffset(0);
-
-        result.setNextOffset(
-            correctNextOffset(request.getOffset(), 0)
-        );
-    }
-
-    private void setOffsetIfQueueEmpty(DequeueRequest request, DequeueResult result) {
-        result.setStatus(MessageStatus.NO_MESSAGE_IN_QUEUE);
-
-        result.setNextOffset(
-            correctNextOffset(request.getOffset(), 0)
-        );
-
-    }
-
-    private void setOffsetIfResultEmpty(DequeueRequest request, DequeueResult result) {
-        result.setStatus(MessageStatus.OFFSET_FOUND_NULL);
-
-        long rolledOffset = consumeQueue.rollToOffset(
-            request.getTopic(),
-            request.getQueueId(),
-            request.getOffset()
-        );
-
-        result.setNextOffset(
-            correctNextOffset(request.getOffset(), rolledOffset)
-        );
-    }
-
-    private void setOffsetIfOffsetSmall(DequeueRequest request, DequeueResult result) {
-        result.setStatus(MessageStatus.OFFSET_TOO_SMALL);
-
-        result.setNextOffset(
-            correctNextOffset(request.getOffset(), result.getMinOffset())
-        );
-    }
-
-    private void setOffsetIfOffsetBig(DequeueRequest request, DequeueResult result) {
-        result.setStatus(MessageStatus.OFFSET_OVERFLOW_BADLY);
-
-        result.setNextOffset(
-            correctNextOffset(request.getOffset(), result.getMaxOffset())
-        );
-    }
-
-    private void setOffsetIfOffsetEqualMax(DequeueRequest request, DequeueResult result) {
-        result.setStatus(MessageStatus.OFFSET_OVERFLOW_ONE);
-
-        result.setNextOffset(
-            request.getOffset()
-        );
-    }
-
-    private void setOffsetByMessageList(DequeueRequest request, DequeueResult result) {
-        long maxOffset = 0;
-
-        for (MessageBO messageBO : result.getMessageList()) {
-            maxOffset = Math.max(maxOffset, messageBO.getQueueOffset());
-        }
-
-        result.setNextOffset(maxOffset + 1);
-    }
-
-
     public void updateOffset(DequeueRequest request, DequeueResult result) {
         updateOrderInfo(request, result);
         updateConsumeOffset(request, result.getNextOffset());
@@ -316,5 +238,80 @@ public class OffsetService {
         return buffer != null;
     }
 
+    private long correctNextOffset(long oldOffset, long newOffset) {
+        MetaConfig metaConfig = storeConfig.getMetaConfig();
+
+        if (storeConfig.isMaster() || metaConfig.isEnableOffsetCheckInSlave()) {
+            return newOffset;
+        }
+
+        return oldOffset;
+    }
+
+    private void setOffsetIfQueueNotExists(DequeueRequest request, DequeueResult result) {
+        result.setStatus(MessageStatus.NO_MATCHED_LOGIC_QUEUE);
+        result.setMinOffset(0);
+        result.setMaxOffset(0);
+
+        result.setNextOffset(
+            correctNextOffset(request.getOffset(), 0)
+        );
+    }
+
+    private void setOffsetIfQueueEmpty(DequeueRequest request, DequeueResult result) {
+        result.setStatus(MessageStatus.NO_MESSAGE_IN_QUEUE);
+
+        result.setNextOffset(
+            correctNextOffset(request.getOffset(), 0)
+        );
+    }
+
+    private void setOffsetIfResultEmpty(DequeueRequest request, DequeueResult result) {
+        result.setStatus(MessageStatus.OFFSET_FOUND_NULL);
+
+        long rolledOffset = consumeQueue.rollToOffset(
+            request.getTopic(),
+            request.getQueueId(),
+            request.getOffset()
+        );
+
+        result.setNextOffset(
+            correctNextOffset(request.getOffset(), rolledOffset)
+        );
+    }
+
+    private void setOffsetIfOffsetSmall(DequeueRequest request, DequeueResult result) {
+        result.setStatus(MessageStatus.OFFSET_TOO_SMALL);
+
+        result.setNextOffset(
+            correctNextOffset(request.getOffset(), result.getMinOffset())
+        );
+    }
+
+    private void setOffsetIfOffsetBig(DequeueRequest request, DequeueResult result) {
+        result.setStatus(MessageStatus.OFFSET_OVERFLOW_BADLY);
+
+        result.setNextOffset(
+            correctNextOffset(request.getOffset(), result.getMaxOffset())
+        );
+    }
+
+    private void setOffsetIfOffsetEqualMax(DequeueRequest request, DequeueResult result) {
+        result.setStatus(MessageStatus.OFFSET_OVERFLOW_ONE);
+
+        result.setNextOffset(
+            request.getOffset()
+        );
+    }
+
+    private void setOffsetByMessageList(DequeueRequest request, DequeueResult result) {
+        long maxOffset = 0;
+
+        for (MessageBO messageBO : result.getMessageList()) {
+            maxOffset = Math.max(maxOffset, messageBO.getQueueOffset());
+        }
+
+        result.setNextOffset(maxOffset + 1);
+    }
 
 }
