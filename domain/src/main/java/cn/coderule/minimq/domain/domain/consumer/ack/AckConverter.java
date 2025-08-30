25057e1b4682fd1235afc0b004e0c5c895c9c9e0
winglechen@aliyun.com <winglechen@gmail.com>
2025-08-30 14:06:40 +0800

    feature: add empty method toCheckPointRequest, toMessage to AckConverter

--- domain/src/main/java/cn/coderule/minimq/domain/domain/consumer/ack/AckConverter.java
+++ domain/src/main/java/cn/coderule/minimq/domain/domain/consumer/ack/AckConverter.java
@@ -1,10 +1,21 @@
 package cn.coderule.minimq.domain.domain.consumer.ack;
 
 import cn.coderule.minimq.domain.domain.consumer.ack.broker.AckRequest;
+import cn.coderule.minimq.domain.domain.consumer.ack.broker.InvisibleRequest;
 import cn.coderule.minimq.domain.domain.consumer.ack.store.AckMessage;
+import cn.coderule.minimq.domain.domain.consumer.ack.store.CheckPointRequest;
+import cn.coderule.minimq.domain.domain.message.MessageBO;
 
 public class AckConverter {
     public static AckMessage toAckMessage(AckRequest request) {
+        return null;
+    }
+
+    public static CheckPointRequest toCheckPointRequest(InvisibleRequest request) {
+        return null;
+    }
+
+    public static MessageBO toMessage(InvisibleRequest request) {
         return null;
     }
 }
