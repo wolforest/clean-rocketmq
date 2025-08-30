cf7eadd4eecebe3ba576d5a56c83e1c036d42325
winglechen@aliyun.com <winglechen@gmail.com>
2025-08-31 07:26:31 +0800

    refactor: use short name embedStore and remoteStore of ConsumeOffsetStore

--- broker/src/main/java/cn/coderule/minimq/broker/infra/store/ConsumeOffsetStore.java
+++ broker/src/main/java/cn/coderule/minimq/broker/infra/store/ConsumeOffsetStore.java
@@ -12,50 +12,50 @@ import cn.coderule.minimq.rpc.store.facade.ConsumeOffsetFacade;
 
 public class ConsumeOffsetStore implements ConsumeOffsetFacade {
     private final BrokerConfig brokerConfig;
-    private final EmbedConsumeOffsetStore embedConsumeOffsetStore;
-    private final RemoteConsumeOffsetStore remoteConsumeOffsetStore;
+    private final EmbedConsumeOffsetStore embedStore;
+    private final RemoteConsumeOffsetStore remoteStore;
 
     public ConsumeOffsetStore(
         BrokerConfig brokerConfig,
-        EmbedConsumeOffsetStore embedConsumeOffsetStore,
-        RemoteConsumeOffsetStore remoteConsumeOffsetStore) {
+        EmbedConsumeOffsetStore embedStore,
+        RemoteConsumeOffsetStore remoteStore) {
         this.brokerConfig = brokerConfig;
-        this.embedConsumeOffsetStore = embedConsumeOffsetStore;
-        this.remoteConsumeOffsetStore = remoteConsumeOffsetStore;
+        this.embedStore = embedStore;
+        this.remoteStore = remoteStore;
     }
 
     @Override
     public OffsetResult getOffset(OffsetRequest request) {
         String topicName = request.getTopicName();
-        if (embedConsumeOffsetStore.containsTopic(topicName)) {
-            return embedConsumeOffsetStore.getOffset(request);
+        if (embedStore.containsTopic(topicName)) {
+            return embedStore.getOffset(request);
         }
 
         if (!brokerConfig.isEnableRemoteStore()) {
             return OffsetResult.build(0);
         }
-        return remoteConsumeOffsetStore.getOffset(request);
+        return remoteStore.getOffset(request);
     }
 
     @Override
     public OffsetResult getAndRemove(OffsetRequest request) {
         String topicName = request.getTopicName();
-        if (embedConsumeOffsetStore.containsTopic(topicName)) {
-            return embedConsumeOffsetStore.getAndRemove(request);
+        if (embedStore.containsTopic(topicName)) {
+            return embedStore.getAndRemove(request);
         }
 
         if (!brokerConfig.isEnableRemoteStore()) {
             return OffsetResult.build(0);
         }
 
-        return remoteConsumeOffsetStore.getAndRemove(request);
+        return remoteStore.getAndRemove(request);
     }
 
     @Override
     public void putOffset(OffsetRequest request) {
         String topicName = request.getTopicName();
-        if (embedConsumeOffsetStore.containsTopic(topicName)) {
-            embedConsumeOffsetStore.putOffset(request);
+        if (embedStore.containsTopic(topicName)) {
+            embedStore.putOffset(request);
             return;
         }
 
@@ -63,14 +63,14 @@ public class ConsumeOffsetStore implements ConsumeOffsetFacade {
             return;
         }
 
-        remoteConsumeOffsetStore.putOffset(request);
+        remoteStore.putOffset(request);
     }
 
     @Override
     public void deleteByTopic(OffsetFilter filter) {
         String topicName = filter.getTopic();
-        if (embedConsumeOffsetStore.containsTopic(topicName)) {
-            embedConsumeOffsetStore.deleteByTopic(filter);
+        if (embedStore.containsTopic(topicName)) {
+            embedStore.deleteByTopic(filter);
             return;
         }
 
@@ -78,14 +78,14 @@ public class ConsumeOffsetStore implements ConsumeOffsetFacade {
             return;
         }
 
-        remoteConsumeOffsetStore.deleteByTopic(filter);
+        remoteStore.deleteByTopic(filter);
     }
 
     @Override
     public void deleteByGroup(OffsetFilter filter) {
         String topicName = filter.getTopic();
-        if (embedConsumeOffsetStore.containsTopic(topicName)) {
-            embedConsumeOffsetStore.deleteByGroup(filter);
+        if (embedStore.containsTopic(topicName)) {
+            embedStore.deleteByGroup(filter);
             return;
         }
 
@@ -93,34 +93,34 @@ public class ConsumeOffsetStore implements ConsumeOffsetFacade {
             return;
         }
 
-        remoteConsumeOffsetStore.deleteByGroup(filter);
+        remoteStore.deleteByGroup(filter);
     }
 
     @Override
     public TopicResult findTopicByGroup(OffsetFilter filter) {
         String topicName = filter.getTopic();
-        if (embedConsumeOffsetStore.containsTopic(topicName)) {
-            return embedConsumeOffsetStore.findTopicByGroup(filter);
+        if (embedStore.containsTopic(topicName)) {
+            return embedStore.findTopicByGroup(filter);
         }
 
         if (!brokerConfig.isEnableRemoteStore()) {
             return TopicResult.empty();
         }
 
-        return remoteConsumeOffsetStore.findTopicByGroup(filter);
+        return remoteStore.findTopicByGroup(filter);
     }
 
     @Override
     public GroupResult findGroupByTopic(OffsetFilter filter) {
         String topicName = filter.getTopic();
-        if (embedConsumeOffsetStore.containsTopic(topicName)) {
-            return embedConsumeOffsetStore.findGroupByTopic(filter);
+        if (embedStore.containsTopic(topicName)) {
+            return embedStore.findGroupByTopic(filter);
         }
 
         if (!brokerConfig.isEnableRemoteStore()) {
             return GroupResult.empty();
         }
 
-        return remoteConsumeOffsetStore.findGroupByTopic(filter);
+        return remoteStore.findGroupByTopic(filter);
     }
 }
