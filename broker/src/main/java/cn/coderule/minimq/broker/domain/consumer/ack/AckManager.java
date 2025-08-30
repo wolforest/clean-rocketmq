db0b4477d1eca3375d4b34f7e27084fa98bcecdb
winglechen@aliyun.com <winglechen@gmail.com>
2025-08-30 14:02:09 +0800

    feature: inject brokerConfig and MQFacade to AckService

--- broker/src/main/java/cn/coderule/minimq/broker/domain/consumer/ack/AckManager.java
+++ broker/src/main/java/cn/coderule/minimq/broker/domain/consumer/ack/AckManager.java
@@ -1,12 +1,18 @@
 package cn.coderule.minimq.broker.domain.consumer.ack;
 
 import cn.coderule.common.convention.service.Lifecycle;
+import cn.coderule.minimq.broker.infra.store.MQStore;
 import cn.coderule.minimq.broker.server.bootstrap.BrokerContext;
+import cn.coderule.minimq.domain.config.server.BrokerConfig;
+import cn.coderule.minimq.rpc.store.facade.MQFacade;
 
 public class AckManager implements Lifecycle {
     @Override
     public void initialize() throws Exception {
-        AckService ackService = new AckService();
+        BrokerConfig brokerConfig = BrokerContext.getBean(BrokerConfig.class);
+        MQFacade mqFacade = BrokerContext.getBean(MQStore.class);
+
+        AckService ackService = new AckService(brokerConfig, mqFacade);
         BrokerContext.register(ackService);
 
         InvisibleService invisibleService = new InvisibleService();
