c897895c8aa4157fab33bab8f900885b1fff17f8
winglechen@aliyun.com <winglechen@gmail.com>
2025-08-30 13:55:00 +0800

    feature: add empty AckConverter

---
+++ domain/src/main/java/cn/coderule/minimq/domain/domain/consumer/ack/AckConverter.java
@@ -0,0 +1,10 @@
+package cn.coderule.minimq.domain.domain.consumer.ack;
+
+import cn.coderule.minimq.domain.domain.consumer.ack.broker.AckRequest;
+import cn.coderule.minimq.domain.domain.consumer.ack.store.AckMessage;
+
+public class AckConverter {
+    public static AckMessage toAckMessage(AckRequest request) {
+        return null;
+    }
+}
