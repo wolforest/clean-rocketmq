f1fb168315be28b29445c5218320729e8c9e2378
winglechen@aliyun.com <winglechen@gmail.com>
2025-09-02 09:06:46 +0800

    test: add empty test file StoreUtilsTest

---
+++ domain/src/test/java/cn/coderule/minimq/domain/utils/StoreUtilsTest.java
@@ -0,0 +1,12 @@
+package cn.coderule.minimq.domain.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StoreUtilsTest {
+
+    @Test
+    void offsetToFileName() {
+    }
+}
--- store/src/test/java/cn/coderule/minimq/store/infra/file/DefaultMappedFileTest.java
+++ store/src/test/java/cn/coderule/minimq/store/infra/file/DefaultMappedFileTest.java
@@ -1,128 +1,19 @@
 package cn.coderule.minimq.store.infra.file;
 
+import java.nio.file.Path;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DefaultMappedFileTest {
 
-    @Test
-    void isFull() {
-    }
+    @TempDir
+    Path tmpDir;
 
     @Test
-    void hasSpace() {
+    void testCreateMappedFile() {
     }
 
-    @Test
-    void containsOffset() {
-    }
 
-    @Test
-    void getInsertPosition() {
-    }
-
-    @Test
-    void setInsertOffset() {
-    }
-
-    @Test
-    void setInsertPosition() {
-    }
-
-    @Test
-    void insert() {
-    }
-
-    @Test
-    void testInsert() {
-    }
-
-    @Test
-    void testInsert1() {
-    }
-
-    @Test
-    void select() {
-    }
-
-    @Test
-    void testSelect() {
-    }
-
-    @Test
-    void selectFromChannel() {
-    }
-
-    @Test
-    void flush() {
-    }
-
-    @Test
-    void commit() {
-    }
-
-    @Test
-    void getMappedByteBuffer() {
-    }
-
-    @Test
-    void sliceByteBuffer() {
-    }
-
-    @Test
-    void warmup() {
-    }
-
-    @Test
-    void testWarmup() {
-    }
-
-    @Test
-    void cleanup() {
-    }
-
-    @Test
-    void destroy() {
-    }
-
-    @Test
-    void getWritePosition() {
-    }
-
-    @Test
-    void setWritePosition() {
-    }
-
-    @Test
-    void getCommitPosition() {
-    }
-
-    @Test
-    void isInMemory() {
-    }
-
-    @Test
-    void getFlushPosition() {
-    }
-
-    @Test
-    void getFileName() {
-    }
-
-    @Test
-    void getMinOffset() {
-    }
-
-    @Test
-    void getMaxOffset() {
-    }
-
-    @Test
-    void getFileSize() {
-    }
-
-    @Test
-    void getStoreTimestamp() {
-    }
 }
