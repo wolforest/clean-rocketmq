d038a75be699bb962d7b10a092b16ab448d532d3
winglechen@aliyun.com <winglechen@gmail.com>
2025-09-02 15:42:26 +0800

    test: add test file DefaultMappedFileQueueTest

---
+++ store/src/test/java/cn/coderule/minimq/store/infra/file/DefaultMappedFileQueueTest.java
@@ -0,0 +1,17 @@
+package cn.coderule.minimq.store.infra.file;
+
+import cn.coderule.minimq.domain.service.store.infra.MappedFileQueue;
+import java.nio.file.Path;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DefaultMappedFileQueueTest {
+
+    @Test
+    void testCreateMappedFileQueue(@TempDir Path tmpDir) {
+        MappedFileQueue queue = new DefaultMappedFileQueue(tmpDir.toString(), 1024);
+        queue.load();
+    }
+}
