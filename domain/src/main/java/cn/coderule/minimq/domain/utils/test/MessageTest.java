f72fdda4fbb05a7cdaa1441f26355e2e9e506389
winglechen@aliyun.com <winglechen@gmail.com>
2025-09-03 08:15:07 +0800

    feature: add method createStoreConfig to ConfigTest

--- domain/src/main/java/cn/coderule/minimq/domain/utils/test/MessageTest.java
+++ domain/src/main/java/cn/coderule/minimq/domain/utils/test/MessageTest.java
@@ -1,4 +1,12 @@
 package cn.coderule.minimq.domain.utils.test;
 
+import cn.coderule.minimq.domain.domain.message.MessageBO;
+
 public class MessageTest {
+    public static MessageBO createMessage() {
+        return MessageBO.builder()
+            .topic("test")
+            .body("test".getBytes())
+            .build();
+    }
 }
