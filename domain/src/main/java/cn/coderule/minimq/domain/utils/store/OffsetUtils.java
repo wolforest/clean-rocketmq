be9ff2846e0f37b46ec2c190518f814fac0120bb
winglechen@aliyun.com <winglechen@gmail.com>
2025-09-03 08:54:50 +0800

    feature: add method fileNameToOffset to OffsetUtils

--- domain/src/main/java/cn/coderule/minimq/domain/utils/store/OffsetUtils.java
+++ domain/src/main/java/cn/coderule/minimq/domain/utils/store/OffsetUtils.java
@@ -1,5 +1,6 @@
 package cn.coderule.minimq.domain.utils.store;
 
+import cn.coderule.common.util.lang.string.StringUtil;
 import java.text.NumberFormat;
 
 public class OffsetUtils {
@@ -10,5 +11,20 @@ public class OffsetUtils {
         nf.setMaximumFractionDigits(0);
         nf.setGroupingUsed(false);
         return nf.format(offset);
+    }
+
+    public static long fileNameToOffset(String fileName) {
+
+        if (StringUtil.isBlank(fileName)) {
+            return 0;
+        }
+
+        int length = fileName.length();
+        if (length < 20) {
+            return 0;
+        }
+
+        String numStr = fileName.substring(length - 20);
+        return Long.parseLong(fileName);
     }
 }
