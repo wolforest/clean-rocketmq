86f1bd9831b14f903bc043a1819b183c04972cb5
winglechen@aliyun.com <winglechen@gmail.com>
2025-09-05 07:57:44 +0800

    feature: impl method flush of SyncCommitLogFlusher

--- store/src/main/java/cn/coderule/minimq/store/domain/commitlog/flush/SyncCommitLogFlusher.java
+++ store/src/main/java/cn/coderule/minimq/store/domain/commitlog/flush/SyncCommitLogFlusher.java
@@ -1,30 +1,44 @@
 package cn.coderule.minimq.store.domain.commitlog.flush;
 
-import cn.coderule.minimq.domain.config.server.StoreConfig;
 import cn.coderule.minimq.domain.domain.cluster.store.InsertFuture;
 import cn.coderule.minimq.domain.domain.cluster.store.InsertResult;
 import cn.coderule.minimq.domain.domain.message.MessageBO;
 import cn.coderule.minimq.domain.service.store.domain.commitlog.CommitLogFlusher;
 import cn.coderule.minimq.domain.service.store.infra.MappedFileQueue;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * simply sync commit log
  * for testing
  */
+@Slf4j
 public class SyncCommitLogFlusher implements CommitLogFlusher {
-    private final StoreConfig storeConfig;
+    private static final int RETRY_TIMES = 10;
+
     private final MappedFileQueue mappedFileQueue;
 
-    public SyncCommitLogFlusher(
-        StoreConfig storeConfig,
-        MappedFileQueue mappedFileQueue
-    ) {
-        this.storeConfig = storeConfig;
+    public SyncCommitLogFlusher(MappedFileQueue mappedFileQueue) {
         this.mappedFileQueue = mappedFileQueue;
     }
 
     @Override
     public InsertFuture flush(InsertResult insertResult, MessageBO messageBO) {
-        return null;
+        boolean status = forceFlush();
+        if (!status) {
+            return InsertFuture.failure();
+        }
+
+        return InsertFuture.success(insertResult, messageBO);
+    }
+
+    private boolean forceFlush() {
+        boolean result = false;
+        for (int i = 0; i < RETRY_TIMES && !result; i++) {
+            result = mappedFileQueue.flush(0);
+            String status = result ? "OK" : "Not OK";
+            log.info("SyncCommitLogFlusher shutdown, retry {} times {}", i + 1, status);
+        }
+
+        return result;
     }
 }
