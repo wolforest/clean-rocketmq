bd4d3d7c8dfe94a3a0becff6ab04e7c11a3194be
winglechen@aliyun.com <winglechen@gmail.com>
2025-09-05 15:30:29 +0800

    feature: impl method createMessage of MessageMock

--- domain/src/main/java/cn/coderule/minimq/domain/utils/test/MessageMock.java
+++ domain/src/main/java/cn/coderule/minimq/domain/utils/test/MessageMock.java
@@ -1,12 +1,29 @@
 package cn.coderule.minimq.domain.utils.test;
 
 import cn.coderule.minimq.domain.domain.message.MessageBO;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.util.HashMap;
 
 public class MessageMock {
     public static MessageBO createMessage() {
+        long now = System.currentTimeMillis();
+        SocketAddress address = new InetSocketAddress("0.0.0.0", 0);
         return MessageBO.builder()
             .topic("test")
             .body("test".getBytes())
+            .flag(0)
+            .queueId(0)
+            .commitOffset(0)
+            .sysFlag(0)
+            .bornTimestamp(now)
+            .storeTimestamp(now)
+            .bornHost(address)
+            .storeHost(address)
+            .reconsumeTimes(0)
+            .preparedTransactionOffset(0)
+            .properties(new HashMap<>())
             .build();
     }
+
 }
