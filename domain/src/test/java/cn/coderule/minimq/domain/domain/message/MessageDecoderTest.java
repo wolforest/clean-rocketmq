0d70835dfdc32d72447c607c60421b50ffdbd618
winglechen@aliyun.com <winglechen@gmail.com>
2025-09-06 09:56:41 +0800

    test: add empty test file MessageDecoderTest and MessageEncoderTest

---
+++ domain/src/test/java/cn/coderule/minimq/domain/domain/message/MessageDecoderTest.java
@@ -0,0 +1,12 @@
+package cn.coderule.minimq.domain.domain.message;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MessageDecoderTest {
+
+    @Test
+    void decode() {
+    }
+}
---
+++ domain/src/test/java/cn/coderule/minimq/domain/domain/message/MessageEncoderTest.java
@@ -0,0 +1,24 @@
+package cn.coderule.minimq.domain.domain.message;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MessageEncoderTest {
+
+    @Test
+    void calculateMessageLength() {
+    }
+
+    @Test
+    void validate() {
+    }
+
+    @Test
+    void calculate() {
+    }
+
+    @Test
+    void encode() {
+    }
+}
