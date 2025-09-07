5c7f192e87de1a675ec5c420ddc939338e0afb42
winglechen@aliyun.com <winglechen@gmail.com>
2025-09-07 11:06:09 +0800

    feature: add QueueMock

--- domain/src/main/java/cn/coderule/minimq/domain/utils/test/MessageMock.java
+++ domain/src/main/java/cn/coderule/minimq/domain/utils/test/MessageMock.java
@@ -1,18 +1,11 @@
 package cn.coderule.minimq.domain.utils.test;
 
-import cn.coderule.common.util.lang.string.StringUtil;
 import cn.coderule.minimq.domain.domain.message.MessageBO;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.util.HashMap;
 
 public class MessageMock {
-    private static final String TOPIC_PREFIX = "MQT_";
-
-    public static String createTopic() {
-        return TOPIC_PREFIX + StringUtil.uuid();
-    }
-
     public static MessageBO createMessage() {
         long now = System.currentTimeMillis();
         SocketAddress address = new InetSocketAddress("0.0.0.0", 0);
