ff03a14007691f49b4fd5c023661bd6e963a70d1
winglechen@aliyun.com <winglechen@gmail.com>
2025-09-07 11:56:33 +0800

    feature: add method createBrokerConfig to ConfigMock

--- domain/src/main/java/cn/coderule/minimq/domain/test/ConfigMock.java
+++ domain/src/main/java/cn/coderule/minimq/domain/test/ConfigMock.java
@@ -1,9 +1,14 @@
 package cn.coderule.minimq.domain.test;
 
 import cn.coderule.minimq.domain.config.business.MessageConfig;
+import cn.coderule.minimq.domain.config.business.TaskConfig;
 import cn.coderule.minimq.domain.config.business.TimerConfig;
 import cn.coderule.minimq.domain.config.business.TopicConfig;
+import cn.coderule.minimq.domain.config.business.TransactionConfig;
+import cn.coderule.minimq.domain.config.network.GrpcConfig;
 import cn.coderule.minimq.domain.config.network.RpcClientConfig;
+import cn.coderule.minimq.domain.config.network.RpcServerConfig;
+import cn.coderule.minimq.domain.config.server.BrokerConfig;
 import cn.coderule.minimq.domain.config.server.StoreConfig;
 import cn.coderule.minimq.domain.config.store.CommitConfig;
 import cn.coderule.minimq.domain.config.store.ConsumeQueueConfig;
@@ -25,5 +30,21 @@ public class ConfigMock {
 
         StorePath.initPath(rootPath);
         return storeConfig;
+    }
+
+    public static BrokerConfig createBrokerConfig() {
+        BrokerConfig brokerConfig = new BrokerConfig();
+
+        brokerConfig.setMessageConfig(new MessageConfig());
+        brokerConfig.setTopicConfig(new TopicConfig());
+        brokerConfig.setTimerConfig(new TimerConfig());
+        brokerConfig.setTransactionConfig(new TransactionConfig());
+        brokerConfig.setTaskConfig(new TaskConfig());
+
+        brokerConfig.setGrpcConfig(new GrpcConfig());
+        brokerConfig.setRpcClientConfig(new RpcClientConfig());
+        brokerConfig.setRpcServerConfig(new RpcServerConfig());
+
+        return brokerConfig;
     }
 }
