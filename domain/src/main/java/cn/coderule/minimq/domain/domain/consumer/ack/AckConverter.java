2c587dd9dc4a58e97243260e6ecc06fd882e8760
winglechen@aliyun.com <winglechen@gmail.com>
2025-09-11 16:12:45 +0800

    feature: add method isConsumeOrderly to AckMessage

--- domain/src/main/java/cn/coderule/minimq/domain/domain/consumer/ack/AckConverter.java
+++ domain/src/main/java/cn/coderule/minimq/domain/domain/consumer/ack/AckConverter.java
@@ -5,12 +5,16 @@ import cn.coderule.minimq.domain.domain.consumer.ack.broker.InvisibleRequest;
 import cn.coderule.minimq.domain.domain.consumer.ack.store.AckMessage;
 import cn.coderule.minimq.domain.domain.consumer.ack.store.CheckPointRequest;
 import cn.coderule.minimq.domain.domain.message.MessageBO;
+import cn.coderule.minimq.domain.utils.message.ExtraInfoUtils;
 
 public class AckConverter {
     public static AckMessage toAckMessage(AckRequest request) {
+        String[] extraInfo = ExtraInfoUtils.split(request.getExtraInfo());
 
-
-        return null;
+        return AckMessage.builder()
+            .reviveQueueId(ExtraInfoUtils.getReviveQid(extraInfo))
+            .invisibleTime(ExtraInfoUtils.getInvisibleTime(extraInfo))
+            .build();
     }
 
     public static CheckPointRequest toCheckPointRequest(InvisibleRequest request) {
--- domain/src/main/java/cn/coderule/minimq/domain/domain/consumer/ack/store/AckMessage.java
+++ domain/src/main/java/cn/coderule/minimq/domain/domain/consumer/ack/store/AckMessage.java
@@ -2,10 +2,17 @@ package cn.coderule.minimq.domain.domain.consumer.ack.store;
 
 import cn.coderule.minimq.domain.domain.cluster.RequestContext;
 import cn.coderule.minimq.domain.domain.consumer.ack.AckInfo;
+import cn.coderule.minimq.domain.domain.meta.topic.KeyBuilder;
 import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AckMessage implements Serializable {
     private RequestContext requestContext;
 
@@ -15,5 +22,7 @@ public class AckMessage implements Serializable {
     private int reviveQueueId;
     private long invisibleTime;
 
-    private boolean isBatch = false;
+    public boolean isConsumeOrderly() {
+        return KeyBuilder.POP_ORDER_REVIVE_QUEUE == reviveQueueId;
+    }
 }
