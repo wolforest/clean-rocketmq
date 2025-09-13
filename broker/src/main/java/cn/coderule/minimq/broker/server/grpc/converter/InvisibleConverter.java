1ac330a77bf1ce83034cadae7dee2ed5157ff263
winglechen@aliyun.com <winglechen@gmail.com>
2025-09-13 17:23:26 +0800

    feature: add method toResponse to InvisibleConverter

--- broker/src/main/java/cn/coderule/minimq/broker/server/grpc/converter/InvisibleConverter.java
+++ broker/src/main/java/cn/coderule/minimq/broker/server/grpc/converter/InvisibleConverter.java
@@ -1,8 +1,13 @@
 package cn.coderule.minimq.broker.server.grpc.converter;
 
 import apache.rocketmq.v2.ChangeInvisibleDurationRequest;
+import apache.rocketmq.v2.ChangeInvisibleDurationResponse;
+import apache.rocketmq.v2.Code;
+import apache.rocketmq.v2.Status;
 import cn.coderule.minimq.domain.domain.cluster.RequestContext;
+import cn.coderule.minimq.domain.domain.consumer.ack.broker.AckResult;
 import cn.coderule.minimq.domain.domain.consumer.ack.broker.InvisibleRequest;
+import cn.coderule.minimq.rpc.common.grpc.response.ResponseBuilder;
 import com.google.protobuf.util.Durations;
 
 public class InvisibleConverter {
@@ -20,4 +25,22 @@ public class InvisibleConverter {
             .invisibleTime(invisibleTime)
             .build();
     }
+
+    public static ChangeInvisibleDurationResponse toResponse(AckResult ackResult) {
+        Status status;
+        if (ackResult.isSuccess()) {
+            status = ResponseBuilder.getInstance()
+                .buildStatus(Code.OK, Code.OK.name());
+        } else {
+            status = ResponseBuilder.getInstance()
+                .buildStatus(Code.INTERNAL_SERVER_ERROR, "");
+        }
+
+        return ChangeInvisibleDurationResponse.newBuilder()
+            .setStatus(status)
+            .setReceiptHandle(ackResult.getReceiptStr())
+            .build();
+    }
+
+
 }
--- domain/src/main/java/cn/coderule/minimq/domain/domain/consumer/ack/broker/AckResult.java
+++ domain/src/main/java/cn/coderule/minimq/domain/domain/consumer/ack/broker/AckResult.java
@@ -1,16 +1,17 @@
 package cn.coderule.minimq.domain.domain.consumer.ack.broker;
 
 import cn.coderule.minimq.domain.core.enums.consume.AckStatus;
-import cn.coderule.minimq.domain.core.exception.BrokerException;
-import cn.coderule.minimq.domain.domain.consumer.receipt.MessageIdReceipt;
+import java.io.Serializable;
+import lombok.Data;
+import lombok.ToString;
 
-public class AckResult {
+@Data
+@ToString
+public class AckResult implements Serializable {
     private AckStatus status;
-    private String extraInfo;
+    private String extraInfo = "";
+    private String receiptStr = "";
     private long popTime;
-
-    private MessageIdReceipt idReceipt;
-    private BrokerException brokerException;
 
     public static AckResult success() {
         AckResult result = new AckResult();
@@ -23,32 +24,4 @@ public class AckResult {
         return status == AckStatus.OK;
     }
 
-    public void setPopTime(long popTime) {
-        this.popTime = popTime;
-    }
-
-    public long getPopTime() {
-        return popTime;
-    }
-
-    public AckStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(AckStatus status) {
-        this.status = status;
-    }
-
-    public void setExtraInfo(String extraInfo) {
-        this.extraInfo = extraInfo;
-    }
-
-    public String getExtraInfo() {
-        return extraInfo;
-    }
-
-    @Override
-    public String toString() {
-        return "AckResult [AckStatus=" + status + ",extraInfo=" + extraInfo + "]";
-    }
 }
