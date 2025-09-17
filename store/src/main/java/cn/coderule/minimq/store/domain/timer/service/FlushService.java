cc436ace5c2a22f70b9db7d5222c09ee16a9130f
winglechen@aliyun.com <winglechen@gmail.com>
2025-09-17 22:38:50 +0800

    feature: add constructor to FlushService

--- store/src/main/java/cn/coderule/minimq/store/domain/timer/service/FlushService.java
+++ store/src/main/java/cn/coderule/minimq/store/domain/timer/service/FlushService.java
@@ -1,4 +1,26 @@
 package cn.coderule.minimq.store.domain.timer.service;
 
-public class FlushService {
+import cn.coderule.common.lang.concurrent.thread.ServiceThread;
+import cn.coderule.minimq.domain.config.server.StoreConfig;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class FlushService extends ServiceThread {
+    private final StoreConfig storeConfig;
+    private final TimerService timerService;
+
+    public FlushService(StoreConfig storeConfig, TimerService timerService) {
+        this.storeConfig = storeConfig;
+        this.timerService = timerService;
+    }
+
+    @Override
+    public String getServiceName() {
+        return FlushService.class.getSimpleName();
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
