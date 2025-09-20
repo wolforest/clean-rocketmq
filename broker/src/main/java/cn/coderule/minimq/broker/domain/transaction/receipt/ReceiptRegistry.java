f8e0255e31cd5167b74ef35e480bcb57676cb3a9
winglechen@aliyun.com <winglechen@gmail.com>
2025-09-20 17:09:56 +0800

    feature: add constructor to ReceiptRegistry

--- broker/src/main/java/cn/coderule/minimq/broker/domain/transaction/receipt/ReceiptRegistry.java
+++ broker/src/main/java/cn/coderule/minimq/broker/domain/transaction/receipt/ReceiptRegistry.java
@@ -1,9 +1,26 @@
 package cn.coderule.minimq.broker.domain.transaction.receipt;
 
+import java.util.Map;
+import java.util.NavigableSet;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicLong;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class ReceiptRegistry {
+
+    private final AtomicLong maxExpireTime;
+    // producerGroup@transactionId -> Set<Receipt>
+    private final ConcurrentMap<String, NavigableSet<Receipt>> receiptMap;
+
+    public ReceiptRegistry() {
+        long now = System.currentTimeMillis();
+        this.maxExpireTime = new AtomicLong(now);
+
+        this.receiptMap = new ConcurrentHashMap<>();
+    }
+
 
     public void register(Receipt receipt) {
 
@@ -15,5 +32,9 @@ public class ReceiptRegistry {
 
     public void remove(Receipt receipt) {
 
+    }
+
+    private String buildKey(String producerGroup, String transactionId) {
+        return producerGroup + "@" + transactionId;
     }
 }
