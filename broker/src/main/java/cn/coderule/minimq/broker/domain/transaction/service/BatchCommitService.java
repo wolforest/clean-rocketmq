cd7e9575fa26829b7b98ddf613654c1e5c33ef29
winglechen@aliyun.com <winglechen@gmail.com>
2025-09-22 09:42:10 +0800

    feature: add method run/calculateCommitInterval to BatchCommitService

--- broker/src/main/java/cn/coderule/minimq/broker/domain/transaction/service/BatchCommitService.java
+++ broker/src/main/java/cn/coderule/minimq/broker/domain/transaction/service/BatchCommitService.java
@@ -4,12 +4,16 @@ import cn.coderule.common.lang.concurrent.thread.ServiceThread;
 import cn.coderule.minimq.broker.infra.store.MQStore;
 import cn.coderule.minimq.domain.config.business.TransactionConfig;
 import cn.coderule.minimq.domain.domain.transaction.CommitBuffer;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class BatchCommitService extends ServiceThread {
     private final TransactionConfig transactionConfig;
     private final CommitBuffer commitBuffer;
     private final MQStore mqStore;
     private final MessageFactory messageFactory;
+
+    private long wakeupTime;
 
     public BatchCommitService(
         TransactionConfig transactionConfig,
@@ -23,6 +27,7 @@ public class BatchCommitService extends ServiceThread {
         this.messageFactory = messageFactory;
 
         this.mqStore = mqStore;
+        initWakeupTime();
     }
 
     @Override
@@ -32,7 +37,35 @@ public class BatchCommitService extends ServiceThread {
 
     @Override
     public void run() {
+        log.info("{} service started", this.getServiceName());
 
+        while (!this.isStopped()) {
+            long interval = calculateCommitInterval();
+            if (interval > 0) {
+                this.await(interval);
+            }
+
+            commit();
+        }
+
+        log.info("{} service stopped", this.getServiceName());
+    }
+
+    private void initWakeupTime() {
+        long now = System.currentTimeMillis();
+        long interval = transactionConfig.getBatchCommitInterval();
+
+        this.wakeupTime = now + interval;
+    }
+
+    private long calculateCommitInterval() {
+        long now = System.currentTimeMillis();
+        long interval = wakeupTime - now;
+        if (interval <= 0) {
+            interval = 0;
+        }
+
+        return interval;
     }
 
     private void commit() {
