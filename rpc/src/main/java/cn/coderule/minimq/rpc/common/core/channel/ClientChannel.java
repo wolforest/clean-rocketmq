07ed2dfa0065d509d8e9d0deb2d62e6c9f077fae
winglechen@aliyun.com <winglechen@gmail.com>
2025-09-24 15:42:19 +0800

    comment: delete useless comments of ClientChannel;

--- rpc/src/main/java/cn/coderule/minimq/rpc/common/core/channel/ClientChannel.java
+++ rpc/src/main/java/cn/coderule/minimq/rpc/common/core/channel/ClientChannel.java
@@ -2,8 +2,6 @@
 package cn.coderule.minimq.rpc.common.core.channel;
 
 import cn.coderule.common.util.net.NetworkUtil;
-import cn.coderule.minimq.broker.server.grpc.service.channel.ChannelManager;
-import cn.coderule.minimq.broker.server.grpc.service.channel.SettingManager;
 import cn.coderule.minimq.rpc.common.core.channel.mock.MockChannel;
 import cn.coderule.minimq.rpc.common.core.relay.RelayService;
 import io.netty.channel.Channel;
@@ -35,78 +33,6 @@ public abstract class ClientChannel extends MockChannel {
     }
 
     public abstract RelayService getRelayService();
-
-//    @Override
-//    public ChannelFuture writeAndFlush(Object msg) {
-//        CompletableFuture<Void> processFuture = new CompletableFuture<>();
-//
-//        try {
-//            if (msg instanceof RpcCommand command) {
-//                RequestContext context = RequestContext.createForInner(this.getClass())
-//                    .setRemoteAddress(remoteAddress)
-//                    .setLocalAddress(localAddress);
-//                if (command.getExtFields() == null) {
-//                    command.setExtFields(new HashMap<>());
-//                }
-//                switch (command.getCode()) {
-//                    case RequestCode.CHECK_TRANSACTION_STATE: {
-//                        CheckTransactionStateRequestHeader header = (CheckTransactionStateRequestHeader) command.readCustomHeader();
-//                        MessageBO MessageBO = MessageDecoder.decode(ByteBuffer.wrap(command.getBody()), true, false, false);
-//                        RelayData<TransactionData, Void> relayData = this.relayService.checkTransaction(context, command, header, MessageBO);
-//                        processFuture = this.processCheckTransaction(header, MessageBO, relayData.getProcessResult(), relayData.getRelayFuture());
-//                        break;
-//                    }
-//                    case RequestCode.GET_CONSUMER_RUNNING_INFO: {
-//                        GetConsumerRunningInfoRequestHeader header = (GetConsumerRunningInfoRequestHeader) command.readCustomHeader();
-//                        CompletableFuture<RelayResult<ConsumerRunningInfo>> relayFuture = this.relayService.getConsumerInfo(context, command, header);
-//                        processFuture = this.processGetConsumerRunningInfo(command, header, relayFuture);
-//                        break;
-//                    }
-//                    case RequestCode.CONSUME_MESSAGE_DIRECTLY: {
-//                        ConsumeMessageDirectlyResultRequestHeader header = (ConsumeMessageDirectlyResultRequestHeader) command.readCustomHeader();
-//                        MessageBO MessageBO = MessageDecoder.decode(ByteBuffer.wrap(command.getBody()), true, false, false);
-//                        processFuture = this.processConsumeMessageDirectly(command, header, MessageBO,
-//                            this.relayService.consumeMessage(context, command, header));
-//                        break;
-//                    }
-//                    default:
-//                        break;
-//                }
-//            } else {
-//                processFuture = processOtherMessage(msg);
-//            }
-//        } catch (Throwable t) {
-//            log.error("process failed. msg:{}", msg, t);
-//            processFuture.completeExceptionally(t);
-//        }
-//
-//        DefaultChannelPromise promise = new DefaultChannelPromise(this, GlobalEventExecutor.INSTANCE);
-//        processFuture.thenAccept(ignore -> promise.setSuccess())
-//            .exceptionally(t -> {
-//                promise.setFailure(t);
-//                return null;
-//            });
-//        return promise;
-//    }
-
-//    protected abstract CompletableFuture<Void> processOtherMessage(Object msg);
-//
-//    protected abstract CompletableFuture<Void> processCheckTransaction(
-//        CheckTransactionStateRequestHeader header,
-//        MessageBO MessageBO,
-//        TransactionData transactionData,
-//        CompletableFuture<RelayResult<Void>> responseFuture);
-//
-//    protected abstract CompletableFuture<Void> processGetConsumerRunningInfo(
-//        RpcCommand command,
-//        GetConsumerRunningInfoRequestHeader header,
-//        CompletableFuture<RelayResult<ConsumerRunningInfo>> responseFuture);
-//
-//    protected abstract CompletableFuture<Void> processConsumeMessageDirectly(
-//        RpcCommand command,
-//        ConsumeMessageDirectlyResultRequestHeader header,
-//        MessageBO MessageBO,
-//        CompletableFuture<RelayResult<ConsumeMessageDirectlyResult>> responseFuture);
 
     @Override
     public ChannelConfig config() {
