30ee7ae1ec1460f039120997bb1338c6360ac8ca
winglechen@aliyun.com <winglechen@gmail.com>
2025-10-17 17:42:59 +0800

    test: add empty test file GroupFlusherTest

---
+++ store/src/test/java/cn/coderule/minimq/store/domain/commitlog/flush/GroupFlusherTest.java
@@ -0,0 +1,14 @@
+package cn.coderule.minimq.store.domain.commitlog.flush;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GroupFlusherTest {
+
+    @Test
+    void testGroupFlush() {
+
+    }
+
+}
