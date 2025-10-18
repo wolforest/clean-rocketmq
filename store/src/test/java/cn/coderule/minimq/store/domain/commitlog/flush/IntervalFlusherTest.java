533db1d1a9ac278105dc1395dc04119b149e0c8c
winglechen@aliyun.com <winglechen@gmail.com>
2025-10-18 22:27:13 +0800

    test: add empty test file IntervalFlusherTest

---
+++ store/src/test/java/cn/coderule/minimq/store/domain/commitlog/flush/IntervalFlusherTest.java
@@ -0,0 +1,19 @@
+package cn.coderule.minimq.store.domain.commitlog.flush;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IntervalFlusherTest {
+
+    @Test
+    void testFlush() {
+        // TODO: 2023/10/23
+    }
+
+    @Test
+    void testTimeout() {
+        // TODO: 2023/10/23
+    }
+
+}
