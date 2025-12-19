c35a599c6098d8ba1e834fb6b180f9f059dc96fd
winglechen@aliyun.com <winglechen@gmail.com>
2025-12-19 16:58:00 +0800

    feature: add Benchmark Suit

---
+++ test/src/main/java/cn/coderule/wolfmq/test/benchmark/core/Suit.java
@@ -0,0 +1,12 @@
+package cn.coderule.wolfmq.test.benchmark.core;
+
+import java.io.Serializable;
+import java.util.List;
+import lombok.Data;
+
+@Data
+public class Suit implements Serializable {
+    private List<Config> configList;
+    private List<Report> reportList;
+    private Benchmark benchmark;
+}
