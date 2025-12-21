a224d275162dfca5ee05474084eb44abe20da42b
winglechen@aliyun.com <winglechen@gmail.com>
2025-12-21 14:38:19 +0800

    feature: add ProduceBenchmarkSuit

---
+++ test/src/main/java/cn/coderule/wolfmq/test/benchmark/ProduceBenchmarkSuit.java
@@ -0,0 +1,11 @@
+package cn.coderule.wolfmq.test.benchmark;
+
+import cn.coderule.wolfmq.test.benchmark.core.BenchmarkSuit;
+
+public class ProduceBenchmarkSuit extends BenchmarkSuit {
+
+    @Override
+    public void prepare() {
+
+    }
+}
