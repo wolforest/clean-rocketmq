c8c1556c579190bbe5bb7359d76d2ce964357ad9
winglechen@aliyun.com <winglechen@gmail.com>
2025-12-28 00:21:47 +0800

    config: add dirList to commitLog config

--- domain/src/main/java/cn/coderule/minimq/domain/config/store/CommitConfig.java
+++ domain/src/main/java/cn/coderule/minimq/domain/config/store/CommitConfig.java
@@ -3,22 +3,25 @@ package cn.coderule.minimq.domain.config.store;
 import cn.coderule.minimq.domain.config.ConfigAttribute;
 import cn.coderule.minimq.domain.core.enums.store.FlushType;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Data;
 
 @Data
 public class CommitConfig implements Serializable {
-    private boolean enableMultiPath = false;
+    private boolean enableMultiDir = false;
     private boolean enableWriteCache = false;
 
     private boolean enableSharding = false;
     private int maxShardingNumber = 10;
 
-    private FlushType flushType = ConfigAttribute.FLUSH_TYPE;
-
     private String dirName = "commitlog";
+    private List<String> dirList = new ArrayList<>();
+
     private int fileSize = ConfigAttribute.MMAP_FILE_SIZE;
 
     private boolean enableFlushSleep = true;
+    private FlushType flushType = ConfigAttribute.FLUSH_TYPE;
     private int minFlushPages = 4;
     private int flushInterval = 500;
     private int flushTimeout = 5_000;
