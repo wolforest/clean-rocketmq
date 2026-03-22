86eb8b574eaae7d7c37dfcd041b75ceed9c67b76
winglechen@aliyun.com <winglechen@gmail.com>
2026-03-22 14:00:07 +0800

    feature: add OffsetCodec for commmitLog and related unit tests

---
+++ store/src/main/java/cn/coderule/minimq/store/domain/commitlog/OffsetCodec.java
@@ -0,0 +1,27 @@
+package cn.coderule.minimq.store.domain.commitlog;
+
+public class OffsetCodec {
+    private final int shardId;
+    private final int maxShardingNumber;
+
+    public OffsetCodec(int shardId, int maxShardingNumber) {
+        this.shardId = shardId;
+        this.maxShardingNumber = maxShardingNumber;
+    }
+
+    public long encode(long offset) {
+        if (offset < 0) {
+            throw new IllegalArgumentException("offset can't be negative");
+        }
+
+        return offset * maxShardingNumber + shardId;
+    }
+
+    public long decode(long offset) {
+        if (offset < 0) {
+            throw new IllegalArgumentException("offset can't be negative");
+        }
+
+        return (offset - shardId) / maxShardingNumber;
+    }
+}
