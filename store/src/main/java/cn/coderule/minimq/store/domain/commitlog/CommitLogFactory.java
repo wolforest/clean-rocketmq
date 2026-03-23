5ac71f5b2b59fe9e039c3850ba6be973be217699
winglechen@aliyun.com <winglechen@gmail.com>
2026-03-23 19:11:11 +0800

    feature: add CommitLogFactory

---
+++ store/src/main/java/cn/coderule/minimq/store/domain/commitlog/CommitLogFactory.java
@@ -0,0 +1,68 @@
+package cn.coderule.minimq.store.domain.commitlog;
+
+import cn.coderule.common.util.io.DirUtil;
+import cn.coderule.minimq.domain.config.server.StoreConfig;
+import cn.coderule.minimq.domain.config.store.CommitConfig;
+import cn.coderule.minimq.domain.domain.store.domain.commitlog.CommitLog;
+import cn.coderule.minimq.domain.domain.store.domain.commitlog.CommitLogFlushPolicy;
+import cn.coderule.minimq.domain.domain.store.infra.MappedFileQueue;
+import cn.coderule.minimq.domain.domain.store.server.CheckPoint;
+import cn.coderule.minimq.store.domain.commitlog.flush.DefaultCommitLogFlushPolicy;
+import cn.coderule.minimq.store.infra.file.AllocateMappedFileService;
+import cn.coderule.minimq.store.infra.file.DefaultMappedFileQueue;
+import cn.coderule.minimq.store.server.bootstrap.StoreContext;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CommitLogFactory {
+    private final StoreConfig storeConfig;
+    private final CommitConfig commitConfig;
+
+    private final int shardNumber;
+    private final CheckPoint checkpoint;
+
+    public CommitLogFactory(StoreConfig storeConfig, CheckPoint checkpoint) {
+        this.storeConfig = storeConfig;
+        this.commitConfig = storeConfig.getCommitConfig();
+        this.shardNumber = commitConfig.getShardingNumber();
+
+        this.checkpoint = checkpoint;
+
+    }
+
+    public List<CommitLog> createByConfig() {
+        List<CommitLog> logList = new ArrayList<>();
+        for (int i = 0; i < shardNumber; i++) {
+            logList.add(i, createByShardId(i));
+        }
+
+        return logList;
+    }
+
+
+    public CommitLog createByShardId(int shardId) {
+        MappedFileQueue fileQueue = initMappedFileQueue(shardId);
+        CommitLogFlushPolicy flushPolicy = initFlushPolicy(shardId, fileQueue);
+        return new DefaultCommitLog( storeConfig, shardId, fileQueue, flushPolicy, checkpoint);
+    }
+
+    private DefaultMappedFileQueue initMappedFileQueue(int shardId) {
+        String dir = storeConfig.getRootDir()
+            + File.separator
+            + commitConfig.getDirName()
+            + File.separator
+            + shardId
+            + File.separator;
+        DirUtil.createIfNotExists(dir);
+
+        AllocateMappedFileService allocateService = StoreContext.getBean(AllocateMappedFileService.class);
+        return new DefaultMappedFileQueue(dir, commitConfig.getFileSize(), allocateService);
+    }
+
+    private CommitLogFlushPolicy initFlushPolicy(int shardId, MappedFileQueue mappedFileQueue) {
+        return new DefaultCommitLogFlushPolicy(shardId, commitConfig, mappedFileQueue, checkpoint);
+    }
+
+
+}
