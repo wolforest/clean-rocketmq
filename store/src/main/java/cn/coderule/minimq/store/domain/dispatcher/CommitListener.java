a89fa5c9fb4588f18d2083a50bf9c6d0b1086f8f
winglechen@aliyun.com <winglechen@gmail.com>
2026-03-26 00:45:59 +0800

    feature: add CommitListener

---
+++ store/src/main/java/cn/coderule/minimq/store/domain/dispatcher/CommitListener.java
@@ -0,0 +1,62 @@
+package cn.coderule.minimq.store.domain.dispatcher;
+
+import cn.coderule.common.lang.concurrent.thread.ServiceThread;
+import cn.coderule.common.util.lang.ThreadUtil;
+import cn.coderule.minimq.domain.config.store.CommitConfig;
+import cn.coderule.minimq.domain.domain.store.domain.commitlog.CommitLog;
+import cn.coderule.minimq.domain.domain.store.server.CheckPoint;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Commit Event Listener
+ * @renamed from CommitEventListener to CommitListener
+ */
+@Slf4j
+public class CommitListener extends ServiceThread {
+    private final CommitConfig config;
+
+    private final CommitLog commitLog;
+    private final CommitQueue queue;
+    private final CheckPoint checkPoint;
+
+    private final int shardId;
+
+    public CommitListener(
+        CommitConfig config,
+        CommitQueue queue,
+        CommitLog commitLog,
+        CheckPoint checkPoint
+    ) {
+        this.config = config;
+
+        this.queue = queue;
+        this.commitLog = commitLog;
+        this.checkPoint = checkPoint;
+        this.shardId = commitLog.getShardId();
+    }
+
+    @Override
+    public String getServiceName() {
+        return CommitListener.class.getSimpleName() + "-" + shardId;
+    }
+
+    @Override
+    public void run() {
+        log.info("{} service started", this.getServiceName());
+
+        while (!this.isStopped()) {
+            try {
+                ThreadUtil.sleep(1);
+                this.listen();
+            } catch (Exception e) {
+                log.error("{} service has exception. ", this.getServiceName(), e);
+            }
+        }
+
+        log.info("{} service end", this.getServiceName());
+    }
+
+    private void listen() {
+
+    }
+}
