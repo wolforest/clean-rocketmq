0784a8aef81a29342acf08fa1a47da46e4e8cf64
winglechen@aliyun.com <winglechen@gmail.com>
2026-04-05 10:57:28 +0800

    refactor: rename Main to ProduceBenchmarkMain

--- test/src/main/java/cn/coderule/wolfmq/test/benchmark/produce/ProduceBenchmarkMain.java
+++ test/src/main/java/cn/coderule/wolfmq/test/benchmark/produce/ProduceBenchmarkMain.java
@@ -3,7 +3,7 @@ package cn.coderule.wolfmq.test.benchmark.produce;
 import cn.coderule.wolfmq.test.manager.ClientManager;
 import cn.coderule.wolfmq.test.manager.ConfigManager;
 
-public class Main {
+public class ProduceBenchmarkMain {
     public static void main(String[] args) throws Exception {
         ConfigManager.init();
         ClientManager.start();
