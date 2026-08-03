1ac1689b825811324a5819b3683096843ce8fbad
winglechen@aliyun.com <winglechen@gmail.com>
2026-08-04 00:23:03 +0800

    test: add RemoteTimerStoreTest

---
+++ broker/src/test/java/cn/coderule/wolfmq/broker/infra/remote/RemoteTimerStoreTest.java
@@ -0,0 +1,31 @@
+package cn.coderule.wolfmq.broker.infra.remote;
+
+import cn.coderule.wolfmq.domain.config.server.BrokerConfig;
+import cn.coderule.wolfmq.domain.mock.ConfigMock;
+import cn.coderule.wolfmq.rpc.common.rpc.RpcClient;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class RemoteTimerStoreTest {
+
+    private RemoteTimerStore store;
+    private BrokerConfig brokerConfig;
+    private RemoteLoadBalance loadBalance;
+    private RpcClient rpcClient;
+
+    @BeforeEach
+    void setUp() {
+        brokerConfig = ConfigMock.createBrokerConfig();
+        loadBalance = mock(RemoteLoadBalance.class);
+        rpcClient = mock(RpcClient.class);
+        store = new RemoteTimerStore(brokerConfig, loadBalance, rpcClient);
+    }
+
+    @Test
+    void constructor_ShouldCreateStore() {
+        assertNotNull(store);
+    }
+}
