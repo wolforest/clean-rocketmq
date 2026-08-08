b80a71fbaa620b96f5ea35318bfcc5dbe25fb1ac
winglechen@aliyun.com <winglechen@gmail.com>
2026-08-08 10:59:53 +0800

    test: add RemoteConsumeOrderStoreTest

---
+++ broker/src/test/java/cn/coderule/wolfmq/broker/infra/remote/RemoteConsumeOrderStoreTest.java
@@ -0,0 +1,31 @@
+package cn.coderule.wolfmq.broker.infra.remote;
+
+import cn.coderule.wolfmq.domain.config.server.BrokerConfig;
+import cn.coderule.wolfmq.domain.mock.ConfigMock;
+import cn.coderule.wolfmq.rpc.common.rpc.RpcClient;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class RemoteConsumeOrderStoreTest {
+
+    private RemoteConsumeOrderStore store;
+    private BrokerConfig brokerConfig;
+    private RemoteLoadBalance loadBalance;
+    private RpcClient rpcClient;
+
+    @BeforeEach
+    void setUp() {
+        brokerConfig = ConfigMock.createBrokerConfig();
+        loadBalance = mock(RemoteLoadBalance.class);
+        rpcClient = mock(RpcClient.class);
+        store = new RemoteConsumeOrderStore(brokerConfig, loadBalance, rpcClient);
+    }
+
+    @Test
+    void constructor_ShouldCreateStore() {
+        assertNotNull(store);
+    }
+}
