10c4359f36f6f2904f1bfa3704a95ea4b7c9712a
winglechen@aliyun.com <winglechen@gmail.com>
2026-08-25 00:01:50 +0800

    test: add ReviveThreadTest

---
+++ broker/src/test/java/cn/coderule/wolfmq/broker/domain/consumer/revive/ReviveThreadTest.java
@@ -0,0 +1,33 @@
+package cn.coderule.wolfmq.broker.domain.consumer.revive;
+
+import cn.coderule.wolfmq.domain.config.business.MessageConfig;
+import cn.coderule.wolfmq.domain.config.server.BrokerConfig;
+import cn.coderule.wolfmq.domain.mock.ConfigMock;
+import cn.coderule.wolfmq.rpc.store.facade.ConsumeOffsetFacade;
+import cn.coderule.wolfmq.rpc.store.facade.MQFacade;
+import cn.coderule.wolfmq.rpc.store.facade.SubscriptionFacade;
+import cn.coderule.wolfmq.rpc.store.facade.TopicFacade;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class ReviveThreadTest {
+
+    @Test
+    void getServiceName_ShouldReturnClassName() {
+        BrokerConfig brokerConfig = ConfigMock.createBrokerConfig();
+        ReviveContext context = ReviveContext.builder()
+            .brokerConfig(brokerConfig)
+            .reviveTopic("revive-topic")
+            .mqFacade(mock(MQFacade.class))
+            .topicFacade(mock(TopicFacade.class))
+            .subscriptionFacade(mock(SubscriptionFacade.class))
+            .consumeOffsetFacade(mock(ConsumeOffsetFacade.class))
+            .build();
+        RetryService retryService = mock(RetryService.class);
+
+        ReviveThread thread = new ReviveThread(context, 0, retryService);
+        assertEquals("ReviveThread", thread.getServiceName());
+    }
+}
